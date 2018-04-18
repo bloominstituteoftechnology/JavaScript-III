90a4e84fabdf54f92e9ffe24c2fb52829c227bd8
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-18 13:42:41 -0700

    getting through the bindings

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window , or Global Binding bind the .this keyword to whatever object is on the global scope.
+
+* 2. Implicit Binding is the most commonly used binding technique for .this, and it ties .this to whatever
+     object is to the immediate left of a . 
+
+* 3. New Binding is used when we have a constructor function, and since a constructor can be used to make 
+     multiple objects, we can use "new" to make new objects easily that inheret the properties for .this
+
+* 4. With Explicit Binding, we can use .call, .apply, and .bind to assign values to .this
 *
 * write out a code example of each explanation above
 */
@@ -12,14 +17,35 @@
 // Principle 1
 
 // code example for Window Binding
+let me = "Andrew";
+
+function sayName(name) {
+    return name;
+};
+
+console.log(sayName(me));
 
 // Principle 2
 
 // code example for Implicit Binding
 
+const jerkStore = {
+  setup: 'The Jerk Store ',
+  person: function(jerk) {
+    console.log(`${this.setup} is out of ${jerk}`);
+  }
+};
+jerkStore.person('George Costanza');
+
 // Principle 3
 
 // code example for New Binding
+
+let bestFriend = 'Elaine';
+
+let Friend = {
+    
+}
 
 // Principle 4
 
