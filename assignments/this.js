91528f15ecf905d3a66a5050886ce22fcc74b4cb
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-04-18 20:43:11 +0100

    implemmented the 4 this binding role

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Default Binding : 
+* 2. implicit binding 
+* 3. explicit binding 
+* 4. new binding
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,38 @@
 // Principle 1
 
 // code example for Window Binding
+var bar = 'bar1';
+function foo(){
+    
+    console.log(this.bar)
+}
+
+foo()
 
 // Principle 2
 
 // code example for Implicit Binding
 
+var o2 = { bar:'bar2', foo : foo }
+var o3 = { bar:'bar3', foo : foo }
+
+o2.foo()
+o3.foo()
+
 // Principle 3
 
 // code example for New Binding
 
+function baz(){
+    this.bar = 'bar5';
+    console.log(this.bar)
+}
+
+var obj2 = new baz()
+
 // Principle 4
 
 // code example for Explicit Binding
+
+var obj = { bar:"bar4" }
+foo.call( obj )
