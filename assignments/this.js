6ae16c561d61683c18e046be3f5e77f4674b9192
Zach Campbell <beazlebubbles@gmail.com>
2018-04-18 13:34:32 -0600

    Answered questions and completed first code example

--- assignments/this.js
+++ assignments/this.js
@@ -2,10 +2,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1.
-* 2.
-* 3.
-* 4.
+* 1.  Window binding: the this keyword gets bound to the window object by default when misused.
+* 2.  This automatically binds to the object left of the . in the invocation if not told where to go.
+* 3.  Explicit binding binds this manually using call bind or apply.
+* 4.  New binding creates a new object and assigns this to that.
 *
 * write out a code example of each explanation above
 */
@@ -13,6 +13,10 @@
 // Principle 1
 
 // code example for Window Binding
+
+let myObject = () => this.name = "Billy";
+myObject();
+console.log(window.name); //stoopid quokka doesn't have a window object
 
 // Principle 2
 
