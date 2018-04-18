dd4e1e4135048c3ec2f396cf1783885accf9a17b
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-19 01:52:34 +0700

    completed this examples

--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,47 @@
 // Principle 1
 
 // code example for Window Binding
+function globalBinding(gblBind) {
+    console.log(this);
+    return gblBind; //?
+};
+globalBinding("Hello");
 
 // Principle 2
 
 // code example for Implicit Binding
+const implicitBinding = {
+    hello: "Hello",
+    sayHello: function(name) {
+        console.log(`${this.hello}, ${name}!`); 
+    }
+};
 
 // Principle 3
 
 // code example for New Binding
+function Person(name) {
+    this.name = name;
+    this.greeting = 'Hello';
+    this.greet = function() {
+        console.log(`${this.greeting}, ${this.name}!`)
+    }
+};
+const matthias = new Person("Matthias");
+matthias.greet();
 
 // Principle 4
 
 // code example for Explicit Binding
+let job = {
+    position: 'SDE',
+    yearsExp: 5
+};
+
+let jobListing = function (skill1, skill2, skill3) {
+    console.log(`Hiring: ${this.position} - Experience Required: ${this.yearsExp} years - Skills Required: ${skill1}, ${skill2}, ${skill3} - React and Node are a plus.`);
+}
+
+let skills = ['HTML', 'CSS', 'JS'];
+
+jobListing.apply(job, skills);
