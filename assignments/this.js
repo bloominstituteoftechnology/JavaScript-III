d04479569020da338b5676fd62b6d75cc12ac846
Anne Courtney <annecourtney_1@yahoo.com>
2018-04-18 15:29:38 -0500

    almost completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,25 +1,66 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Binding is when the this keyword doesn't have anywhere else to look so it looks to the window by default.
+* 2. Implicit Binding is when the this keyword goes with the object to the left of the dot when called. 
+* 3. Explicit Binding binds the this keyword manually using 'call' 'bind' and 'apply'.
+* 4. New Binding creates a new object and assigns the this keyword to an object.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+// let myobject = () => {
+//     this.name = "Billy";
+// }
+// myobject();
+// console.log(window.name);
+//
+
 
 // Principle 2
 
-// code example for Implicit Binding
+// let myObject = {
+// name: 'Anne',
+// color: 'red',
+// number: 11,
+// speak: function () {
+//  return `I'm an object and my name is ${this.name} and my favorite color is ${this.color} and my favortie number is ${this.number}`
+// } 
+// }
+// console.log(myObject.speak());
+
 
 // Principle 3
 
-// code example for New Binding
+let person = function (haircolor, eyecolor, height) {
+    this.haircolor = haircolor;
+    this.eyecolor = eyecolor;
+    this.height = height;
+    this.sayHi = function () {
+        return `My haircolor is ${this.haircolor} and my eyecolor is ${this.eyecolor} and my height is ${this.height}`
+    }
+}
+let newGuy = new person('brown', 'blue', 5);
+console.log(newGuy.sayHi());
+
+
+
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  
+  newman.speak();
+  newman.speak();
 
 // Principle 4
 
