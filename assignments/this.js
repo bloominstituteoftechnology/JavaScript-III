7080945cb46c369d36bcb241980220a2b825bf38
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-19 01:10:31 +0700

    completed prototypes and finished this questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. The global scope of this is calling on properties of the window object.
+* 2. Explicit - calling from another function to a specified global object
+* 3. Implicit - calling from within object
+* 4. new - building a new object using another objects parameters for creating new objects properties
 *
 * write out a code example of each explanation above
 */
