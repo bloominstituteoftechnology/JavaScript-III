1d1ae276e9071db5a52e93cbc5f4c1ec18e3a1cc
Derrick Mei <derrick.mei.168@gmail.com>
2018-04-18 16:37:49 -0500

    finished this.js

--- assignments/this.js
+++ assignments/this.js
@@ -20,17 +20,45 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
+// window.name = 'chrome'
+// function x () {
+//     console.log(this.name)
+// }
 
 // Principle 2
-
 // code example for Implicit Binding
+let derrick = {
+    name: 'Derrick',
+    age: 30,
+    greet: function () {console.log(`${this.name} says hi!`)
+    }
+}
+derrick.greet(derrick);
+
 
 // Principle 3
-
 // code example for New Binding
+function BabyMaker (baby) {
+    this.name = baby;
+    this.cry = function(){
+        console.log(`${this.name} is crying, please take him away`)
+    }
+}
+
+let mikeBaby = new BabyMaker('Mike');
+
+mikeBaby.cry();
 
 // Principle 4
-
 // code example for Explicit Binding
+let person = {
+    name: 'Cindy',
+    age: '31'
+}
+
+let marriageReady = function () {
+    console.log(`My name is ${this.name}, I am ${this.age} years old and I am ready to get married!`)
+}
+
+marriageReady.call(person);
