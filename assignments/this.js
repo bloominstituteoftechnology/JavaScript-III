0553e6a47f3110a32356295ff32172003756550a
fireinjun <fireinjun291@gmail.com>
2018-04-18 23:20:11 -0400

    Mykeal Kenny
    Changed alot, been playing around to see if I understand all the concepts. Let me know what you think.

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,294 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Window/Global Binding
+ * 2. Implicit Binding
+ * 3. New Binding
+ * 4. Explicit Binding
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
 // code example for Window Binding
+/*function sayName(name) {
+    console.log(this);
+    return name; //?
+}
+sayName("Moonchild");
+
+
 
 // Principle 2
 
 // code example for Implicit Binding
 
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function (name) {
+        console.log(`${this.greeting} my name is ${name}`);
+        console.log(this);
+    }
+};
+myObj.sayHello('Jim');
+
+
 // Principle 3
 
 // code example for New Binding
 
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function () {
+        console.log(this.greeting + this.greeter);
+        console.log(this);
+    };
+}
+
+
 // Principle 4
 
 // code example for Explicit Binding
+*/
+class Student {
+    constructor(obj) {
+        this.firstName = obj.firstName;
+        this.lastName = obj.lastName;
+        this.city = obj.city;
+        this.csClass = obj.csClass;
+        this.wholeName = function () {
+            return `${this.firstName} ${this.lastName}`;
+        };
+    }
+
+}
+
+
+
+Student.prototype.Intro1 = function () {
+    return `HELLO WORLD!!!! \n
+        My name is: ${this.wholeName}! But you can call me: ${this.firstName},\n 
+        or ${this.lastName}, if you have military experience. Whatever works for you! \n
+        I'm from ${this.city}, and I attend Lambda School in ${this.csClass}.\n 
+        I'm testing a simple program I made to test my skills in Javascript lesson III.`
+};
+
+Student.prototype.Intro2 = function () {
+    return `Hey, if you're reading this, it means you had second thoughts. \n
+    I'm ${this.wholeName}, a student here. This can be either the greatest \n
+    or worst experiece in your life. You need to think hard about what you want in life. \n
+    This is not a place for those who second guess, those who can't swallow their pride, \n
+    Those who are too immature to be responsible. This is a place for warriors of code.\n
+    We have all taken some of the biggest risks you can take in life. \n
+    Made sacrifices on the daily that your average 'Joe Citizen' would cry about.\n
+    \n
+    If you're already whining and second guessing, this isn't the place for you.`
+}
+
+class Skills extends Student {
+    constructor(obj) {
+        super(obj);
+        this.skillz = obj.skillz
+    }
+};
+
+Skills.prototype.brag = function () {
+    return `I use these skills to get by: ${this.skillz}.`;
+};
+
+Skills.prototype.lament = function () {
+    return `I have these skills: ${this.skillz} from birth and I still \n 
+    struggle \n 
+    every \n 
+    single \n 
+    day \n
+    here.\n
+    Like I said, forget everything you thought you knew about yourself.\n
+    You are no longer the smartest person in the room, you are just a student now.`;
+};
+
+class Goals extends Skills {
+    constructor(obj) {
+        super(obj);
+        this.goalz = obj.goalz
+    }
+};
+
+Goals.prototype.fakeItTillYouMakeIt = function () {
+    return `I really enjoy it here! \n 
+    I've already learned alot! \n 
+    Programming is more about thinking the right way, not necessarily knowing everything. \n
+    Memorizing can only get you so far as well... \n 
+    By the time I'm done, I hope to know how to ${this.goalz}.`
+};
+
+Goals.prototype.dontfakeit = function () {
+    return `I remember when I had free time, \n 
+    and money, \n
+    and I didn't have nightmares about missing semicolons \(\;\)or  curly braces \(\{\}\).\n 
+    Programming is more about thinking the right way, not necessarily knowing everything. \n
+    But, you will need to know HOW to find whatever you don't know. Google-fu is a sought after skill. \n 
+    By the time I'm done, I hope to know how to ${this.goalz}. \n
+    But at the rate I'm going, I may not even make it out of month 1!`
+};
+
+
+class Setbacks extends Skills {
+    constructor(obj) {
+        super(obj)
+        this.setback = obj.setback
+    }
+};
+
+Setbacks.prototype.pushthroughyourproblems = function () {
+    return `Getting here wasn't easy. I personally had ${this.setback} slow me down.\m
+    But, if I can do it, so can you.`
+};
+
+Setbacks.prototype.yourproblemsaretoogreat = function () {
+    return `Getting here wasn't easy. I personally had ${setback} slow me down.\m
+    And that was probably a walk in the park compared to what you're more than likely going through.\n
+    Maybe you should consider going to a community college instead.\n
+    \n
+    Like I said before. This program is for the best of the best. If you don't have the confidence to even try, \n
+    turn around. There will be 5 people lined up to replace you. Not everyone is worthy of this opportunity.`
+};
+
+
+let genericYouCanDoThisSpeech = function (obj) {
+    return (Student.prototype.Intro1() + Skills.prototype.brag() +
+        Goals.prototype.fakeItTillYouMakeIt() + Setbacks.prototype.pushthroughyourproblems())
+}
+
+let idontreallyknowspeech = function (obj) {
+    return (Student.Intro2() + Skills.lament() +
+        Goals.dontfakeit() + Setbacks.pushthroughyourproblems())
+}
+
+
+
+
+
+
+
+
+
+
+
+const id0001 = new Student ({
+    firstName: `Mykeal`,
+    lastName: `Kenny`,
+    city: `Orlando`,
+    csClass: 10,
+    skillz: [`
+    trained attention to detail`,
+        `undying dedication`,
+        `and ability to admit fault and take responsibility.`
+    ],
+    goalz: [ `to be more than just your run of the mill average developer,`, `to be able to carry my skills into the world and help fix it,`, `and to be taken seriously when I have an idea.`],
+    setback: [`6 kids,`, `pending court case for custody of 1 of them,`,`high school drop-out`, `college drop-out`, `never had anyone believe in me,`, `and no other options left.`]
+});
+
+console.log(genericYouCanDoThisSpeech(id0001))
+
+
+/*#########################################################################################################################################*/
+//For reference
+// console.log(this);
+
+
+// === Principle 1: Window/Global Binding ===
+
+// function sayName(name) {
+//   console.log(this);
+//   return name; //?
+// }
+// sayName("Moonchild");
+
+
+// === Principle 2: Implicit Binding ===
+
+// const myObj = {
+//   greeting: 'Hello',
+//   sayHello: function(name) {
+//     console.log(`${this.greeting} my name is ${name}`);
+//     console.log(this);
+//   }
+// };
+// myObj.sayHello('Jim');
+
+// === Principle 3: New Binding ===
+
+// function CordialPerson(greeter) {
+//   this.greeting = 'Hello ';
+//   this.greeter = greeter;
+//   this.speak = function() {
+//     console.log(this.greeting + this.greeter);
+//     console.log(this);
+//   };
+// }
+
+// const jerry = new CordialPerson('Newman');
+// const newman = new CordialPerson('Jerry');
+
+// newman.speak();
+// newman.speak();
+
+// === Principle 4: Explicit Binding ===
+
+// .call, .apply, .bind
+
+// let sayName = () => {
+//   console.log('My name is: ' + this.name);
+// };
+
+// CALL
+
+// let me = {
+//   name: 'Josh',
+//   age: 33
+// };
+
+// let sayName = function(skill1, skill2, skill3) {
+//   console.log('My name is: ' + this.name + ' I like to program with this stuff: ' + skill1 + ', ' + skill2 + ', ' + skill3 );
+// }
+
+// let skills = ['HTML','CSS','JS'];
+
+// sayName.call(me, skills);
+//sayName();
+//sayName.call(me, ...skills );
+
+// APPLY - Just like call but it will iterate over each item in an array!
+// let me = {
+//   name: 'Josh',
+//   age: 33
+// };
+
+// let sayName = function(skill1, skill2, skill3) {
+//   console.log('My name is: ' + this.name + ' I like to program with this stuff: ' + skill1 + ', ' + skill2 + ', ' + skill3 );
+// }
+
+// let skills = ['HTML','CSS','JS'];
+
+// sayName.apply(me, skills );
+
+// BIND - Returns a function with same functionality of call
+/*
+let me = {
+    name: 'Josh',
+    age: 33
+};
+
+let skills = ['HTML', 'CSS', 'JS'];
+
+
+let sayName = function (skill1, skill2, skill3) {
+    console.log('My name is: ' + this.name + ' I like to program with this stuff: ' + skill1 + ', ' + skill2 + ', ' + skill3);
+}
+
+let newFunction = sayName.bind(me, ...skills);
+
+newFunction(); */
