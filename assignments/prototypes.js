da84b8b34df5346fc8a6e5f6a39b67a07807c942
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-04-26 00:21:46 -0400

    Final Update

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,19 +1,28 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment
+  Object oriented design is commonly used in video games. For this part of the assignment
   you will be implementing several classes with their correct inheritance heirarchy.
 
-  In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.  
-  At the bottom of this file are 3 objects that all inherit from Humanoid.  Use the objects at the bottom of the page to test your classes.
-  
+  In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.
+  At the bottom of this file are 3 objects that all inherit from Humanoid. Use the objects at the bottom of the page to test your classes.
+
   Each class has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
 */
+
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+}
 
 /*
   === CharacterStats ===
@@ -22,6 +31,18 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attributes) {
+  GameObject.call(this, attributes);
+  this.hp = attributes.hp;
+  this.name = attributes.name;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`
+}
 
 /*
   === Humanoid ===
@@ -32,7 +53,20 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(attributes) {
+  CharacterStats.call(this, attributes);
+  this.faction = attributes.faction;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
+
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +75,6 @@
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +135,79 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero classes that inherit from the Humanoid class.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
+// Stretch task:
+// * Create Villian and Hero classes that inherit from the Humanoid class.
+// * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+function Hero(attributes) {
+  Humanoid.call(this, attributes);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+
+Hero.prototype.strike = function(villain, num) {
+  villain.hp = villain.hp - num;
+  if (villain.hp <= 0) {
+    return `${this.name} killed ${villain.name}! ${villain.destroy()}`;
+  }
+  else {
+    return `${this.name} attacked ${villain.name}. ${villain.takeDamage()} ${villain.name} has ${villain.hp} point(s) left.`;
+  }
+}
+
+function Villain(attributes) {
+  Humanoid.call(this, attributes);
+}
+
+Villain.prototype = Object.create(Humanoid.prototype);
+
+Villain.prototype.strike = function(hero, num) {
+  hero.hp = hero.hp - num;
+  if (hero.hp <= 0) {
+    return `${this.name} killed ${hero.name}! ${hero.destroy()}`;
+  }
+  else {
+    return `${this.name} attacked ${hero.name}. ${hero.takeDamage()} ${hero.name} has ${hero.hp} point(s) left.`;
+  }
+}
+
+const ironMan = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 6.1,
+  },
+  hp: 20,
+  name: 'Iron Man',
+  faction: 'The Avengers',
+  weapons: [
+    'Flight',
+    'Repulsor Beams',
+  ],
+  language: 'English',
+});
+
+const thanos = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 6.7,
+  },
+  hp: 50,
+  name: 'Thanos',
+  faction: 'Eternal Colony',
+  weapons: [
+    'Superhuman Strength',
+    'Agility',
+  ],
+  language: 'English',
+});
+
+console.log(thanos.strike(ironMan, 5)); // Thanos attacked Iron Man. Iron Man took damage. Iron Man has 15 point(s) left.
+console.log(ironMan.strike(thanos, 10)); // Iron Man attacked Thanos. Thanos took damage. Thanos has 40 point(s) left.
+console.log(thanos.strike(ironMan, 14)); // Thanos attacked Iron Man. Iron Man took damage. Iron Man has 1 point(s) left.
+console.log(ironMan.strike(thanos, 40)); // Iron Man killed Thanos! Thanos was removed from the game.
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,93 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding
+* 2. Explicit Binding
+* 3. new Binding
+* 4. Window Binding
 *
-* write out a code example of each explanation above
+* Write out a code example of each explanation above
 */
 
 // Principle 1
 
 // code example for Window Binding
 
+let getAge = function() {
+  console.log(this.age);
+};
+
+let myAge = {
+  age: 24
+}
+
+window.age = myAge.age;
+getAge();
+
 // Principle 2
 
 // code example for Implicit Binding
+
+let createMethod = function(obj) {
+  obj.sayName = function() {
+    console.log(this.name);
+  };
+};
+
+let myself = {
+  name: 'Eddy',
+  age: 24
+}
+
+let disguise = {
+  name: 'Mr.Robot',
+  age: 42
+}
+
+createMethod(myself);
+createMethod(disguise);
+
+myself.sayName();
+disguise.sayName();
 
 // Principle 3
 
 // code example for New Binding
 
+let Profile = function(attributes) {
+  this.name = attributes.name;
+  this.age = attributes.age;
+  this.location = attributes.location;
+}
+
+let eddy = new Profile({
+  name: 'Eddy',
+  age: 24,
+  location: 'NY'
+});
+
+eddy.name;
+eddy.age;
+eddy.location;
+
 // Principle 4
 
 // code example for Explicit Binding
+
+let sayName = function(lang1, lang2, lang3) {
+  console.log(`Hello, my name is ${this.name} and I know how to code in ${lang1}, ${lang2} and ${lang3}.`);
+};
+
+let me = {
+  name: 'Eddy',
+  age: 24
+}
+
+let langs = ['HTML', 'CSS', 'JavaScript'];
+
+// sayName.call(me, langs[0], langs[1], langs[2]);
+// sayName.apply(me, langs);
+
+let newFunc = sayName.bind(me, ...langs);
+
+newFunc();
