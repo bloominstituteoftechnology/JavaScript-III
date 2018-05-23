929745d97acbea2a946e7e5b3827716365816ab6
Randy Calderon <randycweb@gmail.com>
2018-05-23 16:05:31 -0700

    Finishing up this

--- assignments/this.js
+++ assignments/this.js
@@ -11,15 +11,39 @@
 
 // Principle 1
 // code example for Window Binding
+function sayYo (greet) {
+    console.log(this);
+    return greet;
+}
 
+sayYo('Hi');
 // Principle 2
 
 // code example for Implicit Binding
+const lifeInsurance = {
+    "name": 'Randy Calderon',
+    "price": function(price) {
+        console.log(`${this.name} paid ${this.price}`);
+        console.log(this);
+    }
+};
 
+lifeInsurance.price(1000);
 // Principle 3
 
 // code example for New Binding
+function results(soccer, basketball) {
+    this.soccer = soccer;
+    this.basketball = basketball;
+    this.teams = function() {
+        console.log(`My favorite soccer team is ${this.soccer} and basketball team is ${this.basketball}.`);
+        console.log(this);
+    };
+}
 
+const favoriteTeams = new results('Chicago Fire', 'Detroit Pistons');
+console.log(favoriteTeams.teams())
 // Principle 4
 
 // code example for Explicit Binding
+
