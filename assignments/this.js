4be15b4b6614366986044144769e238c41852787
Sukhada Gholba <sukhadagholba@gmail.com>
2018-05-23 14:55:46 -0700

    Defined 4 principles for this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. If "this" is used in the global scope, it will refer to the DOM Window object by default. This is called Window Binding
+* 2. When a function is called by attaching the function name with a dot to an object, the object before the dot is "this". This is called Implicit Binding. 
+* 3. When a new object of a constructor function is created, "this" refers to the new instance(object) of the constructor function. This is called New Binding. The word new is used to create an object of the constructor function.
+* 4. When call or apply methods are used on functions,"this" is defined explicitly. This is called Explicit Binding.
 *
 * write out a code example of each explanation above
 */
