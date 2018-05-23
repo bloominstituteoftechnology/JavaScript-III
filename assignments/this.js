ef95e5e5c1109d5bb573ec0c0bcb6b3569dd5446
LBWright <lwright.js@gmail.com>
2018-05-23 15:11:58 -0500

    completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - The default value of the this keyword is the window object. 
+* 2. Implicit Binding - the this keyword binds, by default, to the object that it belongs to. aka it belongs to whatever is to the left of the dot, when invoking a method.
+* 3. New Binding - the new keyword creates a new object with the 'this' keyword bound to all attributes of the invoked object
+* 4. Explicit Binding - we call a method on our object called 'call()' or 'apply()' this is used when you want to call an object or method that is global in scope, but you want to call in the context of a certain object.
 *
 * write out a code example of each explanation above
 */
