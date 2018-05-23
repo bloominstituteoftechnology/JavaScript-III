26f5c532610ec7b91d4262dba1f8c2b361d1c1ea
Munsu <dark_specineff2002@yahoo.com>
2018-05-23 15:50:46 -0700

    Khoa Nguyen JavaScript-III

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - the value of this is the global window object
+* 2. Implicit Binding - the value of this is the object before the dot of the function being invoked
+* 3. New Binding - the value of this is the object when called and returned by a constructor
+* 4. Explicit Binding - the value of this is the object when invoked by the .call or .apply method
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,43 @@
 // Principle 1
 
 // code example for Window Binding
+function sayName(name) {
+    console.log(this);
+    return name;
+  }
+  sayName("D'Artagnan");
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+      console.log(`${this.greeting} my name is ${name}`);
+      console.log(this);
+    }
+  };
+  myObj.sayName('Ryan');
 
 // Principle 3
 
 // code example for New Binding
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  
+  newman.speak();
+  newman.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+??????????
