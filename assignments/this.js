9e64a2309a6d378212bf5ba222ae681b96f4d9db
Simon <Simon_Freelance@hotmail.com>
2018-05-23 16:07:47 -0400

    Principle 3 completed

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. `this` can be used for Window/Global Object Binding. It points to the window object which has many built in methods available to the user. this.a = "dragon" would be the same as window.a = "dragon".
 * 2. Implicit Binding is when you use this inside an object. So if I created a new object and set a name property, I could reference that name using this.name for other properties.
-* 3. 
+* 3. The new binding is useful when you need to create instantiations from a constructor so that `this` points to its new instance everytime.
 * 4. 
 *
 * write out a code example of each explanation above
@@ -33,7 +33,14 @@ console.log(robot.sayInformation());
 // Principle 3
 
 // code example for New Binding
-
+function House(windows, doors, rooms) {
+    this.windows = windows;
+    this.doors = doors;
+    this.rooms = rooms;
+}
+const myHouse = new House(4,2,8); // This instance of house has 4 windows, 2 doors, and 8 roooms.
+const thatHouse = new House(3,2,1); // This instance of house has 3 windows, 2 doors, and 1 roooms.
+console.log("How many rooms does my house have?" + " I have " + myHouse.rooms + " rooms");
 // Principle 4
 
 // code example for Explicit Binding
