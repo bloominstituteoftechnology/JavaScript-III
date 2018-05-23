b6acd4405c54f9bd749faa27ccbd42d4333143f7
Simon <Simon_Freelance@hotmail.com>
2018-05-23 15:50:48 -0400

    Finished first principle for this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,8 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. `this` can be used for Window/Global Object Binding. It points to the window object which has many built in methods available to the user. this.a = "dragon" would be the same as window.a = "dragon".
+* 2.
 * 3. 
 * 4. 
 *
@@ -12,7 +12,11 @@
 // Principle 1
 
 // code example for Window Binding
-
+this.name = "Simon";
+function sayName(name) {
+    console.log(this.name); //this references to the window object so it will output Simon instead of the given argument in sayName
+}
+sayName("William"); //Will not log the name William
 // Principle 2
 
 // code example for Implicit Binding
