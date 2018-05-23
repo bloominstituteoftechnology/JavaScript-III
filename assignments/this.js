fc7a8f13d531752e647294e5eb3f9af26a31f881
Terrance Danckert <tbarnett2020@gmail.com>
2018-05-23 17:26:28 -0400

    Terrance Danckert CS11 (JavaScript-III)

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. New Binding
+* 2. Window Binding
+* 3. Implicit
+* 4. Explicit
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,56 @@
 
 // code example for Window Binding
 
+window.name = "terrance";
+function wind(name) {
+  console.log(this);
+  return name;
+}
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const newObj = {
+  prop1: "prop1",
+  logProp: function() {
+    return this.prop1;
+  }
+};
+console.log(newObj.logProp());
 
 // Principle 3
 
 // code example for New Binding
 
+function Person(attr) {
+  this.name = attr.name;
+  this.age = attr.age;
+  this.speak = function() {
+    return `My name is ${this.name} and I am ${this.age} years old`;
+  };
+}
+const terrance = new Person({
+  name: "terrance",
+  age: 23
+});
+console.log(terrance.speak());
+
 // Principle 4
 
 // code example for Explicit Binding
+
+const terrance = {
+  name: "Terrance",
+  age: 23
+};
+
+const greet = function(skill1, skill2, skill3) {
+  return `Hello my name is ${this.name} and I am ${
+    this.age
+  } and I like to ${skill1}, ${skill2} and ${skill3}`;
+};
+
+const skills = ["lift", "run", "eat"];
+
+console.log(greet.call(terrance, ...skills));
