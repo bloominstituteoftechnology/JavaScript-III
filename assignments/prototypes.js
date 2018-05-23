198708ca5d286805874e7050102adc0a645ec5f1
Wdifulvio523 <w.difulvio523@gmail.com>
2018-05-23 17:31:47 -0400

    Completed prototypes assignment using peer programming

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -10,10 +10,19 @@
   
 /*
   === GameObject ===
+
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
 */
+function GameObject (attributes) {
+  this.createdAt = attributes.createdAt;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+};
 
 /*
   === CharacterStats ===
@@ -22,6 +31,18 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(StatAttributes) {
+  GameObject.call(this, StatAttributes);
+  this.hp = StatAttributes.hp;
+  this.name = StatAttributes.name;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+};
+
+
 
 /*
   === Humanoid ===
@@ -32,6 +53,22 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+function Humanoid(HumanoidStats) {
+  CharacterStats.call(this, HumanoidStats);
+  this.faction = HumanoidStats.faction;
+  this.weapons = HumanoidStats.weapons;
+  this.language = HumanoidStats.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}`;
+}
+
+
+
+
  
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
@@ -41,7 +78,7 @@
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +139,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero classes that inherit from the Humanoid class.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window/Global Binding - Kind of like the default. "this" is bound to the window/console. We may not actually want to do this.
+* 1. Window/Global Binding - Kind of like the default. "this" is bound to the window/console. 
 * 2. Implicit Binding - The function called by the preceeding dot is what "this" is linked to. This is most common.
 * 3. Explicit Binding - using .call .apply and .bind -- .apply does much of the same as .call and iterates through arrays -- .bind creates a new function
 * 4. New Binding - Using a constructor function. Refers to the specific instance of the object that is created and returned by the constructor function.
@@ -9,15 +9,15 @@
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 - Window
 
 function sayName(name) {
     console.log(this.name);
     return name;
   }
-  sayName("Moonchild");
+  sayName("WillD");
 
-// Principle 2
+// Principle 2 - Implicit
 
 const myObj = {
     "greeting": "Hello there",
@@ -28,24 +28,7 @@ const myObj = {
 
   myObj.sayHello("Bill");
 
-// Principle 3
-
-function CordialPerson(greeter) {
-    this.greeting = "Hello";
-    this.greeter = greeter;
-    this.speak = function() {
-       return (this.greeting + " " +this.greeter)
-    }
-  }
-  
-  const jerry = new CordialPerson('Newman');
-  const newman = new CordialPerson('Jerry');
-  
-  
-  jerry.speak(); 
-  newman.speak();
-
-// Principle 4
+// Principle 3 - New 
 
 let me = {
     name: 'Bill',
@@ -68,3 +51,21 @@ sayName.apply(me, skills);
 // BIND
 let newFunction = sayName.bind(me, ...skills);
 newFunction();
+
+// Principle 4 -Explicit
+
+
+function CordialPerson(greeter) {
+    this.greeting = "Hello";
+    this.greeter = greeter;
+    this.speak = function() {
+       return (this.greeting + " " +this.greeter)
+    }
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  
+  
+  jerry.speak(); 
+  newman.speak();
