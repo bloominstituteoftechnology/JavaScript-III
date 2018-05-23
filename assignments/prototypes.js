b587dc6b380fb1975042b2f1c4e2a09049bbc758
Ciara Burkett <ciaraburkett@gmail.com>
2018-05-23 17:32:02 -0400

    Uncommented three classes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -2,12 +2,12 @@
   Object oriented design is commonly used in video games.  For this part of the assignment
   you will be implementing several classes with their correct inheritance heirarchy.
 
-  In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.
   At the bottom of this file are 3 objects that all inherit from Humanoid.  Use the objects at the bottom of the page to test your classes.
-  
+
   Each class has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -32,7 +32,7 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +41,7 @@
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +102,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero classes that inherit from the Humanoid class.  
+
+  // Stretch task:
+  // * Create Villian and Hero classes that inherit from the Humanoid class.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
