9e7ccfe666392d00bd1faf4022832c26507945c4
Adrian Adames <adrianadames@gmail.com>
2018-05-23 18:41:45 -0400

    finished with the code example demonstrating what 'this' refers to when used in a global context

--- assignments/this.js
+++ assignments/this.js
@@ -9,13 +9,21 @@
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 // when used in global context, this refers to the window of the current application being used, whether it be a browser window or an application window like here with VSCode. 
 
 // code example for Window Binding
+function greet(name) {
+    return this;
+}
+// greet("john")
+console.log(greet("johnn"));
+
 
 // Principle 2
 
 // code example for Implicit Binding
+
+const myObj
 
 // Principle 3
 
