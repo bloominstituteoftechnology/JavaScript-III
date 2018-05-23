8899925c25ee946f4561fb0e35bb80eba440ce1d
Simon <Simon_Freelance@hotmail.com>
2018-05-23 16:01:39 -0400

    Principle 2 completed

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. `this` can be used for Window/Global Object Binding. It points to the window object which has many built in methods available to the user. this.a = "dragon" would be the same as window.a = "dragon".
-* 2.
+* 2. Implicit Binding is when you use this inside an object. So if I created a new object and set a name property, I could reference that name using this.name for other properties.
 * 3. 
 * 4. 
 *
@@ -18,7 +18,16 @@ function sayName(name) {
 }
 sayName("William"); //Will not log the name William
 // Principle 2
-
+const robot = {
+    name: "Jerry",
+    id: "4758",
+    dateOfMfg: "January 4, 2018",
+    state: "neutral",
+    sayInformation: function () {
+        return `My name is ${this.name} and my ID number is ${this.id}. I was made on ${this.dateOfMfg}.`
+    }
+};
+console.log(robot.sayInformation());
 // code example for Implicit Binding
 
 // Principle 3
