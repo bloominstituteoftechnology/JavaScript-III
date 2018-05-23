d9797c8106f765cabe40e0447943226b0e7a3f4c
unknown <may.alex43@gmail.com>
2018-05-23 18:54:00 -0400

    Finished the this.js definitions, working on prototypes

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,6 +14,14 @@
   * dimensions
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
 */
+GameObject.prototype.destroy=function(){
+  return 'Object was removed from the game.';
+}
+
+let GameObject= function(createdAt, dimensions, destroy){
+  this.createdAt= createdAt;
+  this.dimensions= dimensions;
+  }
 
 /*
   === CharacterStats ===
@@ -22,6 +30,12 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+let CharacterStats= function(hp, name, takeDamage, destroy){
+  this.hp= hp;
+  this.name= name;
+  this.takeDamage= takeDamage;
+  this.destroy= destroy;
+}
 
 /*
   === Humanoid ===
@@ -41,7 +55,7 @@
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +116,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero classes that inherit from the Humanoid class.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding- the this keyword would reference when/where the function was invoked. Will reference left of the invocation (left of the dot) 
+
+* 2. Explicit Binding- when we explicitly state what the this keyword is referencing when we use .call() or .apply() or .bind(). The this keyword will reference the first argument passed into .call() etc.
+
+* 3. new Binding- When we make a constructor and use the new keyword, this will refer to the new object being constructed.
+
+* 4. window Binding- If none of the 3 conditions above are met, the this keyword will default and reference the window object. 
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +15,46 @@
 // Principle 1
 
 // code example for Window Binding
+let sayName= function(){
+    console.log(this.name);
+}
+person={
+    name:Alex
+}
+sayName();
 
 // Principle 2
 
 // code example for Implicit Binding
+let employee={
+    id:1,
+    name:Alex,
+    age:25,
+    sayName: function(){
+        console.log(this.name);
+    }
+}
+employee.sayName();
 
 // Principle 3
 
 // code example for New Binding
+let Pet = function(species, name, age){
+    this.species= species;
+    this.name= name;
+    this.age= age;
+};
+let dog = new Pet('pit', 'Daisy', '16');
 
 // Principle 4
 
 // code example for Explicit Binding
+let tellName=function(){
+    console.log ('My name is ' + this.name);
+};
+
+let character={
+    name:'Alex',
+    age:25
+};
+tellName.call(character);
