951787bc8b498e78be303bf9305bfdb77524cb55
TangledTessellations <mcevoya@oregonstate.edu>
2018-05-23 14:18:51 -0700

    Completed this.js binding descriptions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,20 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit binding is when the this keyword defaults to referring to the
+      scope in which it is called. If it can't find a proper value to bind to
+      locally, it progresses upwards in scope until it does, or ends up
+      within the global namespace
+* 2. Explicit binding is when we specifically dictate to which scope we'd like
+      the this keyword to bind to. We can do this with .call(), .apply() or
+      .bind()
+* 3. new Binding is when we create an oject using the new keyword and the
+      constructor function makes it so that this refers to the scope within
+      the newly created variable.
+* 4. Window binding is when the this keyword is within the global namespace,
+      which usually has some definition for this, potentially undefined. In this
+      case, the this keyword could refer to the browser window object, or a
+      console window. 
 *
 * write out a code example of each explanation above
 */
