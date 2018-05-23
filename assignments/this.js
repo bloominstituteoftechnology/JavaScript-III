beb050892991e9a59172f4005295ace24b4dda46
DirupT <iamcooled@gmail.com>
2018-05-23 14:43:28 -0500

    finished this.js

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,47 @@
 */
 
 // Principle 1
-
+function sayName(name) {
+    console.log(this);
+    return name;
+}
 // code example for Window Binding
 
 // Principle 2
+const greetName = {
+    "greeting": "Hello,",
+    "sayName": function(name) {
+      console.log(`${this.greeting} ${name}`)},
+}
 
 // code example for Implicit Binding
 
 // Principle 3
+function SayName(name) {
+    this.greeting = "Hello,";
+    this.name = name;
+    this.sayName = function() {
+      console.log(`${this.greeting} ${this.name}`);
+    };
+}
+
+const brandon = new SayName('Brandon');
+
+brandon.sayName();
 
 // code example for New Binding
 
 // Principle 4
+let age = {
+    "age": 30,
+}
+
+function sayNames(name1, name2, name3) {
+    console.log(`Hey, ${name1}, ${name2}, and ${name3} are ${this.age} years old.`);
+  }
+  
+  let names = ['Brandon', 'Jess', 'Dan'];
+  
+  sayNames.call(age, ...names);
 
 // code example for Explicit Binding
