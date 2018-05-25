dc449abb548306c7c7aa09b5c61148dfd60a25f6
Nicky Chen <nicky.chen0@gmail.com>
2018-05-24 21:03:43 -0700

    modified stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,55 +7,70 @@
   
   Each class has unique properites and methods that are defined in their block comments below:
 */
-  
-
-  function GameObject(attributes) {
+class GameObject {
+  constructor(attributes) {
     this.createdAt = attributes.createdAt;
     this.dimensions = attributes.dimensions;
   }
-  
-  GameObject.prototype.destroy = function() {
-    return this.name + ' was removed from the game.'
+  destroy() {
+    return `${this.name} was removed from the game.`; // prototype method -> returns the string 'Object was removed from the game.'
   }
-// prototype method -> returns the string 'Object was removed from the game.'
-
-
-
-
-  function CharacterStats (obj) {
-    this.hp = obj.hp;
-    this.name = obj.name;
-    GameObject.call(this, obj);
-  }
-  CharacterStats.prototype = Object.create(GameObject.prototype);
-  CharacterStats.prototype.takeDamage = function() {
-    return this.name + ' took damage.';
-  }
- 
-
-// prototype method -> returns the string '<object name> took damage.'
- // * should inherit destroy() from GameObject's prototype
-
- 
-
-
-function Humanoid (obj) {
-  this.faction = obj.faction;
-  this.weapons = obj.weapons;
-  this.language = obj.language;
-  CharacterStats.call(this, obj);
 }
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-Humanoid.prototype.greet = function() {
-  return this.name + ' offers a greeting in ' + this.language;
+
+class CharacterStats extends GameObject {
+  constructor(attributes) {
+    super(attributes);
+    this.hp = attributes.hp;
+    this.name = attributes.name;
+  }
+  takeDamage() {
+    return `${this.name} took damage.`;
+  }
 }
+
+class Humanoid extends CharacterStats {
+  constructor(attributes) {
+    super(attributes);
+    this.faction = attributes.faction;
+    this.weapons = attributes.weapons;
+    this.language = attributes.language;
+  }
+  greet() {
+    return `${this.name} offers a greeting in ${this.language}`;
+  }
+}
+
+class Villain extends Humanoid {
+  constructor(attributes) {
+    super(attributes);
+  }
+  attacks(character) {
+    character.hp -= 5;
+    if (character.hp <= 0) {
+      return (`${this.name} attacks, ${character.name}'s hp is now 0. ${character.name} faints.`);
+    } else {
+      return (`${this.name} attacks, ${character.name}'s hp is now: ${character.hp}`);
+    }
+  }
+}
+
+class Hero extends Humanoid {
+  constructor(attributes) {
+    super(attributes);
+  }
+  heals(character) {
+    character.hp += 5;
+    return (`${this.name} heals ${character.name}. ${character.name}'s hp is now: ${character.hp}`);
+  }
+}
+
 
 
 //* greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  //should inherit destroy() from GameObject through CharacterStats
-  //should inherit takeDamage() from CharacterStats
+//should inherit destroy() from GameObject through CharacterStats
+//should inherit takeDamage() from CharacterStats
 
- 
+
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -65,81 +80,40 @@ Humanoid.prototype.greet = function() {
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Bruce',
-    faction: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Toungue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Bruce',
+  faction: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Toungue',
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    hp: 15,
-    name: 'Sir Mustachio',
-    faction: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Toungue',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  hp: 15,
+  name: 'Sir Mustachio',
+  faction: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Toungue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    hp: 10,
-    name: 'Lilith',
-    faction: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
-
-  // Stretch task: 
-  // * Create Villian and Hero classes that inherit from the Humanoid class.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
-
-  function Villain(obj) {
-    Humanoid.call(this, obj);
-
-  }
-  Villain.prototype = Object.create(Humanoid.prototype);
-  Villain.prototype.attacks = {}
-
-const newVillain = new Villain ({
+const archer = new Humanoid({
   createdAt: new Date(),
   dimensions: {
     length: 1,
@@ -154,6 +128,64 @@ const newVillain = new Villain ({
     'Dagger',
   ],
   language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.hp); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.faction); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+// Stretch task: 
+// * Create Villian and Hero classes that inherit from the Humanoid class.  
+// * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+
+
+const syndrome = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  hp: 10,
+  name: 'Syndrome',
+  faction: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
 })
 
-console.log(newVillain.greet())
+const mrIncredible = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  hp: 10,
+  name: 'Mr. Incredible',
+  faction: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'English',
+})
+
+
+
+console.log(syndrome.attacks(archer))
+console.log(mrIncredible.heals(archer)) 
+console.log(syndrome.attacks(mage))
+console.log(syndrome.attacks(swordsman))
