ab3a8f06dc1df259de175bbeb5636a6421e24697
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-06-27 16:48:19 -0600

    Finished

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,19 @@
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
 */
 
+/*  ====================== define constructor function=========  */
+
+function GameObject(objectAttributes) {
+  //Properties
+  this.createdAt = objectAttributes.createdAt;
+  this.dimensions = objectAttributes.dimenstions;
+}
+
+//Methods
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`;
+}
+
 /*
   === CharacterStats ===
   * hp
@@ -22,6 +35,20 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(characterAttributes) {
+  GameObject.call(this, characterAttributes); //this inherits from GameObject Attributes
+  this.hp = characterAttributes.hp;
+  this.name = characterAttributes.name;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+//This line is I think what inherits methods?!?!
+//New methods go below this line.
+
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage.`;
+}
 
 /*
   === Humanoid ===
@@ -33,6 +60,19 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(humanoidAttributes) {
+  CharacterStats.call(this, humanoidAttributes);
+  this.faction = humanoidAttributes.faction;
+  this.weapons = humanoidAttributes.weapons;
+  this.language = humanoidAttributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}`;
+}
+
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +81,7 @@
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +142,83 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+  function Villian(villianAttributes) {
+    Humanoid.call(this, villianAttributes);
+    this.spells = villianAttributes.spells;
+  }
+
+  Villian.prototype = Object.create(Humanoid.prototype);
+
+  Villian.prototype.cast = function () {
+    return `${this.name} casts a ${this.spells} spell.`;
+  }
+
+  function Hero(heroAttributes) {
+    Humanoid.call(this, heroAttributes);
+    this.abilities = heroAttributes.abilities;
+  }
+
+  Hero.prototype = Object.create(Humanoid.prototype);
+
+  Hero.prototype.ability = function () {
+    return `${this.name} uses a ${this.abilities} ability.`;
+  }
+
+  const giant = new Villian({
+    createdAt: new Date(),
+    dimensions: {
+      length: 4,
+      width: 2,
+      height: 8,
+    },
+    hp: 25,
+    name: 'Forest Giant',
+    faction: 'Giant Clan',
+    weapons: [
+      'Club',
+      'Fist',
+    ],
+    language: 'Giantish',
+    spells: [
+      'curse',
+      'mock',
+    ],
+  });
+
+  const barbarian = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 5,
+      width: 3,
+      height: 6,
+    },
+    hp: 20,
+    name: 'Connan',
+    faction: 'Barbarian Clan',
+    weapons: [
+      'sword',
+      'sheild',
+    ],
+    language: 'Barbarish',
+    abilities: 'defense',
+  });
+
+
+  console.log(giant.createdAt);
+  console.log(barbarian.createdAt);
+  console.log(barbarian.hp);
+  console.log(giant.name);
+  console.log(giant.faction);
+  console.log(barbarian.weapons);
+  console.log(giant.language);
+  console.log(barbarian.greet());
+  console.log(giant.takeDamage());
+  console.log(giant.cast());
+  console.log(barbarian.ability());
