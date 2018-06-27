76a698a9a8f4dd9f27d1ab2662a51389fcf08cc1
William <kwontaewoongah@gmail.com>
2018-06-27 14:46:33 -0700

    finished this.js

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
 *
 * 1. default binding-this points to the global object, in browsers this is window.  When the this keyword does not have a containing object, constructor function or bind, call, apply affiliations.
 * 2. Implicit binding- the function referencing the this.x variable is contained inside an object.  
-* 3. The this variable is passed off due to a function with bind, call, or apply.  
-* 4. Part of a constructor function called with new.  
+* 3. Explicit Binding-The this variable is passed off to a function with bind, call, or apply as a parameter may be an object.  
+* 4. New Binding-This points to the object passed into the constructor function at time of new invocation.
 *
 * write out a code example of each explanation above
 */
