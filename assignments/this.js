cab137c2361e574789cb20eace67c9a47d3394de
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-06-27 17:26:06 -0400

    opening pull

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding
+* 2. Implict Binding
+* 3. Explicit Binding
+* 4. New Binding
 *
 * write out a code example of each explanation above
 */
