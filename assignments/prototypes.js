80fa0a675ae5655e8b3ea6e27b27371b81e2674f
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-06-27 18:22:48 -0400

    Stretch Complete

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -44,7 +44,10 @@ function CharacterStats(attr) {
 CharacterStats.prototype = Object.create(GameObject.prototype);
 
 CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`;
+  let damage = Math.floor(Math.random() * 5);
+  this.hp -= damage;
+  return `${this.name} took ${damage} damage. They are at ${this.hp} health!`;
+
 }
 
 /*
@@ -143,3 +146,82 @@ Humanoid.prototype.greet = function() {
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+  function fightItOut(hero, villian){
+    console.log(`${villian.name} is attempting to commit a felony!`);
+    while(hero.hp > 0 && villian.hp > 0){
+      console.log(villian.commitCrime());
+      console.log(hero.takeDamage());
+      console.log(hero.foilRobbery());
+      console.log(villian.takeDamage());
+    }
+    if(hero.hp > 0){
+      console.log(`${hero.name} has defeated ${villian.name}!`);
+    }else if(this.rival.hp > 0){
+      console.log(`$villian.name} has gotten away with his ill gotten gains!`);
+    }else {
+      console.log('Something effed up somewhere...');
+    }
+  }
+
+  function Villian(attr) {
+    Humanoid.call(this, attr);
+    this.enemy = attr.enemy;
+  }
+
+  Villian.prototype = Object.create(Humanoid.prototype);
+
+  Villian.prototype.commitCrime = function() {
+    let comeBacks = ['Nothing can stop me!', 'Come to my aid Minions!', ':evilcackle:'];
+    return comeBacks[Math.floor(Math.random()*comeBacks.length)];
+  }
+
+  function Hero(attr) {
+    Humanoid.call(this, attr);
+    this.rival = attr.rival;
+  }
+
+  Hero.prototype = Object.create(Humanoid.prototype);
+
+  Hero.prototype.foilRobbery = function() {
+    let heroActions = ['Stop, Evil-Doer!', 'Nothing can stop Lady Justice!', ':heroicpose:'];
+    return heroActions[Math.floor(Math.random()*heroActions.length)];
+  }
+
+  const deadBanshee = new Villian({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 30,
+    name: 'The Death Banshee',
+    faction: 'Association of Evil-Doers',
+    weapons: [
+      'Scream',
+      'Ghost Powers',
+    ],
+    language: 'English',
+    enemy: '',
+  });
+
+  const shiningKnight = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 35,
+    name: 'The Shining Knight',
+    faction: 'J.O.O.D.',
+    weapons: [
+      'Plate Armor',
+      'Light Sword',
+    ],
+    language: 'English',
+    rival: '',
+  });
+
+  fightItOut(shiningKnight, deadBanshee);
