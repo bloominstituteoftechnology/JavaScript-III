c926cdfc30c18a8be9b9d2f2aa7cc96c46b44e81
AI2EJAY <JHammett1314@gmail.com>
2018-06-27 16:56:19 -0500

    Half of this.js done

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window Binding
+* 1. Window Binding is when you are binding outside of any scope that would bind to the most global object aka The window
 * 2. Implicit Binding is when you look to the left of the dot in the function is invoked.
 * 3. Explicit Binding
 * 4. New Binding
@@ -13,26 +13,37 @@
 
 // code example for Window Binding
 
+function example(i) {
+    this.i = "Ball"
+    console.log(this.i)
+}
+
+example();
+
+
+
 // Principle 2
 
 // code example for Implicit Binding
-function Human(traits) {
-    this.name = traits.name
-    this.age = traits.age
-    this.gender = traits.gender
-    this.eat = function () {
-        console.log('${this.name} scoffs down their meal')
+
+const Justin = {
+    name : "Justin",
+    age : "26",
+    gender : "M",
+    
+    eats : function(name) {
+        console.log(`${this.name} scoffs down their meal`);
+    }
 }
 
-const justin = {
-    name = "Justin",
-    age = 26,
-    gender = "M",
-}
+Justin.eats()
+
+
 
 // Principle 3
 
 // code example for New Binding
+
 
 // Principle 4
 
