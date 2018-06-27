a3fce42b3b9df17870df62058dd79bb8ace1388d
William <kwontaewoongah@gmail.com>
2018-06-27 15:13:35 -0700

    finished prototypes.js sans stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,7 +14,11 @@
   * dimensions
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
 */
-
+function GameObject(param) {
+  this.createdAt=param.createdAt;
+  this.dimensions=param.dimensions;
+}
+GameObject.prototype.destroy=function() {return 'Object was removed from the game';};
 /*
   === CharacterStats ===
   * hp
@@ -22,7 +26,13 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-
+function CharacterStats(param) {
+  GameObject.call(this,param)
+  this.hp=param.hp;
+  this.name=param.name;
+}
+CharacterStats.prototype=Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage=function(){return `${this.name} took damage.`;}
 /*
   === Humanoid ===
   * faction
@@ -32,6 +42,14 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid(param) {
+  CharacterStats.call(this,param);
+  this.faction=param.faction;
+  this.weapons=param.weapons;
+  this.language=param.language;
+}
+Humanoid.prototype=Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet=function(){return `${this.name} offers a greeting in ${this.language}`};
  
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
@@ -41,7 +59,7 @@
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +120,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
