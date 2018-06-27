f0f9bfc8def18eac7c623e967e3b0c66870e25fd
Cwright0428 <cwright0428@yahoo.com>
2018-06-27 16:36:22 -0500

    couple questions answered

--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,16 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. implicit binding - look to the left of the " . " when you call/invoke the function
+* 2. Explicit binding - using "call" "apply" or "bind" to tell the "this" keyword it's context  
+* 3. new binding - "new" keyword is bound to new object being constructed
+* 4. window binding - if none of the other types apply "this" defaults to the viewport (window object)
 *
 * write out a code example of each explanation above
 */
+
+
+
 
 // Principle 1
 
@@ -17,10 +20,42 @@
 
 // code example for Implicit Binding
 
+const MyObject = function (){
+    this.name = 'MyObjectName';
+    this.myProperty = 'property';
+  };
+ 
+  MyObject.prototype.doStuff = function (action) {
+    console.log(this.name + ' is ' + action + '!');
+  }
+ 
+  var obj = new MyObject();
+ 
+  obj.doStuff('awesome'); // prints 'MyObjectName is awesome!'
+
+
+
 // Principle 3
 
 // code example for New Binding
 
+function TagLine(who) {
+    this.what = "Iam";
+    this.why = who;
+    this.speak = function() {
+      console.log( this.what + ' ' + this.why)
+    }
+  }
+  
+  const josh = new TagLine('DrumLife');
+  
+
+  josh.speak();
+
+
 // Principle 4
 
 // code example for Explicit Binding
+
+const drummer = { name: 'John', myFavoriteActivity: 'drumming' };
+MyObject.prototype.doStuff.call(drummer, drummer.myFavoriteActivity);
