a503ea5c5438f60177b363203bc0d8b5832c622c
jordan-massingill <jordan_massingill@hotmail.com>
2018-06-27 16:32:38 -0500

    This Keyword
    Answered text questions
    Added examples of the 4 kinds of this keyword binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: when the "this" keyword isn't bound to anything in particular,
+    it binds to the Window object.
+* 2. Implicit Binding: When the "this" keyword is bound to the object referred to
+    on its left in dot notation when called.
+* 3. new Binding: When creating new objects using a constructor function and the
+    "new" keyword, the "this" keyword is bound to that new object.
+* 4. Explicit Binding: Directing the "this" keyword to bind to a particular
+    object using the .call(), .apply(), or .bind() methods on a function containing
+    "this".
 *
 * write out a code example of each explanation above
 */
@@ -12,14 +17,34 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this.name);
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObject = {
+  "fee": 14,
+  "fi": "Raquetball",
+  "fo": "Spanish",
+  "fum": function () {return `Hello! I am ${this.fee} years old, I love ${this.fi} and my family all speak ${this.fo}.`}
+}
+
+myObject.fum();
 
 // Principle 3
 
 // code example for New Binding
+function MythicalCreature(param) {
+  this.species: param.species,
+  this.loves: param.loves,
+  this.hates: param.hates
+}
+
+const jackalope = new MythicalCreature({
+  species: "jackalope",
+  loves: "pizzaberries",
+  hates: "crocs"
+});
 
 // Principle 4
 
