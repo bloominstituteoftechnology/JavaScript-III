602982014ae34078e189eea410ccd8327ab25e7b
sca4cs <shannon.atkinson.ca@gmail.com>
2018-06-27 17:16:21 -0400

    this keyword practice

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. window
-* 2. implicit
-* 3. explicit
-* 4. new
+* 1. window - 'this' refers to the window when there is no object to point to
+* 2. implicit - 'this' refers to the object from within the object
+* 3. explicit - 'this' refers to the object from outside the object
+* 4. new - 'this' refers to an object created by a constructor function
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,44 @@
 // Principle 1
 
 // code example for Window Binding
+this.name = "my window example";
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObject = {
+        'greeting': 'Hello',
+        'sayHello': function(name) {
+        console.log(`${this.greeting} my name is: ${name}`);
+       }
+     }
+
+ myObject.sayHello("Shannon");
 
 // Principle 3
 
 // code example for New Binding
+function CandyFactory(flavor) {
+    this.flavor = flavor;
+    this.speak = function() {
+        console.log(`${this.flavor} is my favorite flavor of candy.`)
+    }
+}
+
+const susie = new CandyFactory('Cherry');
+
+susie.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+const myObject = {
+      'name': 'Shannon',
+      'age': 24
+    }
+    
+function sayName() {
+    console.log(`My name is: ${this.name}`);
+}
+
+sayName.call(myObject);
