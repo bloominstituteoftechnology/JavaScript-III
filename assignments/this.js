d2a6d2f6433b6c52a0595e1c8a1a350e5dde9ae7
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-06-27 13:38:05 -0700

    add answers to this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window
+* 2. Implicit
+* 3. Explicit
+* 4. New
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,51 @@
 // Principle 1
 
 // code example for Window Binding
+// console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+// const myObj = {
+//   greeting: 'Hello',
+//   sayHello: function(name) {
+//     console.log(`${this.greeting} my name is ${name}`);
+//     console.log(this);
+//   }
+// };
+// myObj.sayHello('Ryan');
 
 // Principle 3
 
 // code example for New Binding
+// function CordialPerson(greeter) {
+//   this.greeting = 'Hello ';
+//   this.greeter = greeter;
+//   this.speak = function() {
+//     console.log(this.greeting + this.greeter);
+//     console.log(this);
+//   };
+// }
+
+// const jerry = new CordialPerson('Newman');
+// const newman = new CordialPerson('Jerry');
+
+// newman.speak();
+// newman.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+// function CordialPerson(greeter) {
+//   this.greeting = 'Hello ';
+//   this.greeter = greeter;
+//   this.speak = function() {
+//     console.log(this.greeting + this.greeter);
+//     console.log(this);
+//   };
+// }
+
+// const jerry = new CordialPerson('Newman');
+// const newman = new CordialPerson('Jerry');
+// jerry.speak.call(newman); 
+// newman.speak.apply(jerry);
