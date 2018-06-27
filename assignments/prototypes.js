fa7e1bee0eb622077f82990a1f88ae58ee9a4bc5
Lauren <llwor94@gmail.com>
2018-06-27 15:29:48 -0700

    added some method calls within methods, made it so that .takeDamage decreases hp points until it gets to 0, then the object is destroyed. added an array of all of the players that are still alive and removed them from the array once they are destroyed, added communication between objects in the greeting method

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,14 +14,13 @@
   * dimensions
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
 */
+let alivePlayers = [];
+
 function GameObject(attributes) {
   this.createdAt = attributes.createdAt;
   this.dimensions = attributes.dimensions;
 }
 
-GameObject.prototype.destroy = function () {
-  return 'Object was removed from the game.';
-};
 /*
   === CharacterStats ===
   * hp
@@ -33,11 +32,25 @@ function CharacterStats(attributes) {
   GameObject.call(this, attributes);
   this.hp = attributes.hp;
   this.name = attributes.name;
+  alivePlayers.push(this.name);
 }
 
 CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.destroy = function () {
+  alivePlayers = alivePlayers.filter(player => player !== this.name);
+  
+  return `${this.name} was removed from the game.`;
+};
+
 CharacterStats.prototype.takeDamage = function () {
-  return `${this.name} took damage.`;
+  this.hp -= 2;
+  if (this.hp > 0) {
+    return `${this.name} took damage. Their health points have been decreased to ${this.hp}.`;
+  } else {
+    return this.destroy();
+  }
+  
 };
 /*
   === Humanoid ===
@@ -56,9 +69,36 @@ function Humanoid(attributes) {
 }
 
 Humanoid.prototype = Object.create(CharacterStats.prototype);
-Humanoid.prototype.greet = function () {
-  return `${this.name} offers a greeting in ${this.language}`;
+Humanoid.prototype.greet = function (character) {
+  return `${this.name} offers a greeting to ${character.name} in ${this.language}.\n` + character.acceptGreeting(this);
 };
+Humanoid.prototype.acceptGreeting = function (greeter){
+  if (greeter.language === this.language) {
+    return `${this.name} accepts and returns ${greeter.name}'s greeting.`
+  } else {
+    return `${this.name}: I do not understand you. You must be an enemy. I must attack you now.\n` + greeter.takeDamage();
+  }
+}
+
+function Villian(attributes) {
+  Humanoid.call(this, attributes);
+}
+
+Villian.prototype = Object.create(Humanoid.prototype);
+Villian.prototype.attack = function (victim){
+  return victim.takeDamage() + (`\n${this.name}: Muahaha! My evil plan is working!`);
+}
+
+function Hero(attributes) {
+  Humanoid.call(this, attributes);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+Hero.prototype.rescue = function (victim) {
+  victim.hp += 2;
+  return(`${victim.name}'s health points has been restored to ${victim.hp}! ${this.name} to the rescue!`)
+}
+
 /*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -118,48 +158,6 @@ Humanoid.prototype.greet = function () {
     language: 'Elvish',
   });
 
-  
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-  
-
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
-
-  function Villian(attributes) {
-    Humanoid.call(this, attributes);
-  }
-
-  Villian.prototype = Object.create(Humanoid.prototype);
-  Villian.prototype.attack = function (victim) {
-    victim.hp -= 2;
-    if (victim.hp > 0) {
-      return(`${victim.name}'s health points have decreased to ${victim.hp}! Muahaha!`)
-    } else {
-      return victim.destroy();
-    }
-  }
-
-  function Hero(attributes) {
-    Humanoid.call(this, attributes);
-  }
-
-  Hero.prototype = Object.create(Humanoid.prototype);
-  Hero.prototype.rescue = function (victim) {
-    victim.hp += 2;
-    return(`${victim.name}'s health points has been restored to ${victim.hp}! ${this.name} to the rescue!`)
-  }
-
   const villian = new Villian({
     createdAt: new Date(),
     dimensions: {
@@ -193,8 +191,31 @@ Humanoid.prototype.greet = function () {
     language: 'Common Tongue',
   });
 
-  console.log(villian.attack(mage));
-  console.log(hero.rescue(mage));
-  console.log(villian.attack(mage));
-  console.log(villian.attack(mage));
-  console.log(villian.attack(mage));
+  console.log(alivePlayers);
+  console.log('1: ' + mage.createdAt); // Today's date
+  console.log('2: ' + archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log('3: ' + swordsman.hp); // 15
+  console.log('4: ' + mage.name); // Bruce
+  console.log('5: ' + swordsman.faction); // The Round Table
+  console.log('6: ' + mage.weapons); // Staff of Shamalama
+  console.log('7: ' + archer.language); // Elvish
+  console.log('8: ' + archer.greet(swordsman)); // Lilith offers a greeting in Elvish.
+  console.log(mage.greet(swordsman));
+  console.log('9: ' + mage.takeDamage()); // Bruce took damage.
+  console.log('10: ' + swordsman.destroy()); // Sir Mustachio was removed from the game.
+  
+
+  // Stretch task: 
+  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+  
+  
+  console.log(alivePlayers);
+  console.log('\n 11: ' + villian.attack(mage));
+  console.log('\n 12: ' + hero.rescue(mage));
+  console.log('\n 13: ' + villian.attack(mage));
+  console.log('\n 14: ' + villian.attack(mage) + '\n');
+  console.log(alivePlayers);
+  
