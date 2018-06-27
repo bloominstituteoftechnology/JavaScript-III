d65f1a2ff1a02ae3c98036c2b121ee92312d8a4e
tulf <timzulf@gmail.com>
2018-06-27 18:51:28 -0400

    finished prototypes, mystified as to how to change the actual object properties. No stretch yet.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -2,12 +2,12 @@
   Object oriented design is commonly used in video games.  For this part of the assignment
   you will be implementing several classes with their correct inheritance heirarchy.
 
-  In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three classes: GameObject, CharacterStats, Humanoid.
   At the bottom of this file are 3 objects that all inherit from Humanoid.  Use the objects at the bottom of the page to test your classes.
-  
+
   Each class has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -19,7 +19,7 @@
   === CharacterStats ===
   * hp
   * name
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.' ${this.name}
   * should inherit destroy() from GameObject's prototype
 */
 
@@ -29,19 +29,48 @@
   * weapons
   * language
   * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit destroy() from GameObject through CharacterStats object.create
   * should inherit takeDamage() from CharacterStats
 */
- 
-/*
-  * Inheritance chain: Humanoid -> CharacterStats -> GameObject
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+
+function GameObject(game) {
+  this.createdAt = game.createdAt;
+  this.dimensions = game.dimensions
+}
+
+GameObject.prototype.destroy = function() {
+  return 'Sir Mustachio was removed from the game.'
+}
+
+function CharacterStats(char) {
+  GameObject.call(this, char);
+  this.hp = char.hp;
+  this.name = char.name
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`
+}
+
+
+function Humanoid(human) {
+  CharacterStats.call(this, human);
+  this.faction = human.faction;
+  this.weapons = human.weapons;
+  this.language = human.language
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}`
+}
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +131,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero classes that inherit from the Humanoid class.  
+
+  // Stretch task:
+  // * Create Villian and Hero classes that inherit from the Humanoid class.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
