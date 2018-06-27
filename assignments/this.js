98b4d613680ff23c4b7ecc862716d3d8f13bac09
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-06-27 18:50:06 -0400

    completed all

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
 *
 * 1. Window Binding - The value of 'this' references the window Object.
 * 2. Implict Binding - When the function is invoked 'this' referes to what is on the left of the dot. 
-* 3. Explicit Binding - 
-* 4. New Binding - 
+* 3. New Binding - Used with constructor functions. Defines 'this' as the new instance created from the constructor.  
+* 4. Explicit Binding - Whenever .call/.apply/.bind are called. These methods explicity tell you what the 'this' keyword will be in a given function. 
 *
 * write out a code example of each explanation above
 */
@@ -19,7 +19,7 @@ windowBinding();
 
 // Principle 2
 // code example for Implicit Binding
-const implicitObj = {
+function implicitObj() {
     greeting: 'Hello',
     greetMe: function (name) {
         console.log(`${this.greeting} ${name}`);
@@ -29,7 +29,21 @@ implicitObj.greetMe('Jurgen');
 
 // Principle 3
 // code example for New Binding
+function Student(name, class, major) {
+    this.name = name; 
+    this.class = class;
+    this.major = major;
+};
+let jurgen = new Student('Jurgen', 'CS12', 'Engineering');
 
 
 // Principle 4
 // code example for Explicit Binding
+function explicitBinding() {
+    console.log(`${this.name}`);
+}
+let jurgen = {
+    name: 'Jurgen'
+}
+explicitBinding.call(jurgen);
+
