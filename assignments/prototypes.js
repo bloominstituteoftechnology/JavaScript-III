a01b74d7edba382c6458a739b0b96c204f2a246d
Charlie Winslow <cwinslow22@yahoo.com>
2018-06-27 17:24:59 -0500

    Starting prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -39,6 +39,8 @@
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+
+
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
 /*
