2136db58d8be2d9419d546f62a4ead2dd181fecf
AI2EJAY <JHammett1314@gmail.com>
2018-06-27 17:44:00 -0500

    this.js Completed

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
 *
 * 1. Window Binding is when you are binding outside of any scope that would bind to the most global object aka The window
 * 2. Implicit Binding is when you look to the left of the dot in the function is invoked.
-* 3. Explicit Binding
-* 4. New Binding
+* 3. Explicit Binding is where you call "this" using the function methods .call , .apply , and .bind.
+* 4. New Binding is where you link the New variable to the parent via using "new ParentName". That way you can keep the features of the parent on the child
 *
 * write out a code example of each explanation above
 */
@@ -26,7 +26,7 @@ example();
 
 // code example for Implicit Binding
 
-const Justin = {
+const justin = {
     name : "Justin",
     age : "26",
     gender : "M",
@@ -36,15 +36,44 @@ const Justin = {
     }
 }
 
-Justin.eats()
+justin.eats()
 
 
 
 // Principle 3
 
 // code example for New Binding
+function Human(prop1, prop2, prop3) {
+    this.name = prop1;
+    this.age = prop2;
+    this.gender = prop3;
+    this.sleep = function() {
+        console.log(`${this.name} falls asleep zzzZZZzzzZZZZzzz`);
+    }
+};
+
+const matt = new Human("Matt", 24, "M")  
+const brandon = new Human("Brandon", 24, "M")  
+
+matt.sleep()
+brandon.sleep()
+
+
+
 
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const reptar = {
+    name : "Reptar",
+    age : "4564132",
+    gender : "M",
+}
+
+function rawr(name) {
+    console.log(`${this.name} says RAWRRR!`);
+}
+
+rawr.call(reptar)
