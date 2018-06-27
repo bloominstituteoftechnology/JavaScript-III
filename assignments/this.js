876237b3309a5bf0de5bc419953045e6dbc9e62a
Carey <careycbaldwin@gmail.com>
2018-06-27 14:19:12 -0400

    initial commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding involves a scenario in which the 'this' keyword has no objects or functions to reference
+other than the entire window (the all encompassing object where the code is being written)
+* 2. Implicit Binding
+* 3. New Binding
+* 4. Explicit Binding
 *
 * write out a code example of each explanation above
 */
