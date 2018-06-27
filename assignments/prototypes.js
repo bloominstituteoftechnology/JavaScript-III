e9daf9add17fbfad2a33b5a03a62c46f280cbbc3
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-06-27 17:27:02 -0400

    finished this.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,29 +1,27 @@
-
+/*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
-
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
-
   At the bottom of this file are 3 objects that all inherit from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
 
   Each constructor function has unique properites and methods that are defined in their block comments below:
+*/
 
-
-
+/*
   === GameObject ===
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string 'Object was removed from the game.'
+*/
 
-
-
+/*
   === CharacterStats ===
   * hp
   * name
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
+*/
 
-
-
+/*
   === Humanoid ===
   * faction
   * weapons
@@ -31,13 +29,13 @@
   * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
+*/
 
-
-
+/*
   * Inheritance chain: Humanoid -> CharacterStats -> GameObject
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
   * Instances of CharacterStats should have all of the same properties as GameObject.
-
+*/
 
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
@@ -57,7 +55,6 @@
     ],
     language: 'Common Toungue',
   });
-
   const swordsman = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -74,7 +71,6 @@
     ],
     language: 'Common Toungue',
   });
-
   const archer = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -91,7 +87,6 @@
     ],
     language: 'Elvish',
   });
-
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.hp); // 15
@@ -102,7 +97,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
+*/
 
   // Stretch task:
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
