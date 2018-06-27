c6d6241d5d2b8005903923a55f1a5fca33460548
Grant Reighard <grantreighard@Grants-iMac.fios-router.home>
2018-06-27 16:43:56 -0400

    Initial commit.
    - Finished four principles of new.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window/global: used in a global scope, "this" refers to the window object
+* 2. implicit: when a method is called in an object.method context, "this" refers to the object to the left of the method
+* 3. explicit: "this" is bound using .call, .apply, or .bind
+* 4. new: when creating an object with an object creator function, "this" refers to the new object
 *
 * write out a code example of each explanation above
 */
