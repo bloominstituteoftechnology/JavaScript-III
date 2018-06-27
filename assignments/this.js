451ff43469689a43229ba3ee9a222063df06f2b6
Sean <sean47900@gmail.com>
2018-06-27 15:53:50 -0500

    Started questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window 
+* 2. new 
+* 3. implicit
+* 4. explicit
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,8 @@
 // Principle 1
 
 // code example for Window Binding
+
+console.log(this);
 
 // Principle 2
 
