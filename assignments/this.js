6ff51c4ce4f57812585735be6139f72e0eb85388
bri79319 <bri79319@gmail.com>
2018-06-27 16:45:46 -0400

    JS
    finished this.js file

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,74 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Implicit Binding
-* 2. Explicit Binding
-* 3. New Binding
-* 4. Window Binding
+* 1. Implicit Binding - invoking a function to retrieve a 
+property within an object, which lies in the function.
+
+* 2. Explicit Binding - invoking a function but with 
+multiple parameters.
+
+* 3. New Binding - used for making constructor functions
+to help copy a layout for the objects.
+
+* 4. Window Binding - default object when 
+none of the above apply.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+this.name = "CS 12 window example";
+
+function sayName(name) {
+  console.log(this.name);
+}
+
+sayName("Josh");
 
 // Principle 2
 
-// code example for Implicit Binding
+const myObject = {
+    'greeting': 'What up',
+    'sayHello': function(name) {
+     console.log(`${this.greeting} my name is: ${name}`);
+   }
+ }
+
+ myObject.sayHello("Erica");
 
 // Principle 3
 
-// code example for New Binding
+function CordialPerson(greeter) {
+    // new === this 
+    this.greeting = "Hello";
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log( `${this.greeting} ${this.greeter}`)
+    }
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  const josh = new CordialPerson('CS 12');
+  
+  jerry.speak();
+  newman.speak();
+  josh.speak();
 
 // Principle 4
 
-// code example for Explicit Binding
+const myObject = {
+    'name': 'Jim',
+    'age': 30
+  }
+  
+  const skills = ['HTML','CSS','JS'];
+  
+  function sayName(skill1, skill2, skill3) {
+    console.log(`My name is: ${this.name} and I like to program using these skills: ${skill1}, ${skill2}, ${skill3} `);
+  }
+  
+  sayName.call(myObject, skills[0], skills[1], skills[2]);
+  sayName.call(myObject, skills);
+  sayName.call(myObject, ...skills);
