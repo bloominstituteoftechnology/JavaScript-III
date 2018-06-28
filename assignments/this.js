99a4b9585e473bddcdc74dd5c2d7bc87946e5dec
EricaThompson <ericathompson.nc@gmail.com>
2018-06-28 05:32:01 -0700

    this.js complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,32 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit = Implied, this refers to what is to the left of the dot.
+* 2. Explicit = When using .call, .bind or .apply to tells what this is referring to 
+* 3. New = The constructor keyword that creates an instance of this based on a Class
+* 4. Window = If nothing else is bound to the this, it keeps bubbling out until it hits the window object
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function sample(){
+  console.log(this);
+};
 
 // Principle 2
 
-// code example for Implicit Binding
+function Example(name){
+    return `${this.name}`;
+};
 
 // Principle 3
 
-// code example for New Binding
+const idea = new Example({
+    name = "Good",
+});
 
 // Principle 4
 
-// code example for Explicit Binding
+Example.call(this, name);
