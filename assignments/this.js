1a1b410c639f944646c003e816b03f81c1f432a9
Huthman King <Huff@Huthmans-MacBook-Pro.local>
2018-08-08 16:37:03 -0400

    Completed the task regarding providing an explanation for the this keyword

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. The "this" keyword's value is that of the window/console object when contained by the global scope. This is known as window/global object binding.
-* 2. 
-* 3. 
-* 4. 
+* 2. Implicit Binding regards a function that is called preceded by a dot, the keyword this, is the object before the dot.
+* 3. New binding refers to this as the exact instance when an object is created and returned through the use of a constructor function.
+* 4. This is specifically defined whenever call or the apply method is used through JavaScript. 
 *
 * write out a code example of each explanation above
 */
@@ -17,7 +17,7 @@
 
 // code example for Implicit Binding
 
-// Principle 3
+// Principle 
 
 // code example for New Binding
 
