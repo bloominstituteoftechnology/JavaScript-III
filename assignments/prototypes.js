b7038d15eec591dbb33f89d1259adfb4497bd2fe
Michael Trevino <preince560m@gmail.com>
2018-08-08 17:24:41 -0500

    everything done but still trying a few things

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -41,7 +41,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +102,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
