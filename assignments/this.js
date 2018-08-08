3741096bf2e7beaa4717d8bf0e6358641ba3f4be
verydecent <wnjhwng@gmail.com>
2018-08-08 13:05:48 -0700

    Initial PR

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. Window Binding; "This" is always refered to the window it is used for. So If this does not extend anything it will refer to the window. Or it will be undefined.
 * 2. 
 * 3. 
-* 4. 
+* 4. Explicit Binding; This is used when we are using call to conect a function's "this" from a variable from another object source.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,12 @@
 // Principle 1
 
 // code example for Window Binding
+
+//this refers to the window, we should run into an error
+function incrementor(n) {
+    console.log(n + this.num);
+}
+//if we introduced let num = 1 however, the this would not refer to the window, but to the value of 1
 
 // Principle 2
 
@@ -24,3 +30,18 @@
 // Principle 4
 
 // code example for Explicit Binding
+
+function foo() {
+	console.log( this.a );
+}
+
+var obj = {
+	a: 2
+};
+
+var obj2 = {
+    a: 5
+}
+
+foo.call( obj ); // 2
+foo.call( obj2 ); // 5
