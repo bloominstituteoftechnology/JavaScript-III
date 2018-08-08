7ad62924a5dff2dbae6986818273f54d274c57a0
Melanie Barth <mbarth1967@gmail.com>
2018-08-08 16:43:01 -0600

    JavaScript III assignment

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,7 +14,13 @@
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
-
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.dimensions = attributes.dimensions; 
+}
+GameObject.prototype.destroy = function() {
+  return `Object was removed from the game.`
+};  
 /*
   === CharacterStats ===
   * hp
@@ -22,7 +28,11 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(attributes) {
+  this.hp = attributes.hp;
+  this.name = attributes.name;
 
+CharacterStats.prototype = Object.create(GameObject.prototype);
 /*
   === Humanoid ===
   * faction
@@ -32,6 +42,26 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid(attributes) {
+  this.faction = attributes.faction;
+  this.weapons = attributes.weapons;
+  this.languages = attributes.languages;
+}
+
+Humanoid.prototype.takeDamage = function() {
+  return `${Object.name} took damage.`
+};
+Humanoid.prototype.CharacterStats = function(attributes) {
+  GameObject.call(attributes)
+};
+Humanoid.prototype.CharacterStats = function(destroy) {
+  GameObject.call(destroy)
+}
+};
+
+Humanoid.prototype = Object.create(CharacterStats.prototype)
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +71,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +132,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+ /* Stretch task: 
+  * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+  * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  * Create two new objects, one a villian and one a hero and fight it out with method!*/
