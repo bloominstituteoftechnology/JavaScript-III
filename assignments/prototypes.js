0f78279a709add4c7c77274d34a4863dbe4dd96d
m-j-hacker <mjhacker@gmail.com>
2018-08-08 16:14:28 -0500

    Got to Humanoid section of prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,19 +1,30 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
+
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+/* Creating a constructor function
+ *
+ *
+ */
+function GameObject(attr) {
+  this.createdAt = attr.createdAt;
+  this.dimensions = attr.dimensions;
+  }
+// creating destroy method on GameObject
+  GameObject.prototype.destroy = function() {
+    return `${this} was removed from the game`;
 
 /*
   === CharacterStats ===
@@ -22,6 +33,19 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(attr) {
+  this.hp = attr.hp;
+  this.name = attr.name;
+
+  }
+
+  // inheriting from GameObject
+  CharacterStats.prototype = Object.create(GameObject.prototype);
+
+  // method on prototype of CharacterStats
+  CharacterStats.prototype.takeDamage = function() {
+    return `${this.name} took damage.`;
+
 
 /*
   === Humanoid ===
@@ -104,7 +128,7 @@
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 */
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+  // Stretch task:
+  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
