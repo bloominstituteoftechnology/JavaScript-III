f382a7cfe76b6460f4a12734c6f390a5601def95
Michael Agard <mdagard@gmail.com>
2018-08-08 14:54:45 -0400

    initial commit.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding allows if no other binding is attached, the window is what is used for 'this'.
+* 2. Implicit binding occurs when dot notation is used to invoke a function.
+* 3. New binding creates an instance of a user-defined object type or of one of the built-in object types that has a constructor function.
+* 4. Explicit binding of this occurs when .call(), .apply(), or .bind() are used on a function.
 *
 * write out a code example of each explanation above
 */
