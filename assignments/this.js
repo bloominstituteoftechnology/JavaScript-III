f3429db76aea5a8f2658310b36db1e1515ca053c
Tristen Neu <tristenneu@Tristens-MacBook-Pro.local>
2018-08-09 14:24:59 -0500

    This Complete

--- assignments/this.js
+++ assignments/this.js
@@ -12,18 +12,44 @@
 // Principle 1
 
 // code example for Window Binding
-function firstFunction() {
+function myFirstFunction() {
   return this;
 }
+
+myFirstFunction();
 
 // Principle 2
 
 // code example for Implicit Binding
+const myFirstObj = {
+  myFirstProp: 30,
+  myFirstMethod: function() {
+    return this.myFirstProp;
+  }
+};
+
+console.log(myFirstObj.myFirstMethod());
 
 // Principle 3
 
 // code example for New Binding
+function myFirstConstructor() {
+  this.mySecondProp = 59;
+}
+
+let mySecondObj = new myFirstConstructor();
+console.log(mySecondObj.mySecondProp);
 
 // Principle 4
 
 // code example for Explicit Binding
+function mySecondConstructor(myFirstParam, mySecondParam) {
+  return this.myThirdProp + this.myFourthProp + myFirstParam + mySecondParam;
+}
+
+const myThirdObject = { myThirdProp: 1, myFourthProp: 3 };
+
+// The first parameter is the object to use as
+// 'this', subsequent parameters are passed as
+// arguments in the function call
+console.log(mySecondConstructor.call(myThirdObject, 5, 7));
