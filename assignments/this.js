4b79b227ec2fe3973f639e0ecf6adcd31959a06c
Tristen Neu <tristenneu@Tristens-MacBook-Pro.local>
2018-08-09 14:22:30 -0500

    This Complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding - context is implied as the object to the left of the dot
+* 2. Explicit Binding - context is referenced using functions/methods such as call, apply, bind
+* 3. New Binding - context is the object being instantiated
+* 4. Window/Global Object Binding - context not clearly defined (defaults to window object)
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,9 @@
 // Principle 1
 
 // code example for Window Binding
+function firstFunction() {
+  return this;
+}
 
 // Principle 2
 
