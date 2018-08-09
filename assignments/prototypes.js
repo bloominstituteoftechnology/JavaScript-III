244696a2d051c3133b22a28b26ebadf7e083c5d5
Colin Preston <prestonresult@gmail.com>
2018-08-09 03:34:20 -0500

    changed names for readbility

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -63,6 +63,7 @@ return `${this.name} offers a greeting in ${this.language}.`
 };
 
  
+ 
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -70,6 +71,10 @@ return `${this.name} offers a greeting in ${this.language}.`
 */
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
+
+
+
+
 
 
   const mage = new Humanoid({
@@ -132,7 +137,6 @@ return `${this.name} offers a greeting in ${this.language}.`
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
