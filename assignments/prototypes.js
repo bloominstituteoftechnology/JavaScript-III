90c5236cc1023f41a4013195e91e2184b13cf806
lyang9 <lyang9@buffalo.edu>
2018-08-11 20:31:03 -0400

    Fixed bugs on stretch task

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -71,66 +71,66 @@ Humanoid.prototype.greet = function() {
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Bruce',
-    faction: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Toungue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Bruce',
+  faction: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Toungue',
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    hp: 15,
-    name: 'Sir Mustachio',
-    faction: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Toungue',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  hp: 15,
+  name: 'Sir Mustachio',
+  faction: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Toungue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    hp: 10,
-    name: 'Lilith',
-    faction: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  hp: 10,
+  name: 'Lilith',
+  faction: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.hp); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.faction); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
   // Stretch task: 
@@ -138,121 +138,94 @@ Humanoid.prototype.greet = function() {
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
 
-  function Villian(villAttr) {
-    Humanoid.call(this, villAttr);
-    this.weapons = villAttr.weapons;
-    this.attack = villAttr.attack;
+function Villian(villAttr) {
+  Humanoid.call(this, villAttr);
+  this.weapons = villAttr.weapons;
+  this.attack = villAttr.attack;
+}
+Villian.prototype = Object.create(Humanoid.prototype);
+Villian.prototype.stab = function(villian) {
+  //print villian stabs hero
+  console.log(`${this.name} stabs ${hero.name}`);
+  //hero loses hp
+  hero.hp -= 3;
+  //print hero loses hp
+  console.log(`${hero.name} took damage 5 times`);
+  if (hero.hp <= 0) {
+    //if hero has 0 hp, villian wins
+    console.log(`${hero.name} has 0 hp. ${hero.name}:loser ${this.name}:winner!`);
   }
-  Villian.prototype = Object.create(Humanoid.prototype);
-  Villian.prototype.stab = function(villian) {
-    console.log(`${this.name} stabbed ${hero.name}`);
-    console.log(`${this.name} took damage 5 times`);
-    if (villian.hp <= 0) {
-      return `${this.name} :loser ${hero.name} :winner!`;
-    }
-    if (`${this.name} took damage 5 times`) {
-      villian.hp -= 5;
-    }
+  else {
+    //otherwise, print hero hp
+    console.log(`${hero.name} has ${hero.hp} hp.`)
   }
+}
 
 
-  function Hero(heroAttr) {
-    Humanoid.call(this, heroAttr);
-    this.weapons = heroAttr.weapons;
-    this.attack = heroAttr.attack;
+function Hero(heroAttr) {
+  Humanoid.call(this, heroAttr);
+  this.weapons = heroAttr.weapons;
+  this.attack = heroAttr.attack;
+}
+Hero.prototype = Object.create(Humanoid.prototype);
+Hero.prototype.pierce = function(hero) {
+  //print hero pierces villian
+  console.log(`${this.name} pierces ${villian.name}`);
+  //villian loses hp
+  villian.hp -= 5;
+  //print villian loses hp
+  console.log(`${villian.name} took damage 3 times`);
+  if (villian.hp <= 0) {
+    //if villian has 0 hp, hero wins
+    console.log(`${villian.name} has 0 hp. ${villian.name}:loser ${this.name}:winner!`);
   }
-  Hero.prototype = Object.create(Humanoid.prototype);
-  Hero.prototype.pierce = function(hero) {
-    console.log(`${this.name} pierce ${villian.name}`);
-    console.log(`${this.name} took damage 3 times`);
-    if (hero.hp <= 0) {
-      return `${this.name} :loser ${villian.name} :winner!`;
-    }
-    if (`${this.name} took damage 3 times`) {
-      hero.hp -= 3;
-    }
+  else {
+    //otherwise, print villian hp
+    console.log(`${villian.name} has ${villian.hp} hp.`);
   }
+}
 
 
+const villian = new Villian({
+  createdAt: new Date(),
+  dimensions: {
+    length: 5,
+    width: 3,
+    height: 7,
+  },
+  hp: 48,
+  name: 'Nero',
+  faction: 'Knights of Mars',
+  weapons: [
+    'Sword of Ruin',
+  ],
+  language: 'Common Toungue',
+  attack: 'Stab',
+});
+
+const hero = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 6,
+    width: 3,
+    height: 6,
+  },
+  hp: 42,
+  name: 'Tiberius',
+  faction: 'Order of The Fallen God',
+  weapons: [
+    'Sword of Prosperity',
+  ],
+  language: 'Common Toungue',
+  attack: 'Pierce',
+});
 
 
-  const villian = new Villian({
-    createdAt: new Date(),
-    dimensions: {
-      length: 5,
-      width: 3,
-      height: 7,
-    },
-    hp: 48,
-    name: 'Nero',
-    faction: 'Knights of Mars',
-    weapons: [
-      'Sword of Ruin',
-    ],
-    language: 'Common Toungue',
-    attack: 'Stab',
-  });
-
-  const hero = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 6,
-      width: 3,
-      height: 6,
-    },
-    hp: 42,
-    name: 'Tiberius',
-    faction: 'Order of The Fallen God',
-    weapons: [
-      'Sword of Prosperity',
-    ],
-    language: 'Common Toungue',
-    attack: 'Pierce',
-  });
+while (hero.hp > 0 && villian.hp > 0) {
+  hero.pierce(villian);
+  if (villian.hp > 0) {
+    villian.stab(hero);
+  }
+}  
 
 
-  
-
-
-
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  console.log(`${hero.name} has ${hero.hp} hp left`);
-  console.log(hero.pierce(villian));
-  console.log(`${villian.name} has ${villian.hp} hp left`);
-  console.log(villian.stab(hero));
-  
- 
