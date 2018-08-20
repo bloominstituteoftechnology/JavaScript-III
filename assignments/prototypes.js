54a39046398df9a291cb12b7f2d9e94c8b5c449c
kurtve <kurt@vanetten.us>
2018-08-20 16:42:25 -0400

    Initial commit (nothing done yet)

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,11 +1,16 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
+  Object oriented design is commonly used in video games.  For this part of the
+  assignment you will be implementing several constructor functions with their
+  correct inheritance heirarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject,
+  CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.
+  Use the objects at the bottom of the page to test your constructor functions.
   
-  Each constructor function has unique properites and methods that are defined in their block comments below:
+  Each constructor function has unique properites and methods that are defined in
+  their block comments below:
 */
   
 /*
@@ -15,6 +20,8 @@
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
 
+
+
 /*
   === CharacterStats ===
   * hp
@@ -22,6 +29,8 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+
 
 /*
   === Humanoid ===
@@ -32,12 +41,16 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
+
+
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,40 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
+/* The four principles of "this"
+
+   In your own words, explain the four principle for the "this" keyword below.
+
+   1. Window Binding:
+
+
+   2. Implicit Binding:
+
+
+   3. New Binding:
+
+
+   4. Explicit Binding:
+
+
+
+   Write out a code example of each explanation above.
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
 
+
+// Principle 2
 // code example for Implicit Binding
 
-// Principle 3
 
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
 
+
+// Principle 4
 // code example for Explicit Binding
+
+
+
