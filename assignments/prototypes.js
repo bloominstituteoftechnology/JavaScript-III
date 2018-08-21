e5cbb1b8304cd7d4a447563b8e3dd384babd7700
Kendra Williams <kmonique.williams@gmail.com>
2018-08-21 18:07:20 -0400

    prototypes.js completed

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -39,9 +39,43 @@
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+function GameObject (objAttributes) {
+  this.createdAt = objAttributes.createdAt;
+  this.dimensions = objAttributes.dimensions;
+}
+
+GameObject.prototype.destroy = function (){
+  return `${this.name} was removed from the game.`;
+}
+
+function CharacterStats(charAttributes){
+  GameObject.call(this, charAttributes);
+  this.hp = charAttributes.hp;
+  this.name = charAttributes.name;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function (){
+  return `${this.name} took damage`;
+};
+
+function Humanoid(humanAttributes){
+  CharacterStats.call(this, humanAttributes);
+  this.faction = humanAttributes.faction;
+  this.weapons = humanAttributes.weapons;
+  this.language = humanAttributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function (){
+  return `${this.name} offers a greeting in ${this.language}`;
+}
+
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +136,6 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
