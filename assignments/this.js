52e14004170ed6e2538854b0bbb9dc8a17d47ded
paulapivat <paul.apivat@gmail.com>
2018-08-21 11:50:02 +0700

    add answers to this.js

--- assignments/this.js
+++ assignments/this.js
@@ -13,14 +13,62 @@
 
 // code example for Window Binding
 
+// this is not referencing any object, so the computer keeps looking at a higher level
+function saySomething(){
+    console.log(this)
+}
+
+
 // Principle 2
 
 // code example for Implicit Binding
+let car = {
+    model: 'Mazda CX3',
+    vroom: function(){
+      return `${this.model} is silky smooth!`
+    }
+  }
+  
+  car.vroom()
 
 // Principle 3
 
 // code example for New Binding
+function User(obj) {
+    // this = {};
+    this.firstName = obj.firstName;
+    this.lastName = obj.lastName;
+    this.lovesJavascript = true;
+    this.saysHi = function(person) {
+        return `${this.firstName} says hi to ${person}!`
+    };
+    // return this
+};
+
+let Corey = new User({
+    firstName: 'Corey',
+    lastName: 'Feldman'
+})
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const yourObject = {
+    name: 'Paul',
+    city: 'BKK'
+}
+
+const thingsYouEnjoy = ['bball', 'card collecting', 'coding', 'fire']
+
+function tellUsAboutYourself(thing1, thing2, thing3, thing4){
+return `Hi! My name is ${this.name}, I live in ${this.city}, and I enjoy ${thing1}, ${thing2}, and ${thing3} and ${thing4}.`
+}
+
+const paulIntro = tellUsAboutYourself.bind(yourObject);
+paulIntro(...thingsYouEnjoy);
+
+// tellUsAboutYourself.call(yourObject, ...thingsYouEnjoy );
+
+// tellUsAboutYourself.apply(yourObject, thingsYouEnjoy);
+
