b99ffd23f9909e922108d46698236dd3fa049940
paulapivat <paul.apivat@gmail.com>
2018-08-21 11:12:15 +0700

    start assignment this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding: Absence an object, "this" will reference Window which is at the highest level
+* 2. Implicit Binding: When "this" is within an object, it references that object
+* 3. New Binding: When calling 'new', you're creating a new instance of a class, 'this' references that class
+* 4. Explicit Binding: When using .call .apply .bind to bind objects to functions
 *
 * write out a code example of each explanation above
 */
