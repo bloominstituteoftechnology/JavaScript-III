3d74ef354b5b6927ba591cd025e04a1317740def
Clem <stacy.clem@e-hps.com>
2018-08-21 20:38:11 +0100

    Updated Implicit Binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. New
-* 2. Window
-* 3. Implicit
-* 4. Explicit
+* 1. New Binding - is when you create a constructor then and setting it to a new object , or specific instance of the object. 
+*    It will then be returned by the constructor function.
+*
+* 2. Window/Global Binding - "this" will be bound to the window if there is no constructor it can be bound too.
+* 3. Implicit - Is a function that is called with a preceding dot. What this mean is
+*    that anything that is left of the dot notation for a function is "this"
+* 
+*  4. Explicit - This is when you can use a call or apply method to explicity define "this". It allows you override 
+*     constructors by explicitly calling a new context with call or apply.  
 *
 * write out a code example of each explanation above
 */
@@ -21,6 +26,15 @@ HelloWorld('Stefan');
 
 // code example for Implicit Binding
 
+const stefanObj = {
+    greet: 'Hello World, ',
+    greeting: function(name)
+ {
+     console.log(`${this.greet} my name is ${name}`)
+  
+ }}
+
+stefanObj.greeting('Stefan')
 // Principle 3
 
 // code example for New Binding
