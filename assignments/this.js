56d6feae6dd670b88135b00bb6f79131c7da705f
Chris Juelfs <CRJuelfs24@comcast.net>
2018-08-21 15:52:27 -0500

    this.js completed!

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,56 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window: when used outside a function 
+* 2. implicit: refering to the object withing the object.
+* 3. new: creating new key:value pairings based on the custom labels we make later on.
+* 4. explicit: .call, .apply, or .bind
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
-
+// Principle 1: window
 // code example for Window Binding
+function saySomething(){
+    console.log(this)
+  };
 
-// Principle 2
-
+// Principle 2: implicit
 // code example for Implicit Binding
+let person = {
+    name: 'Chris Juelfs',
+    age: 25,
+    gender: 'Male',
+    aboutMe: function() {
+        return `Hello my name is ${this.name} and i'm a ${this.age} yrs old ${this.gender}.`
+    };
+};
 
-// Principle 3
+console.log(person.aboutMe());
 
+// Principle 3: new
 // code example for New Binding
+function Shape(obj) {
+    this.name = obj.name;
+    this.sides = obj.sides;
+    this.stateSides = function() {
+        return `A ${this.name} has ${this.sides} sides.`
+    };
+};
 
-// Principle 4
+let Triangle = new Shape({
+    name: 'Triangle',
+    sides: 3
+});
+console.log(Triangle.stateSides());
 
+// Principle 4: explicit
 // code example for Explicit Binding
+const chris = {
+    name: 'Chris'
+};
+const interests = ['Video Games', 'Disc Golf', 'Coding'];
+function aboutMe(a, b, c) {
+    return `Hello my name is ${this.name} and my interests are ${a}, ${b}, ${c}.`
+};
+console.log(aboutMe.apply(chris, interests));
