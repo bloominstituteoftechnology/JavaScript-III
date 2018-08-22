e8d1b42504b7ad0c620cf3eb046b23e4d37eb007
David Reichardt <davereichardt@gmail.com>
2018-08-21 23:29:21 -0400

    Having dificulty with prototypes, keep getting undefined

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
 
+function GameObject(obj){
+  this.createdAt = obj.createdAt;
+  this.dimensions = obj.dimensions;
+};
+GameObject.prototype.destroy = function(obj){
+  return `${obj} was removed from the game.`;
+};
+
 /*
   === CharacterStats ===
   * hp
@@ -22,6 +30,17 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+function CharacterStats(obj){
+  this.hp = obj.hp;
+  this.name = obj.name;
+};
+CharacterStats.prototype.takeDamage = function(obj){
+  return `${this.name} took damage.`;
+};
+
+
 
 /*
   === Humanoid ===
@@ -32,6 +51,18 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+function Humanoid(obj){
+  this.faction = obj.faction;
+  this.weapons = obj.weapons;
+  this.language = obj.language;
+}
+
+Humanoid.prototype.greet = function(name, language){
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +72,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +133,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
