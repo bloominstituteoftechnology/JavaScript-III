2987e59a236aa4ec4212df42d04164a268a507c8
FreeKeyBoy <jjhutton81@gmail.com>
2018-08-21 19:18:57 -0700

    Finished the first problem on file this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding is when .this is referencing a value in the global environment. 
+* 2. Implicit binding is when whatever is to the left 
+* 3. New binding is when .this is used to create a new function based on a previous function.
+* 4. Explicit binding is when .this is used with .call .apply or .bind on a function.
 *
 * write out a code example of each explanation above
 */
@@ -12,10 +12,30 @@
 // Principle 1
 
 // code example for Window Binding
+function win() {
+    return this;
+}
+
+//or
+console.log(this);
+console.log(window === this);
 
 // Principle 2
 
 // code example for Implicit Binding
+
+const ThisIsObject = function () {
+    this.name = 'This Is Object';
+    this.stuff = 'Stuff';
+}
+
+ThisIsObject.prototype.doStuff = function (action) {
+    console.log(`${this.name} ${this.stuff}!`);
+}
+
+ var obj = new ThisIsObject();
+
+ obj.doStuff();
 
 // Principle 3
 
