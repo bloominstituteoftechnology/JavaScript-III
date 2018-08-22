f474860b242725f10cf6eda1120de86e0c0036f9
leelichtsinn <lee.lichtsinn@gmail.com>
2018-08-21 22:06:31 -0400

    added explinations for the 4 principles of this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window or global object binding. In the browser "this" will be bound to the window object, while in node, "this" will be bound to the global object.
+* 2. Implicit binding, "this" is the object before the dot and called function.
+* 3. New keword binding, "this" refers to the instance of an object returned by a constructor function.
+* 4. Explicit binding, uses the js call and apply methods to pass the value of "this" to another.
 *
 * write out a code example of each explanation above
 */
