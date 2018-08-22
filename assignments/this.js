3c13534254b1eb38091ef1618b47c29680ac34f2
Alex Ostrander <alexanderostrander23@gmail.com>
2018-08-21 22:20:24 -0400

    Added call to explicit binding

--- assignments/this.js
+++ assignments/this.js
@@ -2,21 +2,13 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *   
 * 1. Window Binding
-
 When scope is global 'this' is the object of the window which is the object that is in the browser.
-
 * 2. New Binding
-
 'This' refers to the new object created when using a constructor function.
-
 * 3. Explicit binding
-
 Whenever apply or call is used this means that the object is explicitly defined.
-
 * 4. Implicit binding
-
 Used with dot notation to invoke a function, this refers to the variable in the object its referencing.
-
 *
 * write out a code example of each explanation above
 */
@@ -48,6 +40,8 @@ function sport() {
 const Alex = {
     sport: 'Hockey'
 }
+
+sport.call(Alex);
 // Principle 4
 
 const personObj = {
