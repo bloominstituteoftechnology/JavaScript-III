ce7bdc9947ededc9e1f919c61e724cb0ef12da21
Pascale Pierre <pascaleee@gmail.com>
2018-08-21 21:32:29 -0500

    completed first part of THIS challange

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When used alone "this" is a global object.
+* 2. In Implicit binding "this" is the object. The owner of hte function. What comes after the dot is the property.
+* 3. In new binding, using "new" we create an instance of the object with new.
+* 4. Explicit binding is used to call an object method with another object as an argument.
 *
 * write out a code example of each explanation above
 */
