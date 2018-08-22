833994116e437ed7282295f256a6ee066d1bec6a
FreeKeyBoy <jjhutton81@gmail.com>
2018-08-21 19:52:43 -0700

    Finished the last problem on file this.js

--- assignments/this.js
+++ assignments/this.js
@@ -55,3 +55,15 @@ function Console(maker, model, resolution, year) {
 // Principle 4
 
 // code example for Explicit Binding
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  
+  function Food(name, price) {
+    Product.call(this, name, price);
+    this.category = 'food';
+  }
+  
+  console.log(new Food('cheese', 5).name);
