67684897fea62b83b3955aabed8157456833b0c3
Pascale Pierre <pascaleee@gmail.com>
2018-08-21 22:08:51 -0500

    completed 3 out of 4 principle examples

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,42 @@
 */
 
 // Principle 1
+var x = this;
+
+console.log(x);
 
 // code example for Window Binding
 
 // Principle 2
+var imBind = {
+  food : "apples",
 
+whatsForDinner: function(name){
+  console.log(`Would you like ${this.food} for dinner, ${name}`);
+}}
+console.log(imBind.whatsForDinner("Pascale"));
 // code example for Implicit Binding
 
 // Principle 3
 
 // code example for New Binding
+function Outfit(type, style, color) {
+  this.type = type;
+  this.style = style;
+  this.color = color;
+  this.speak = function() {
+    console.log(`I love my new ${this.color}, ${this.style} ${this.type}!!!!!`);
 
+  };
+}
+
+var outfit1 = new Outfit('Dress', 'Vintage', 'Purple');
+var outfit2 = new Outfit('Pantsuit', 'Modern', 'Black');
+
+console.log(Outfit)
+outfit1.speak();
+outfit2.speak();
 // Principle 4
+
 
 // code example for Explicit Binding
