fef29dc758db90975fcabfb2e9ef45872cdba090
Andres Rivera <andres.rivera.toro@gmail.com>
2018-08-21 22:39:40 -0400

    initial commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit
+* 2. Explicit
+* 3. New
+* 4. Global Object
 *
 * write out a code example of each explanation above
 */
