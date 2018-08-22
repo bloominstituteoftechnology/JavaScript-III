8595b06eea8aa94bf33b1edb01fadabbc80fc738
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-08-21 22:35:21 -0500

    completed the prototypes page need to finish my questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. window binding has no parent. It 
 * 2. 
 * 3. 
 * 4. 
