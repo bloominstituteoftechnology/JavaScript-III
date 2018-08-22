b0afad69a16a93bd85262817511ca8d4101ffd76
Willie Yturralde <willieino@hotmail.com>
2018-08-21 23:04:19 -0700

    modify js files.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,180 +14,204 @@
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
-function GameObject(obj) {
-  this.createdAt = obj.createdAt;
-  this.dimensions = obj.dimensions;
-   }
-
-GameObject.prototype.destroy = function() {
-  return (`${this.name} was removed from the game.`);
-}
-/*
-  === CharacterStats ===
-  * hp
-  * name
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
-//const CharacterStats = new GameObject();
-
-function CharacterStats(obj) {
-  GameObject.call(this, obj);
-  this.hp = obj.hp;
-  this.name = obj.name;
-}
-CharacterStats.prototype = Object.create(GameObject.prototype);
-
-CharacterStats.prototype.takeDamage = function() {
-  return (`${this.name} took damage`)
-};
-
-/*
-  === Humanoid ===
-  * faction
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
-//const Humanoid = new CharacterStats();
-
-function Humanoid(obj) {
-  this.faction = obj.faction;
-  this.weapons = obj.weapons;
-  this.language = obj.language;
-  CharacterStats.call(this, obj);
-}
-Humanoid.prototype = Object.create(CharacterStats.prototype);
-
-Humanoid.prototype.greet = function() {
-  return (`${this.name} offers a greeting in ${this.language}.`);
-}
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
-
-// Test you work by uncommenting these 3 objects and the list of console logs below:
-
-
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Bruce',
-    faction: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Toungue',
-  });
-
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    hp: 15,
-    name: 'Sir Mustachio',
-    faction: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Toungue',
-  });
-
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    hp: 10,
-    name: 'Lilith',
-    faction: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
-
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
-  function Hero(Obj) {
+function theWar() {
+  let villanHp = 20;
+  let heroHp = 20;
+  
+  function GameObject(obj) {
+    this.createdAt = obj.createdAt;
+    this.dimensions = obj.dimensions;
+     }
+  
+  GameObject.prototype.destroy = function() {
+    return (`${this.name} was removed from the game.`);
+  }
+  /*
+    === CharacterStats ===
+    * hp
+    * name
+    * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+    * should inherit destroy() from GameObject's prototype
+  */
+  //const CharacterStats = new GameObject();
+  
+  function CharacterStats(obj) {
+    GameObject.call(this, obj);
+    this.hp = obj.hp;
+    this.name = obj.name;
+  }
+  CharacterStats.prototype = Object.create(GameObject.prototype);
+  
+  CharacterStats.prototype.takeDamage = function() {
+    return (`${this.name} took damage`)
+  };
+  
+  /*
+    === Humanoid ===
+    * faction
+    * weapons
+    * language
+    * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+    * should inherit destroy() from GameObject through CharacterStats
+    * should inherit takeDamage() from CharacterStats
+  */
+  //const Humanoid = new CharacterStats();
+  
+  function Humanoid(obj) {
+    this.faction = obj.faction;
+    this.weapons = obj.weapons;
+    this.language = obj.language;
+    CharacterStats.call(this, obj);
+  }
+  Humanoid.prototype = Object.create(CharacterStats.prototype);
+  
+  Humanoid.prototype.greet = function() {
+    return (`${this.name} offers a greeting in ${this.language}.`);
+  }
+  /*
+    * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+    * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+    * Instances of CharacterStats should have all of the same properties as GameObject.
+  */
+  
+  // Test you work by uncommenting these 3 objects and the list of console logs below:
+  
+  
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      hp: 5,
+      name: 'Bruce',
+      faction: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama',
+      ],
+      language: 'Common Toungue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      hp: 15,
+      name: 'Sir Mustachio',
+      faction: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Toungue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      hp: 10,
+      name: 'Lilith',
+      faction: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+  
+    console.log(mage.createdAt); // Today's date
+    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+    console.log(swordsman.hp); // 15
+    console.log(mage.name); // Bruce
+    console.log(swordsman.faction); // The Round Table
+    console.log(mage.weapons); // Staff of Shamalama
+    console.log(archer.language); // Elvish
+    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+    console.log(mage.takeDamage()); // Bruce took damage.
+    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  
+  
+    // Stretch task: 
+    // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+    // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+    // * Create two new objects, one a villian and one a hero and fight it out with methods!
+    function Hero(Obj) {
+      Humanoid.call(this, Obj);
+    }
+    Hero.prototype = Object.create(Humanoid.prototype);
+  
+  Hero.prototype.attack = function() {
+    return (`Hero attacks the Villan with ${this.weapons[0]}! He does damage. Villan has ${villanHp += - 6} points left.`);
+  }
+  
+  function Villan(Obj) {
     Humanoid.call(this, Obj);
   }
-  Hero.prototype = Object.create(Humanoid.prototype);
-
-Hero.prototype.attack = function() {
-  return (`Hero attacks the Villan with ${this.weapons[0]} does 1 point damage. Villan has ${this.hp - 2} points left.`);
-}
-
-function Villan(Obj) {
-  Humanoid.call(this, Obj);
-}
-Villan.prototype = Object.create(Humanoid.prototype);
-
-Villan.prototype.attacks = function() {
-return (`Villan attacks the Hero with ${this.weapons[0]} does 1 point damage. Hero has ${this.hp - 1} points left.`);
-}
-
-const Gollum = new Hero({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Joe',
-    faction: 'Mage Guild',
-    weapons: [
-      'Staff of Joe',
-    ],
-    language: 'Common Toungue',
-  });
-
-const Shazam = new Villan({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Golly',
-    faction: 'Mage Guild',
-    weapons: [
-      'Sword of Shazam',
-    ],
-    language: 'Common Toungue',
-  });
-
-console.log(Gollum.attack());
-console.log(Shazam.attacks());
+  Villan.prototype = Object.create(Humanoid.prototype);
+  
+  Villan.prototype.attacks = function() {
+  return (`Villan attacks the Hero with ${this.weapons[0]}! He does damage. Hero has ${heroHp += - 4} points left.`);
+  }
+  
+  const Gollum = new Hero({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      hp: 5,
+      name: 'Joe',
+      faction: 'Mage Guild',
+      weapons: [
+        'BattleAxe of Victory',
+      ],
+      language: 'Common Toungue',
+    });
+  
+  const Shazam = new Villan({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      hp: 5,
+      name: 'Golly',
+      faction: 'Mage Guild',
+      weapons: [
+        'Sword of Shazam',
+      ],
+      language: 'Common Toungue',
+    });
+  
+  
+  for (let i = 0; i < 4; i++) {
+    console.log(Gollum.attack());
+    console.log(Shazam.attacks());
+    whoDead(heroHp, villanHp);
+  }
+  
+  function whoDead(heroLife, villanLife) {
+    if (villanLife < 1) {
+      console.log(`The Villan is Dead!`);
+      return;
+    } else {
+      if (heroLife < 1) {
+        console.log(`and the Hero is Dead!`);
+        return;
+      }
+    }
+  }
+  
+  }
+  
+  console.log(theWar());
