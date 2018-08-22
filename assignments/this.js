fc2504f0240e8e3e29400ea8f11153311ed7fb41
erscriber <erscriber@gmail.com>
2018-08-21 23:50:50 -0400

    This.js in progress

--- assignments/this.js
+++ assignments/this.js
@@ -1,21 +1,30 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit binding the this keyword is referring to whatever is to the left of the dot. 
+* 2. Explicit binding uses call, apply and bind to say what a function is going to be. Call makes you indicate each argument individually, apply allows you to use an array and bind gives you a new array you can call later.
+* 3. New binding when used, a new object is created inside of the function. 
+* 4. Window binding is the default use of the this keyword. If nothing is specified, window binding seaches the entire environment. This keyword is used outside of a function.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function(window) {
+	return this;
+}
 
 // Principle 2
 
-// code example for Implicit Binding
+const me = {
+	name: 'Erin',
+	sayName: function(){
+		console.log(this.name);
+	}
+};
+me.SayName();
+
 
 // Principle 3
 
@@ -23,4 +32,12 @@
 
 // Principle 4
 
-// code example for Explicit Binding
+const sayName = function(){
+	console.log('My name is ' + this.name);
+};
+
+const Erin = {
+	name: 'Erin',
+};
+
+sayName.call(Erin);
