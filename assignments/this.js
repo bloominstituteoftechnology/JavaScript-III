b524ff47b77840138e4b0ae640af912a0e9ca95c
jthomps838 <joe.thompson838@gmail.com>
2018-08-21 21:27:17 -0400

    finished this.js file

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global/window:
+        this will always point to window in this situation, and can be seen at the global scope or in an empty function.
+* 2. Implicit:
+        can only be accessed by the object before calling it using dot notation.
+* 3.Explicit:
+        used when you want to have both objects/functions seperated.
+* 4.New Binding:
+        accessed when creating an instance of an object.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +16,39 @@
 // Principle 1
 
 // code example for Window Binding
+  // In global scope
+  this.apple = {};
+  // Inside an empty function
+  function emptyFunc() {
+    return this;
+  }
 
 // Principle 2
-
 // code example for Implicit Binding
-
+  const SpaceShip = {
+    fuelType : 'Star Dust',
+    fullFuel : true,
+    amntEngines : 8,
+    distanceOnFull : "42 billion light years",
+    pplCapacity: 10802,
+    runDiagnostics() {
+      console.log(`All ${this.amntEngines} engines are operational`);
+    }
+  }
+  SpaceShip.runDiagnostics();
 // Principle 3
-
 // code example for New Binding
-
+  function Dog() {
+    this.color = 'black';
+  }
+  const Lab = new Dog();
 // Principle 4
-
 // code example for Explicit Binding
+function speak() {
+  console.log(`${this.name} says Hi!`);
+}
+
+const person = {
+        name : 'Fred'
+}
+speak.apply(person);
