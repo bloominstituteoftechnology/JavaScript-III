bd1f9855f91907f968381f8f78dd51c6384869cc
Andres Rivera <andres.rivera.toro@gmail.com>
2018-08-21 23:36:37 -0400

    principle 1 completed for this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Implicit
-* 2. Explicit
+* 1. Global Object
+* 2. Implicit
 * 3. New
-* 4. Global Object
+* 4. Explicit
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,11 @@
 // Principle 1
 
 // code example for Window Binding
+function dontSpeak() {
+  console.log('I know just what you\'re saying.')
+};
+
+dontSpeak(); // call-site for dontSpeak is in the global scope; call-stack is: dontSpeak
 
 // Principle 2
 
