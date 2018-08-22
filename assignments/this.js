1e3214397fb2543ec3db405555820b266d327a03
Bruce Cabanayan <bcabanayan@Bruces-MBP.domain>
2018-08-21 17:54:15 -0700

    Completed this keyword exercises

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,56 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global binding occurs when the 'this' keyword is invoked outside of the context of a defined object. In a browser, this context would be the Window. 
+* 2. Implicit binding occurs when 'this' is invoked as part of a function that is called using dot notation. The context is the object to the left of the dot. 
+* 3. Explicit binding occurs when 'this' is invoked as part of a function upon which .call, .apply, or .bind are used. 
+* 4. New binding occurs when a constructor function is used to create a new object. The context is the specific instance of the object that is created by the constructor function.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function windowExample () {
+    console.log(this);
+} 
+
+windowExample(); // logs Window due to window binding
 
 // Principle 2
 
-// code example for Implicit Binding
+let implicitSandwich = {
+    bread: 'Wheat',
+    meat: 'Turkey',
+    spread: 'Mayo',
+    toast: function() {
+        console.log(`Ding! ${this.bread} bread is toasted!`)
+    }
+}
+
+implicitSandwich.toast(); //logs Wheat bread is toasted! due to implicit binding
 
 // Principle 3
 
-// code example for New Binding
+function NewSandwich(bread, meat, spread) {
+    this.bread = bread;
+    this.meat = meat;
+    this.spread = spread;
+    this.toast = function () {
+        console.log(`Ding! ${this.bread} bread is toasted!`)
+    }
+}
+
+let BeefWithMustard = new NewSandwich('Rye', 'Roast beef', 'Mustard');
+
+BeefWithMustard.toast(); //logs Rye bread is toasted! due to new binding
 
 // Principle 4
 
-// code example for Explicit Binding
+let PBAndJ = {
+    bread:'White',
+    meat: 'None',
+    spread: 'Peanut butter and jelly'
+  }
+  
+BeefWithMustard.toast.call(PBAndJ); // logs White bread is toasted! due to explicit binding
