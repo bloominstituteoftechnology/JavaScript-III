d099d9659a3dfa7bdfcd158c71d8ea45d4737e56
mmorpchicago <mmohamed.rp@gmail.com>
2018-08-23 17:23:07 -0500

    Added responses to this.js - Officially complete.. LOL

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Principle one is where this is bound to the window, typically in situations where this has not been implicitly or explicitly specified or created with a new statement.
+* 2. Implicit binding is where an object is created and this is generally implicit to the object definition specified at design time.
+* 3. New binding generally takes effect when the object constructor is called and instanciated. This is generally at run time, throughout the lifecycle of the scope.
+* 4. Explicit binding is where this is specifically defined on a function or object, with its parameters, using inbuilt functions like call, apply and bind, each having separate params.
 *
 * write out a code example of each explanation above
 */
@@ -54,4 +54,4 @@ function aiCar(obj){
 
 const mercedes = new Car({model: "X5", year: 2019});
 
-aiCar.call(mercedes)
+aiCar.apply(mercedes, objProp)
