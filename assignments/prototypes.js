36558ae1fb34788ef09a1f82d30043ebd78b8248
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-08-23 23:40:35 -0400

    Completed: Prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,30 +1,52 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
+
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
-  === GameObject ===
+  === GameObject ====================================================================================
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
 
+function GameObject(properties) {
+     this.createdAt = properties.createdAt; // <-- Semicolon //
+     this.dimensions = properties.dimensions; // <-- Semicolon //
+}; // <-- Semicolon //
+
+GameObject.prototype.destroy = function() {
+     return `${this.name} was removed from the game.`; // <-- Semicolon //
+}; // <-- Semicolon //
+
 /*
-  === CharacterStats ===
+  === CharacterStats ================================================================================
   * hp
   * name
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
 
+function CharacterStats(stats) {
+     // This line allows the GameObject function to modify the data within CharacterStats that are found in GameObject initially //
+     GameObject.call(this, stats)
+     this.hp = stats.hp; // <-- Semicolon //
+     this.name = stats.name; // <-- Semicolon //
+}
+// In order to inherit from GameObject, we must create a copy of GameObject that we can bind to the current variable (CharacterStats) //
+CharacterStats.prototype = Object.create(GameObject.prototype); // <-- Semicolon //
+// Now I can create my independent 'takeDamage' function for CharacterStats to link the methods between CharacterStats //
+CharacterStats.prototype.takeDamage = function() {
+     // *** I'm note sure what 'this.name' is (actually) referring to ***//
+     return `${this.name} took damage.`; // <-- Semicolon //
+}
 /*
-  === Humanoid ===
+  === Humanoid =====================================================================================
   * faction
   * weapons
   * language
@@ -32,16 +54,27 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(object) {
+     // The first thing I have to do is declare my context, right? //
+     CharacterStats.call(this, object)
+     this.faction = object.faction; // <-- Semicolon //
+     this.weapons = object.weapons; // <-- Semicolon //
+     this.language = object.language; // <-- Semicolon //
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype); // <-- Semicolon //
+Humanoid.prototype.greet = function(language){
+     return `${this.name} offers a greeting in ${this.language}.`
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
-// Test you work by uncommenting these 3 objects and the list of console logs below:
+// Test your work by uncommenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +135,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+
+  // Stretch task:
+  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,29 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
+The for principles of "this";
+/* in your own words. explain the four principle for the "this" keyword below.
 */
+* 1. Window or Global Object Binding
+	In the JavaScript programming language, 'scope' referes to the  context of a variable. The global or windows scope is the most universal context of code - simply meaning that all variables declared in this scope are accessible to more localized (or private) scopes. With this understanding, Global Object Binding can be said to be a type of replication that creates a new function within the global/univesal context.
+* 2. Implicit Binding
+	- when a function is called using dot notation, the
+* 3. New Binding
+	- when a new variable
+* 4. Explicit Binding
 
-// Principle 1
+* write out a code example of each explanation above
+
+
+Principle 1
 
 // code example for Window Binding
 
-// Principle 2
+Principle 2
 
 // code example for Implicit Binding
 
-// Principle 3
+Principle 3
 
 // code example for New Binding
 
-// Principle 4
+Principle 4
 
 // code example for Explicit Binding
