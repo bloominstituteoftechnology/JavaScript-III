c689bebe15e934f7dc0669520aca883de9e338c9
David247365 <ds051128@gmail.com>
2018-09-12 14:56:53 -0500

    Completed Challenge 1 on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -3,9 +3,11 @@
 *
 * 1. Principle 1: Window/Global Object Binding: If you are in the global scope the value of this will refer to the window or console Object.
 
-* 2.  
-* 3. 
-* 4. 
+* 2. Principle 2: Implicit Binding: Whatever is directly left of the invocation is the keyword context.
+
+* 3. Principle 3: Explicit Binding: When we use .call(), .apply(), or.bind() methods, we are explicitly defining this.
+
+* 4. Principle 4: New Binding:  Whenever we use a Constructor Function we use the new keyword and this refers to the specific instance of the object that is created and returned by the constuctor function.
 *
 * write out a code example of each explanation above
 */
