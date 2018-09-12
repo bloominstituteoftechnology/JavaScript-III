725e205db5d72c8329db2a781d2107e4b557ba8e
bummings <sup@fauxkno.com>
2018-09-12 16:32:59 -0400

    working my way thru the this assignment

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
-/* The for principles of "this";
+/* The for* principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+*                      *: four
+* 1. Global binding: The value of 'this' refers to the overall global object- in the browser, this would be the Window object.
+* 2. Implicit binding: The value of 'this' is "bound" to the object calling it- the property "name" is bound to the object. The value of the name property is like a pointer- that is to say, "this one, this is what 'this' refers to."
+* 3.  New binding: The value os 'this' is refers directly to the object that is created and returned by the constructor function. It will have access to the __prototype__ "DNA", that is to say: each instantiation of an object will have access to the same properties which are relative to a unique "this".
+* 4. Explicit binding: The constuctor function's "DNA" or prototype can be overridden or just re-worked with the use of explicit binding. 
 *
 * write out a code example of each explanation above
 */
