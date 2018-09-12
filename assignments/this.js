dcefdc62207378f22909eee0bc2f441a9e27f2d6
Steve Smodish <smodish@hotmail.com>
2018-09-12 15:38:30 -0400

    Initial commit/PR

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. WINDOW - this refers to the window object when in the global scope.
+* 2. IMPLICIT - this refers to the object directly left of the dot from the function calling it.
+* 3. EXPLCIT - When "call" or "apply" is used, this refers to the object used as the first argument.
+* 4. NEW - When using constructor functions, this refers to the new object being created.
 *
 * write out a code example of each explanation above
 */
