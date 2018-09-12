50119962e6306e82a93293ce0a15ab86658b0852
MEBonham <programmer@mckaybonham.com>
2018-09-12 13:48:13 -0600

    Wrote explanation of four "this" rules

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. "this" is found inside functions. If the function has something to the left of it, with a dot notation, where it is invoked, then that thing to the left is IMPLICITLY the context for "this".
+* 2. Otherwise, instead of a normal function invocation, the function can be invoked through the .call(), .apply(), or .bind() methods of a function. In these cases, the first argument to the function invocation will be the context for "this". This is called EXPLICIT binding of "this".
+* 3. If the function around "this" is a constructor function, and therefore it's invoked through the "new" keyword, then the "new" binding tells "this" that its context is the new object being created by that constructor.
+* 4. If none of the above apply, "this" defaults to referring to the window in which JavaScript is running.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,8 @@
 // Principle 1
 
 // code example for Window Binding
+
+
 
 // Principle 2
 
