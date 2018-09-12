2cb98e67dea03d4132f1d2eba9514a0fb6c5d734
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2018-09-12 14:33:17 -0600

    this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. implicit: points to the object on which the function is called, so whatever is to the left of the .this.
+* 2. explicit: when we explicity state what this is using call, apply, bind, ie what comes after call in parentheses.
+* 3. new: the new keyword must be used in conjunction with a constructor function for an object, so this refers to the newly created object.
+* 4. window: defaults to global scope. 
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,33 @@
 // Principle 1
 
 // code example for Window Binding
+const personalInfo = {
+    'name': 'Ian',
+    'location': 'the Y-O',
+    'instrument': 'guitar',
+};
+console.log(this.name);
 
 // Principle 2
 
 // code example for Implicit Binding
+personalInfo.instrument();
+
 
 // Principle 3
 
 // code example for New Binding
+function personalInfo() {
+    this.name = name;
+    this.location = location;
+    this.instrument = instrument;
+}
+
+const kevinInfo = new personalInfo;
 
 // Principle 4
 
 // code example for Explicit Binding
+const genres = ['rock', 'jazz', 'blues'];
+function jammin(genres)
+jammin.call(personalInfo, ...genres);
