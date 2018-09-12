a761af20cca08e0c15fbba5fe87404e695cab1b5
Brad Mortensen <b.mort86@gmail.com>
2018-09-12 15:59:18 -0600

    finished stretch goals of prototypes.js assignment

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -72,6 +72,44 @@ Humanoid.prototype.greet = function () {
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+function Hero (heroAttributes) {
+  Humanoid.call(this, heroAttributes);
+  
+}
+
+Hero.prototype = 
+Object.create(Humanoid.prototype);
+
+Hero.prototype.swordSlash = function(){
+  if (this.hp >= 1){
+      return (`${demon.name}'s hp has been sword slashed to ${demon.hp - Math.floor(Math.random()*30)}!`)
+  }else{
+      return `Dude, you're already dead`;
+  }
+}
+
+function Villain (villainAttributes) {
+  CharacterStats.call(this, villainAttributes);
+  
+}
+
+Villain.prototype = 
+Object.create(Humanoid.prototype);
+
+Villain.prototype.curse = function(){
+  if (this.hp >= 0){
+      return (`${highKnight.name} has been cursed to ${highKnight.hp - Math.floor(Math.random()*15)}!`)
+  }
+}
+
+
+
+
+
+
+
+
+
 //Test you work by uncommenting these 3 objects and the list of console logs below:
 
 
@@ -125,6 +163,40 @@ const archer = new Humanoid({
   language: 'Elvish',
 });
 
+const highKnight = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 8,
+    width: 55,
+    height: 4,
+  },
+  hp: 30,
+  name: 'Dale the Most High Knight',
+  faction: 'The Round Table',
+  weapons: [
+    'lance',
+    'sword and shield',
+    'mace'
+  ],
+  language: 'Kings English',
+});
+const demon = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: null,
+    width: null,
+    height: null
+  },
+  hp: 66,
+  name: 'Ördög',
+  faction: 'underworld',
+  weapons: [
+    'Curse',
+    'Pestilence',
+  ],
+  language: 'none',
+});
+
 console.log(mage.createdAt); // Today's date
 console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
 console.log(swordsman.hp); // 15
@@ -135,9 +207,11 @@ console.log(archer.language); // Elvish
 console.log(archer.greet()); // Lilith offers a greeting in Elvish.
 console.log(mage.takeDamage()); // Bruce took damage.
 console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(demon.curse());
+console.log(highKnight.swordSlash());
 
 
 // Stretch task: 
 // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
 // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-// * Create two new objects, one a villian and one a hero and fight it out with methods!z
+// * Create two new objects, one a villian and one a hero and fight it out with methods!
