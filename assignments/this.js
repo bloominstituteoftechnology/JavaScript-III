68e42e419209f60c947dee71ba24a385009fa8a1
Corey Gumbs <cgumbs@Coreys-MacBook-Pro-3.local>
2018-09-12 16:29:04 -0400

    [chore] this.js started

--- assignments/this.js
+++ assignments/this.js
@@ -4,9 +4,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
+* 1. implicit binding is when this is used inside of the scope of the function/object in which it is referenced to.
+* 2. explicit binding is when this is referenced via a function/object outside of the scope of function/object and brought into that scope
+via an outside source such as another function.
+* 3. window binding
 * 4. 
 *
 * write out a code example of each explanation above
@@ -19,6 +20,16 @@
 // Principle 2
 
 // code example for Implicit Binding
+let objPerson = {
+    "name": "Corey",
+    "age": 41,
+    "fav-color": "blue",
+    greeting: function(){
+        console.log(`Hello, my name is ${this.name}`);
+    }
+}
+
+objPerson.greeting();
 
 // Principle 3
 
