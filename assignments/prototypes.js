725daae7775569e61a41a97eb22cbdd0c2e4fed9
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-09-12 15:26:01 -0500

    finished project

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -14,6 +14,13 @@
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+function GameObject(parameter){
+  this.createdAt = parameter.createdAt;
+  this.dimensions = parameter.dimensions;
+}
+GameObject.prototype.destroy = function () {
+  return `Object was removed from the game`;
+}
 
 /*
   === CharacterStats ===
@@ -22,6 +29,15 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(parameter){
+  GameObject.call(this, parameter);
+  this.hp = parameter.hp;
+  this.name = parameter.name;
+}
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage`;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
 
 /*
   === Humanoid ===
@@ -32,7 +48,16 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+ function Humanoid(parameter){
+   CharacterStats.call(this,parameter);
+   this.faction = parameter.faction;
+   this.weapons = parameter.weapons;
+   this.language = parameter.language;
+ }
+ Humanoid.prototype.greet = function (){
+   return `${this.name} offers a greeting in ${this.language}`
+ }
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -108,3 +133,4 @@
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
