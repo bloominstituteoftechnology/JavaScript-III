9a300b9c5f710f440bd5e156b158aa96a3a3248a
Brad Mortensen <b.mort86@gmail.com>
2018-09-12 13:32:57 -0600

    finished this.js assignment

--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,47 @@
 // Principle 1
 
 // code example for Window Binding
-console.log(this);
+
+function windowBound(){
+    return this;
+}
+console.log(windowBound());
+
+
 // Principle 2
 
 // code example for Implicit Binding
+const user = {
+    name: 'Brad',
+    age: 32,
+    greet() {
+      console.log(`Hello, my name is ${this.name}`);
+    }
+  }
+user.greet();
 
 // Principle 3
 
 // code example for New Binding
 
+function UserFunc (name, age) {  
+    this.name = name
+    this.age = age
+  }
+  
+  const me = new UserFunc('Biff', 76);
+
+  console.log(me);
 // Principle 4
 
 // code example for Explicit Binding
+
+function greet () {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+  
+const user1 = {
+    name: 'James',
+    age: 47,
+}
+greet.call(user1);
