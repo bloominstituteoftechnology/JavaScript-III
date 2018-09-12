bb16412c57e6166b5929a0fa1223e788ed2d53c5
nickkruger <ngkruger@gmail.com>
2018-09-12 16:40:32 -0600

    updated this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,23 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding
+    
+        When .this returns a value from anywhere within an object.
+
+* 2. Implicit Binding
+
+        When .this returns a value from a specific variable within an object.
+
+* 3. New Binding
+
+        When .this returns a value from existing variables within an object and creates a new object.
+
+* 4. Explicit Binding
+
+
+
+
 *
 * write out a code example of each explanation above
 */
