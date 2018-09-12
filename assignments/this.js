f462f4024c284c7b314ed74d254810fe1ad2e6b4
Edward J <edjeong99@gmail.com>
2018-09-12 18:14:48 -0400

    complete project and stretch goal

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. this bind to global as a default
+* 2. implicit binding is most common.  when a function is called with dot, what's left of dot is this
+* 3. new binding.  when constructor function is used to create a new object, this bind to the created object
+* 4. explicit binding.  when method call, apply are used this binds to what these methods indicate.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,42 @@
 
 // code example for Window Binding
 
+// console.log(this);
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const person = {
+"name" : "edward",
+speak : function(){
+    // console.log(`${this.name} is a person`);
+}
+};
+
+person.speak();
+
 
 // Principle 3
 
 // code example for New Binding
 
+function Animal(att) {
+    this.name = att;
+    this.speak = function(){
+        console.log(`${this.name} is an animal`);
+    }
+}
+
+
+const Rabbit = new Animal("rabbit");
+// Rabbit.speak();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+const Dog = new Animal("dog");
+Dog.speak();
+
+Dog.speak.call(Rabbit);
