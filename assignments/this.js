572b93b77f4e491aa1347d20c1af57b9875ddfb8
ThomasUtsey <tomutsey@yahoo.com>
2018-09-12 16:39:14 -0400

    completed the this worksheet starting on prototypes

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,59 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding
+
+Occurs when the this is used in conjunction with a . infront of it. 
+
+* 2. Explicit Binding
+
+occurs when this is used with a function method call,apply or bind. In the case of the Bind it could be mistook for implicit because the this is lead by a paramater ..
+
+
+* 3. New Binding
+
+
+
+* 4. Window Binding
+
+window binding is using the this keyword to call on an object in the global scope.
+most of the time this occurs on accident and it returns undefined
 *
 * write out a code example of each explanation above
 */
+//------------------------------------------
+// Principle 1// code example for Window Binding
+this.name();
 
-// Principle 1
+//-------------------------------------------
+// Principle 2// code example for Implicit Binding
 
-// code example for Window Binding
 
-// Principle 2
+let user = {
+  name: 'Tom',
+  age: 41,
+  speak:function(){
+    console.log(`${this.name} says Hi!!`)
+  }
+}
+user.speak();
+// -------------------------------------------
+// Principle 3// code example for New Binding
+let Car = function(make, model ,year){
+  this.make = make;
+  this.model = model;
+  this.year = year; 
+};
+let mycar = new Car('Dodge','Charger',2018);
 
-// code example for Implicit Binding
+//--------------------------------------------
+// Principle 4 // code example for Explicit Binding
+let sayName = function (){
+  console.log('my name is '  + this.name)
+}
+let tom = {
+  name:'Thomas',
+  age:'40'
+}
+sayName.call(tom);
 
-// Principle 3
-
-// code example for New Binding
-
-// Principle 4
-
-// code example for Explicit Binding
