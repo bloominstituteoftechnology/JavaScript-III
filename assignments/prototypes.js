81af12851bd538ee3d4113560d1e2d23e2dcfada
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-12 16:41:43 -0500

    added hero constructor and created a hero

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -64,7 +64,7 @@ Humanoid.prototype.greet = function() {
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-
+/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -125,9 +125,32 @@ Humanoid.prototype.greet = function() {
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-
+*/
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+function Hero(heroAttrs) {
+  Humanoid.call(this, heroAttrs);
+  this.transformations = heroAttrs.transformations;
+  this.abilities = heroAttrs.abilities;
+  this.equipment = heroAttrs.equipment;
+}
+Hero.prototype = Object.create(Humanoid.prototype);
+
+const ichigo = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 3,
+  },
+  hp: 30,
+  name: 'Ichigo Kurosaki',
+  weapons: ['Zangetsu', 'Tensa Zangetsu'],
+  language: 'Japanese',
+  equipment: ['Substitute Shinigami Badge', 'Protective Charm', 'Oken Clothing'],
+  abilities: ['Shunpo', 'Getsuga Tensho', 'Bankai', 'Hollowfication'],
+})
