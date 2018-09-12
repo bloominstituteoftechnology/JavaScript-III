000bdde275b4207585f612f80dc11d34903faf92
Ayinde K. Williams <ayindetec10@gmail.com>
2018-09-12 14:44:36 -0500

    Creating a commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. This
+* 2.
+* 3.
+* 4.
 *
 * write out a code example of each explanation above
 */
