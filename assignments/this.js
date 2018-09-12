95b5b8f43c5236f8f43008c2c9a48a46b09854ac
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-12 11:10:42 -0700

    setup repo

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding
+* 2. Implicit Binding
+* 3. New Binding
+* 4. Explicit Binding
 *
 * write out a code example of each explanation above
 */
