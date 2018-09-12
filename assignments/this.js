ea19ae38e7ce75c36efb1ba295fa5a8db03c995c
Angelo Deleon <rudy209@comcast.net>
2018-09-12 14:59:28 -0700

    finished this.js questions

--- assignments/this.js
+++ assignments/this.js
@@ -9,18 +9,65 @@
 * write out a code example of each explanation above
 */
 
-// Principle 1
 
+
+
+// Principle 1
 // code example for Window Binding
+function sayName(name) {
+    console.log(this);
+    return name;
+  }
+  sayName("D'Artagnan");
+
 
 // Principle 2
-
 // code example for Implicit Binding
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+      console.log(`${this.greeting} my name is ${name}`);
+      console.log(this);
+    }
+  };
+  myObj.sayHello('Ryan');
+
+
 
 // Principle 3
-
 // code example for New Binding
+function CordialPerson(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
+  
+  newman.speak();
+  newman.speak();
+
 
 // Principle 4
-
 // code example for Explicit Binding
+keyword (Function Methods!)
+
+const personInfo = {
+  'name': 'Bob'
+}
+
+const skills = ['HTML', 'CSS', 'JS'];
+const moreSkills = [' more HTML', 'CSS', 'JS'];
+
+
+function introduce(skills1, skills2, skills3){
+  console.log(`Hello!  My name is: ${this.name} and I like to program in: ${skills1}, ${skills2}, ${skills3}`)
+}
+
+introduce.call(personInfo, ...skills);
+
+Or use .apply(array) or .bind(save as new var);
