95632b3ea122885ab2964ec5d03b05acc7693ea9
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-12 14:33:26 -0500

    this assignment done

--- assignments/this.js
+++ assignments/this.js
@@ -1,27 +1,53 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. This when used globally will try binding to the window
+* 2. Implicit binding will allow you use to use keywords from within your block scope
+* 3. Explicit binding allows us to attach multiple arrays and or objects to a function so we can use this within the function
+* 4. Used within a constructor function that is used to build objects
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
-//      console.log(this);
+//  console.log(this);
 
 // Principle 2
 
-// code example for Implicit Binding
+    // const darkWizard = {
+    //     'name': 'Grumpy',
+    //     'spell_book': 'Dark wizard book',
+    //     'gender': 'male',
+    //     'saying': function() {
+    //       console.log(`I'm ${this.name} and I use the ${this.spell_book}`)
+    //     }
+    //  }
+    //  darkWizard.saying();
 
 // Principle 3
 
-// code example for New Binding
+    // const newChar = {
+    //   'name': 'Bob',
+    //   'skills': 'Cooks a mean burger'
+    // }
+    
+    // const foodArr = ['lettuce', 'tomato', 'mayo']
+    
+    // function letsCook(food1, food2) {
+    //   console.log(`${this.name} makes burgers with ${food1} and ${food2}`)
+    // }
+    
+    // letsCook.call(newChar, ...foodArr)
 
 // Principle 4
 
-// code example for Explicit Binding
+    //function ninja(weapon, color, fightStyle) {
+    //   this.outfit = color;
+    //   this.weapon = weapon;
+    //   this.style = fightStyle;
+    // }
+    
+    // const lazyNinja = new ninja('Throwing star', 'blue', 'sleepy')
+    
+    // console.log(lazyNinja)
