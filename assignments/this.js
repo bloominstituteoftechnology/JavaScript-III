40504fe9c844a2d0309de5b3d237c7ae69d47146
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-12 16:00:27 -0500

    finished this.js

--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,42 @@
 // Principle 1
 
 // code example for Window Binding
-
+function sayAge (age) {
+    console.log(this);
+    return age;
+}
+// sayAge(5);
 // Principle 2
 
 // code example for Implicit Binding
+const brandon = {
+    greeting: 'hey',
+    sayName: function (name) {
+        console.log(`${this.greeting} I'm ${name}`);
+    }
+};
 
+brandon.sayName('Brandon');
 // Principle 3
 
 // code example for New Binding
-
+const anthony = {
+    'name':'Anthony',
+}
+const learning = ['HTML', 'CSS', 'JS'];
+function intro(skills1, skills2, skills3) {
+    console.log(`Hello my name is ${this.name} and I am learning ${skills1}, ${skills2}, ${skills3}`);
+}
+intro.call(anthony, ...learning);
 // Principle 4
 
 // code example for Explicit Binding
+function Human(person) {
+    this.greeting = 'Hello ';
+    this.person = person;
+    this.talk = function() {
+        console.log(this.greeting + this.person);
+    };
+}
+const harry = new Human('Harry');
+harry.talk();
