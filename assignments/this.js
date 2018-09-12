f8334b431a33f3a120ceb16553c1ff40eede6378
Katie Cleary <katiecleary@airmaster.lan>
2018-09-12 15:58:33 -0400

    answered discussion questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding - if "this" is used while in global scope, it will bind to the window object;
+* 2. Implicit Binding -used when referring to a property within an object, the word this followed by .
+* 3. New binding - used when creating an object constructor. When the constructor is used to create a specific instance 
+the format of a new binding tells the parameters to attach in the new object being created from the constructor
+* 4. Explicit binding -
 *
 * write out a code example of each explanation above
 */
