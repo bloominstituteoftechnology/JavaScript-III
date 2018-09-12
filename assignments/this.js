08f0bc070e93ac6a17a80e0480a5da81a6885bc5
SavannahGreuel <smallidgesavannah@gmail.com>
2018-09-12 13:49:20 -0600

    initial commit, this questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When you use this in the global scope, the "this" value will be the window. It will pull up alot of things you dont want it to pull up.
+* 2. Implicit binding is used when dot notation is used to invoke a function. whatever is left of the dot is it's context 
+* 3. New binding is  used when a constructor function is used. 
+* 4. Explicit binding is when call or apply explicitly defines this
 *
 * write out a code example of each explanation above
 */
