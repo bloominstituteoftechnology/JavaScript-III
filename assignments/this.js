816e9c411c792c33cac7491bbc0ec5c1488fe696
Hargobind Atwal <ethaipath@gmail.com>
2018-09-12 13:14:21 -0700

    init commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. If this is called in the gobal scope it will dreference the window.
 * 2. 
 * 3. 
 * 4. 
