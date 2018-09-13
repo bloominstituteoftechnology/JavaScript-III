d7ee585ed62ebbd9bafd3c7d75fe8f67e535cf55
jarheadm249 <36177264+jarheadm249@users.noreply.github.com>
2018-09-12 23:30:14 -0400

    Steve Alverson

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,13 +7,21 @@
   
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.dimensions = attributes.dimensions;
+}
+GameObject.prototype.destroy = function() {
+  return "Object was removed from the game."
+}
+
 
 /*
   === CharacterStats ===
@@ -22,6 +30,17 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attr) {
+  GameObject.call(this, attr)//properties
+  this.hp = attr.hp;
+  this.name = attr.name;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype)//methods
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`
+}
+
 
 /*
   === Humanoid ===
@@ -32,7 +51,21 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(human) {
+  CharacterStats.call(this, human) //properties
+  this.faction = human.faction;
+  this.weapons = human.weapons;
+  this.language = human.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype) //methods
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}`
+}
+
+
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +74,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +135,84 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+// Stretch task:
+// * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
+// * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+function Hero(superman) {
+  Humanoid.call(this, superman)
+}
+Hero.prototype = Object.create(Humanoid.prototype)
+Hero.prototype.attack = function(object) {
+  object.hp -= 1; 
+  if(object.hp === 0) {
+    return `${object.destroy()}`;
+  } else {
+    return `${object.takeDamage()} You now have ${object.hp} HP`;
+  }
+}
+
+const kent = new Hero({
+  createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 10,
+    name: 'Clark',
+    faction: 'DC Universe',
+    weapons: [
+      'Fists',
+      'Kick',
+    ],
+    language: 'krypton',
+});
+
+
+
+
+function Villain(joker) {
+  Humanoid.call(this, joker)
+}
+
+Villain.prototype = Object.create(Humanoid.prototype)
+Villain.prototype.attack = function(object) {
+  object.hp -= 1; 
+  if(object.hp === 0) {
+    return `${object.destroy()}`;
+  } else {
+    return `${object.takeDamage()} You now have ${object.hp} HP`;
+  }
+}
+
+
+const wayne = new Villain({
+  createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 10,
+    name: 'Bruce',
+    faction: 'DC Universe',
+    weapons: [
+      'Batmobile',
+      'Batplane',
+    ],
+    language: 'Batinese',
+});
+
+
+console.log(kent.attack(wayne));
+console.log(wayne.attack(kent));
+console.log(kent.attack(wayne));
+console.log(wayne.attack(kent));
+console.log(kent.attack(wayne));
+console.log(wayne.attack(kent));
+console.log(kent.attack(wayne));
+console.log(wayne.attack(kent));
