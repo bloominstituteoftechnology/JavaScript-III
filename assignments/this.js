355737ae2080e8e2d3b86a89c24745dcdd079e9b
Zatara-Wrought <zatara.forever@gmail.com>
2018-09-13 09:37:31 -0700

    -insert self deprecating comment here-

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,39 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. By default 'this' attaches to window/global object.
+* 2. If 'this' is called within object method scope, 'this' will attach to the object and look for property matches.
+* 3. If the 'new' keyword is used, 'this' will default to the properties associated.
+* 4. If .call(), .apply(), or .bind() are employed, the 'this' context is manually set. 
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
-// code example for Window Binding
+this.name = "hello world";
+ console.log(this.name);
 
 // Principle 2
 
-// code example for Implicit Binding
+const Obj = {
+  name: "Sam",
+  food: "Steak",
+  speak: () => {
+      console.log(`${this.name} loves ${this.food}.`);
+  }
+}
+Obj.speak();
 
 // Principle 3
-
-// code example for New Binding
+function Human(attributes){
+  this.name = attributes.name;
+  this.age = attributes.birthday;
+}
+const Sam = new Person ({
+  name: "carlo",
+  birthday: "June 25th"
+})
 
 // Principle 4
 
-// code example for Explicit Binding
+newObj.speak.call(obj);
