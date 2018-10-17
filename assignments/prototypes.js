d3ac29c0fa24fa5ddc8c94fcdb5ec42516f1de04
Trai Lynne <trailynne@outlook.com>
2018-10-17 16:29:09 -0600

    Established inheritance from CharacterStats prototype to Humanoid

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -60,29 +60,19 @@ CharacterStats.prototype.takeDamage = function() {
 
 // == Constructor Function ==
 function Humanoid(attributes) {
+    CharacterStats.call(this, attributes);
     this.faction = attributes.faction;
     this.weapons = attributes.weapons;
     this.language = attributes.language;
 }
 
 // == Constructor Methods ==
+Humanoid.prototype = Object.create(CharacterStats.prototype);
 Humanoid.prototype.greet = function() {
     return `${this.name} offers greeting in ${this.language}`;
 }
 
 // Testing
-const trai = new Humanoid({
-    faction: 'Libra',
-    klanguage: 'English',
-    weapons: [
-        'dagger',
-        'spells',
-        'twin pistols'
-    ]
-});
-
-console.log(trai);
-console.log(trai.greet());
 
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -92,67 +82,67 @@ console.log(trai.greet());
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
+
+/*const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
+        length: 2,
+        width: 1,
+        height: 1,
     },
     hp: 5,
     name: 'Bruce',
     faction: 'Mage Guild',
     weapons: [
-      'Staff of Shamalama',
+        'Staff of Shamalama',
     ],
     language: 'Common Toungue',
-  });
+});
 
-  const swordsman = new Humanoid({
+const swordsman = new Humanoid({
     createdAt: new Date(),
     dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
+        length: 2,
+        width: 2,
+        height: 2,
     },
     hp: 15,
     name: 'Sir Mustachio',
     faction: 'The Round Table',
     weapons: [
-      'Giant Sword',
-      'Shield',
+        'Giant Sword',
+        'Shield',
     ],
     language: 'Common Toungue',
-  });
+});
 
-  const archer = new Humanoid({
+const archer = new Humanoid({
     createdAt: new Date(),
     dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
+        length: 1,
+        width: 2,
+        height: 4,
     },
     hp: 10,
     name: 'Lilith',
     faction: 'Forest Kingdom',
     weapons: [
-      'Bow',
-      'Dagger',
+        'Bow',
+        'Dagger',
     ],
     language: 'Elvish',
-  });
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.hp); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.faction); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 */
 
 // Stretch task: 
