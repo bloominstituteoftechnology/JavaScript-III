3e1fd0bd5e9cf3ed666ce70c3d184a17c2b1499b
Trai Lynne <trailynne@outlook.com>
2018-10-17 14:18:31 -0600

    Switched parts of my explicit and implict definitions

--- assignments/this.js
+++ assignments/this.js
@@ -5,9 +5,9 @@
 
 * 1. Window/Global Object Binding is the automatic defining of the 'this' keyword as the entire window that is being rendered since it is an object. 
 
-* 2. Implicit Binding is the definition of the 'this' keyword to any object invoking a globally defined method. So something.doSomething() makes 'something' equal to 'this'. It is implied that the attached object is 'this' thing being called upon. That specific object and all its attributes are being defined as 'this'.
+* 2. Implicit Binding is the defenition of 'this' is the definition of the 'this' keyword as a specific object that has been defined and is usually followed by one of its attributes or methods. So something.doSomething() makes 'something' equal to 'this'. It is implied that the attached object is 'this' thing being called upon. That specific object and all its attributes are being defined as 'this'.
 
-* 3. Explicit Binding is the binding of 'this' is the definition of the 'this' keyword as a specific object that has been defined and is usually followed by one of its attributes or methods. When invoking a globally defined function, the specific object equal to 'this' must be .call()ed with it's attributes individually inserted based on the parameters of the method, .apply()ed with an array of its attributes  inserted as the parameters, or .bind()ed and stored as a variable to be used for later.
+* 3. Explicit Binding is the definition of the 'this' keyword to any object invoking a globally defined method. When invoking a globally defined function, the specific object equal to 'this' must be .call()ed with it's attributes individually inserted based on the parameters of the method, .apply()ed with an array of its attributes  inserted as the parameters, or .bind()ed and stored as a variable to be used for later.
 
 * 4. New Binding is the definition of the 'this' keyword as the specific object being created by a constructor method to the attributes that are attached and specifically defined for the object within the prototype or template of the object.
 *
