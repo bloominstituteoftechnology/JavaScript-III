3074409b8f2ac2d78d663e15dd9866d11e13e2d6
jamespagedev <jamespagedev@outlook.com>
2018-10-17 12:35:48 -0700

    defined principles in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. If this is used in the global scope, it refers to the window of that scope.
+* 2. Using dot notation, this can be encapsulated within the next scope.
+* 3. If you use this in a class's constructor method, it will refer to the instantiated object
+* 4. obj.call(), obj.apply() and obj.bind() are prototypes for classes for allowing this to be explicitely defined where it points to
 *
 * write out a code example of each explanation above
 */
