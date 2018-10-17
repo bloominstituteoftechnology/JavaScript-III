5d12ee5df0ccc40cad8ece6790500842304c4809
Lidii <lidiiany92@gmail.com>
2018-10-17 16:47:30 -0400

    Finihed this.js assignment

--- assignments/this.js
+++ assignments/this.js
@@ -32,8 +32,46 @@
 
 
 * 3. New Binding:
-    
-* 4. 
+    Whenever a constructor function is used, this refers to the specific instance of 
+    the object that is created and returned by the constructor function.
+
+        // Constructor Function
+    // --------------------
+    function GreetMe(name) {
+        this.greeting = 'Hello ';
+        this.name = name;
+        this.speak = function() {
+            console.log(this.greeting + this.name);
+            console.log(this);
+        }
+    };
+
+    var greetJohn = new GreetMe('John');
+    var greetJane = new GreetMe('Jane');
+
+    greetJohn.speak();
+    greetJane.speak();
+
+
+    In the example above, both the greetJohn and greetJane variables are assigned a unique object returned by 
+    the GreetMe constructor function. Therefore, this inside of the speak method points to the unique GreetMe 
+    object instance stored in the variable on which the speak method is being called. This is evident by looking at 
+    the name property on the GreetMe object.
+
+* 4. Explicit Binding:
+    Whenever JavaScript’s call or apply method is used, this is explicitly defined.
+
+    greetJohn.sayGoodbye.call(greetJane); //this points to greetJane
+    greetJane.sayGoodbye.apply(greetJohn); //this points to greetJohn
+
+    JavaScript’s call and apply methods allow you to execute a function in a different context. 
+    The first argument passed to either of these methods explicitly sets what this points to. 
+    Because call and apply are explicit, they present the clearest case of what this points to. 
+    This is evident by looking at the name property logged to the console by each invocation of the sayGoodbye method.
+
+
+
+
 *
 * write out a code example of each explanation above
 */
