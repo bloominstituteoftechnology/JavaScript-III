107efa6fa83d1d3bd9433b3cb7451466b02869f9
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-10-17 15:51:27 -0500

    Completed all required objectives.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,9 +1,11 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
+  Object oriented design is commonly used in video games.  
+  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
 
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  
+  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
@@ -14,6 +16,13 @@
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+function GameObject(stats){
+  this.createdAt = stats.createdAt;
+  this.dimensions = stats.dimensions;
+}
+GameObject.prototype.destroy = function(){
+  return `${this.name} was removed from the game.`;
+}
 
 /*
   === CharacterStats ===
@@ -22,6 +31,16 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(stats){
+  GameObject.call(this, stats);
+  this.hp = stats.hp;
+  this.name = stats.name;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage.`;
+}
+
 
 /*
   === Humanoid ===
@@ -32,6 +51,18 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid(stats){
+  CharacterStats.call(this, stats);
+  this.faction = stats.faction;
+  this.weapons = stats.weapons;
+  this.language = stats.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +72,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +133,20 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+  // function Villan(stats){
+  //   Humanoid.call(this, stats);
+  // }
+  // Villan.prototype = Object.create(Humanoid.prototype);
+
+
+  // function Hero(stats){
+  //   Humanoid.call(this.stats);
+  // }
+  // Hero.prototype = Object.create(Humanoid.prototype);
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding -- default binding of the this keyword to the window or DOM, the biggest element in the browser.
+* 2. Implicit Binding -- imply this winthin an object to the object itself. or this reffereing to the block of code the this keyword iis in.
+* 3. Explicit Binding -- asigning the value of this to an object using .call .apply and .bind
+* 4. new Binding -- binding to a new object within a constructor function.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,52 @@
 // Principle 1
 
 // code example for Window Binding
+function world(){
+  "use strict"
+  console.log(this)
+}
 
+world();
 // Principle 2
 
 // code example for Implicit Binding
+const obj = {
+      name: 'someThing',
+      food: 'apple',
+      find: function(){
+        console.log(`${this.name} found ${this.food}.`);
+      }
+    }
+    
+    obj.find();
+// Principle 3 new
 
-// Principle 3
+function Man(name){
+    this.name = name;
+    this.weapon = 'Fists';
+    this.speak = function(){
+      console.log(`He uses his ${this.weapon}. They call him ${this.name}.`);
+    }
+  }
+  
+  const joe = new Man('Stray Dog');
+  const Reinhardt = new Man('German Power');
+  
+  joe.speak();
+  Reinhardt.speak();
 
-// code example for New Binding
+// Principle 4  explicit
 
-// Principle 4
-
-// code example for Explicit Binding
+const person = {
+      name: 'Arnold'
+    }
+    
+    const food = ['Bannan', 'slurpie', 'chocolate'];
+    
+    function announce(skills1, skills2, skills3){
+      "use strict"
+      console.log(`Hello!  My name is ${this.name} and this is what i ate. ${skills1}, ${skills2}, ${skills3}.`)
+    }
+    announce.call(person, food[0], food[1], food[2]);
+    announce.apply(person, food);
+    announce.bind(person, food);
