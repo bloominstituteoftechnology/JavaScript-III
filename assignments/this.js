302829401260dc17370e1e7cffda2c94c8647837
cbtsao47 <cbtsao47@gmail.com>
2018-10-17 16:01:27 -0500

    initial commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,47 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding (default because the window itself is an object)
+* 2. implicit binding (for object methods)
+* 3. explicit binding (for functions)
+* 4. new key word binding
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
+this;
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+const obj = {
+  firstName: "ben",
+  lastName: 'tsao',
+  fullName: function() {
+    console.log(`${this.firstName} ${this.lastName}`);
+  }
+};
+obj.fullName();
 
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function (){
 
+;}
+
+// Principle 4
 // code example for Explicit Binding
+
+const me = {
+    name: 'ben',
+    age: 99
+};
+
+const myskills =['confused','lurk','question mark'];
+function introduce(skill1,skill2,skill3){
+    console.log(`Hai! I'm ${this.name} and these are my skills: ${skill1}, ${skill2}, ${skill3}`);
+};
+introduce.apply(me,myskills);
