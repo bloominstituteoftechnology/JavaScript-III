ea0f83c5b332a24fb0ba4adf25a3b9f686b1605c
ToMMaaY <thuynh629@gmail.com>
2018-10-17 14:36:19 -0700

    Completed the stretch goals for prototype.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -21,7 +21,7 @@ function GameObject(info){
 }
 
 GameObject.prototype.destroy = function() {
-  return 'Object was removed from the game.';
+  return `${this.name} was removed from the game.`;
 }
 
 /*
@@ -64,7 +64,43 @@ Humanoid.prototype = Object.create(CharacterStats.prototype);
 Humanoid.prototype.greet = function(){
   return `${this.name} offers a greeting in ${this.language}`;
 }
+
+// Hero
+
+function Hero(heroAttributes){
+  Humanoid.call(this, heroAttributes);
+  this.attackPwr = heroAttributes.attackPwr;
+  this.status = heroAttributes.status;
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+Hero.prototype.attack = function(hero, attackee){
+  attackee.hp -= hero.attackPwr;
+  if (attackee.hp > 0) {
+    return `${hero.name} attacked ${attackee.name} and dealt ${hero.attackPwr} damage. ${attackee.name} has ${attackee.hp} hp left.`;
+  } else if (attackee.hp <= 0){
+    return `${hero.name} has defeated ${attackee.name}!`;
+  }
+}
+
+// Villain
  
+function Villain(villainAttributes){
+  Humanoid.call(this, villainAttributes);
+  this.attackPwr = villainAttributes.attackPwr;
+  this.status = villainAttributes.status;
+}
+
+Villain.prototype = Object.create(Humanoid.prototype);
+Villain.prototype.attack = function(villain, attackee){
+  attackee.hp -= villain.attackPwr;
+  if (attackee.hp > 0) {
+    return `${villain.name} attacked ${attackee.name} and dealt ${villain.attackPwr} damage. ${attackee.name} has ${attackee.hp} hp left.`;
+  } else if (attackee.hp <= 0){
+    return `${villain.name} has defeated ${attackee.name}!`;
+  }
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -124,6 +160,44 @@ Humanoid.prototype.greet = function(){
     language: 'Elvish',
   });
 
+  const batman = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    hp: 30,
+    name: 'Batman',
+    faction: 'Gotham Avengers',
+    weapons: [
+      'Ninja Stars',
+      'Awesome Tools',
+    ],
+    language: 'English',
+    attackPwr: 5,
+    status: 'Super Hero'
+  });
+
+  const joker = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 5,
+    },
+    hp: 30,
+    name: 'The Joker',
+    faction: 'Mayham of Gothem',
+    weapons: [
+      'Bombs',
+      'Guns',
+    ],
+    language: 'English',
+    attackPwr: 4,
+    status: 'Evil Villain'
+  });
+
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.hp); // 15
@@ -134,9 +208,29 @@ Humanoid.prototype.greet = function(){
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  // Hero and Villan
 
+  console.log(batman);
+  console.log(joker);
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+  // Batman vs Joker
+  console.log('----------Batman battles The Joker!!!----------')
+
+  console.log(batman.attack(batman, joker));
+  console.log(joker.attack(joker, batman));
+  console.log(batman.attack(batman, joker));
+  console.log(joker.attack(joker, batman));
+  console.log(batman.attack(batman, joker));
+  console.log(joker.attack(joker, batman));
+  console.log(batman.attack(batman, joker));
+  console.log(joker.attack(joker, batman));
+  console.log(batman.attack(batman, joker));
+  console.log(joker.attack(joker, batman));
+  console.log(batman.attack(batman, joker)); // Batman defeats Joker
+
+  
