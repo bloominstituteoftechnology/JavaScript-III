6d2f2550a2c0b2421bcf4496ead6c981781693b4
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-10-17 12:46:30 -0700

    add answers to first Qs on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding: when none of the above apply, this points to the window. 
+* 2. implicit binding: looking to the left of the dot at call time
+* 3. new binding: this is bound to the new object being constructed, whenever a function is invocted with the new key word
+* 4. explicet binding: tellign a function what the context of this is goign to be using call, apply, or bind
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,7 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this.location);
 
 // Principle 2
 
