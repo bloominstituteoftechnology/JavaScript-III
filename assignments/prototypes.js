00fe5b3cc5b3e769de2845c3364fc604dbad3410
iradsanchez <ira.d.sanchez@gmail.com>
2018-10-22 20:49:24 -0700

    Troubleshooting the inheritance of destroy and takeDamage but otherwise am done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,11 +1,18 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
+  Object oriented design is commonly used in video games.  
+  For this part of the assignment you will be implementing 
+  several constructor functions with their correct inheritance 
+  heirarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: 
+  GameObject, CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up 
+  inheriting from Humanoid.  Use the objects at the bottom of 
+  the page to test your constructor functions.
   
-  Each constructor function has unique properites and methods that are defined in their block comments below:
+  Each constructor function has unique properites and methods 
+  that are defined in their block comments below:
 */
   
 /*
@@ -15,6 +22,15 @@
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
 
+function GameObject(characterClass){
+  this.createdAt = characterClass.createdAt;
+  this.dimensions = characterClass.dimensions;
+}
+
+GameObject.prototype.destroy = function(){
+  return 'Object was removed from the game.';
+}
+
 /*
   === CharacterStats ===
   * hp
@@ -22,6 +38,16 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(character) {
+  GameObject.call(this, character);
+  this.hp = character.name;
+  this.name = character.name;
+}
+
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} took damage.`;
+}
 
 /*
   === Humanoid ===
@@ -32,7 +58,18 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(user) {
+  GameObject.call(this, user);
+  CharacterStats.call(this, user);
+  this.faction = user.faction;
+  this.weapons = user.weapons;
+  this.language = user.language;
+}
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +78,7 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -100,9 +137,9 @@
   console.log(mage.weapons); // Staff of Shamalama
   console.log(archer.language); // Elvish
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
