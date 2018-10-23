c350a33c085077bd6de8dba353cfdb188dd49fd7
unknown <bj2336@email.vccs.edu>
2018-10-22 20:47:01 -0700

    this Binding Questions Answered

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding, the default binding for this, where this refers to the Window Object. Can be overridden with "use strict"
+* 2. Implicit Binding, when JavaScript determines where this points, to know look to the left of the dot in the function call, thisObject.function()
+* 3. Explicit Binding, when You determine where this points, the function containing this is standalone, and must be couple with .call(), .apply(), or .bind() to know what this is.
+* 4. New Binding, allows you to use an obect template to create new instances of an object which is used as this for the method being called.
 *
 * write out a code example of each explanation above
 */
