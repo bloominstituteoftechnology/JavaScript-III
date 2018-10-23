020ef617eba967cbd38bad48777cf3ada8e7bf4a
Jonathan Dorety <kyullog@gmail.com>
2018-10-23 13:09:13 -0400

    Complete discussion questions in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,18 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding
+When this is used on the global scale, it refers to the global (in the case of a browser, window) object
+
+* 2. Implicit binding
+This is the common binding for properties or functions created inside an object.  If they are called by using dot notation, the this refers to the object
+to the immediate left of the dot of the property or function being called.
+
+* 3. Explicit binding
+this is bound specifically and clearly to an object by the call(), apply() or bind() methods.
+
+* 4. New binding
+Used within constructors to bind this to each individual instance of an Object returned by that constructor.
 *
 * write out a code example of each explanation above
 */
