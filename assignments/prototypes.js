05decba56e4884e356812fa228e51d35e4cdb78b
dickwinters <coding1965@gmail.com>
2018-10-23 22:21:47 -0500

    Strech

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -121,18 +121,154 @@ const archer = new Humanoid({
   language: 'Elvish',
 });
 
-console.log(mage.createdAt); // Today's date
-console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-console.log(swordsman.hp); // 15
-console.log(mage.name); // Bruce
-console.log(swordsman.faction); // The Round Table
-console.log(mage.weapons); // Staff of Shamalama
-console.log(archer.language); // Elvish
-console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-console.log(mage.takeDamage()); // Bruce took damage.
-console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+// console.log(mage.createdAt); // Today's date
+// console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+// console.log(swordsman.hp); // 15
+// console.log(mage.name); // Bruce
+// console.log(swordsman.faction); // The Round Table
+// console.log(mage.weapons); // Staff of Shamalama
+// console.log(archer.language); // Elvish
+// console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+// console.log(mage.takeDamage()); // Bruce took damage.
+// console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 //
 // Stretch task:
 // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
+
+function Hero(heroAttributes) {
+  Humanoid.call(this, heroAttributes);
+  this.heroDefend = function(attacker, weapon) {
+    const strong = 10;
+    const mild = 5;
+    const weak = 2;
+    let hit = '';
+    for (let i = 0; i < this.weapons.length; i++) {
+      if (this.weapons[i] === weapon && i === 0) {
+        attacker.hp -= strong;
+        hit = strong;
+      } else if (this.weapons[i] === weapon && i === 1) {
+        attacker.hp -= mild;
+        hit = mild;
+      } else if (this.weapons[i] === weapon && i === 2) {
+        attacker.hp -= weak;
+        hit = weak;
+      }
+    }
+    console.log(`${attacker.name} struck by ${weapon}. Lost ${hit}  `);
+    console.log(`${attacker.name}'s health is at ${attacker.hp}`);
+    console.log('');
+
+    if (attacker.hp <= 0) {
+      console.log(`${attacker.name} has been defeated`);
+    }
+  };
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+
+function Villian(villianAttributes) {
+  Humanoid.call(this, villianAttributes);
+  this.villianAttack = function(defender, weapon) {
+    const strong = 10;
+    const mild = 5;
+    const weak = 2;
+    let hit = '';
+    for (let i = 0; i < this.weapons.length; i++) {
+      if (this.weapons[i] === weapon && i === 0) {
+        defender.hp -= strong;
+        hit = strong;
+      } else if (this.weapons[i] === weapon && i === 1) {
+        defender.hp -= mild;
+        hit = mild;
+      } else if (this.weapons[i] === weapon && i === 2) {
+        defender.hp -= weak;
+        hit = weak;
+      }
+    }
+    console.log(`${defender.name} struck by ${weapon}. Lost ${hit}  `);
+    console.log(`${defender.name}'s health is at ${defender.hp}`);
+    console.log('');
+
+    if (defender.hp <= 0) {
+      console.log(`${defender.name} has been defeated`);
+    }
+  };
+}
+
+Villian.prototype = Object.create(Humanoid.prototype);
+
+const lancelot = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 7,
+    width: 7,
+    height: 5,
+  },
+  hp: 100,
+  name: 'Lancelot',
+  faction: 'Round Table',
+  weapons: ['Sword', 'Spear', 'Shield'],
+  language: 'Common Toungue',
+});
+
+const blackKnight = new Villian({
+  createdAt: new Date(),
+  dimensions: {
+    length: 9,
+    width: 5,
+    height: 7,
+  },
+  hp: 105,
+  name: 'Black Knight',
+  faction: 'Mystics of the night',
+  weapons: ['Mace', 'Sword', 'Axe'],
+  language: 'Unknown',
+});
+
+// lancelot.heroDefend(blackKnight, 'Sword');
+// lancelot.heroDefend(blackKnight, 'Spear');
+// lancelot.heroDefend(blackKnight, 'Shield');
+//
+// blackKnight.villianAttack(lancelot, 'Mace');
+// blackKnight.villianAttack(lancelot, 'Sword');
+// blackKnight.villianAttack(lancelot, 'Axe');
 // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villian and one a hero and fight it out with methods!
+//
+
+blackKnight.villianAttack(lancelot, 'Sword');
+blackKnight.villianAttack(lancelot, 'Mace');
+lancelot.heroDefend(blackKnight, 'Shield');
+lancelot.heroDefend(blackKnight, 'Shield');
+lancelot.heroDefend(blackKnight, 'Sword');
+blackKnight.villianAttack(lancelot, 'Mace');
+blackKnight.villianAttack(lancelot, 'Axe');
+lancelot.heroDefend(blackKnight, 'Sword');
+blackKnight.villianAttack(lancelot, 'Axe');
+blackKnight.villianAttack(lancelot, 'Sword');
+lancelot.heroDefend(blackKnight, 'Sword');
+lancelot.heroDefend(blackKnight, 'Sword');
+lancelot.heroDefend(blackKnight, 'Sword');
+blackKnight.villianAttack(lancelot, 'Axe');
+blackKnight.villianAttack(lancelot, 'Mace');
+lancelot.heroDefend(blackKnight, 'Shield');
+blackKnight.villianAttack(lancelot, 'Sword');
+blackKnight.villianAttack(lancelot, 'Axe');
+lancelot.heroDefend(blackKnight, 'Spear');
+lancelot.heroDefend(blackKnight, 'Spear');
+lancelot.heroDefend(blackKnight, 'Spear');
+blackKnight.villianAttack(lancelot, 'Mace');
+blackKnight.villianAttack(lancelot, 'Sword');
+lancelot.heroDefend(blackKnight, 'Spear');
+lancelot.heroDefend(blackKnight, 'Spear');
+lancelot.heroDefend(blackKnight, 'Sword');
+blackKnight.villianAttack(lancelot, 'Mace');
+blackKnight.villianAttack(lancelot, 'Axe');
+lancelot.heroDefend(blackKnight, 'Sword');
+blackKnight.villianAttack(lancelot, 'Axe');
+blackKnight.villianAttack(lancelot, 'Sword');
+lancelot.heroDefend(blackKnight, 'Sword');
+lancelot.heroDefend(blackKnight, 'Sword');
+lancelot.heroDefend(blackKnight, 'Sword');
+blackKnight.villianAttack(lancelot, 'Axe');
+blackKnight.villianAttack(lancelot, 'Mace');
