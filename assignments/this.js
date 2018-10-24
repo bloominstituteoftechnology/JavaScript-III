00fb9b7192bb2b17e9f8c7cb9b6a35a098021d89
Joseph Eastman <josephryan96@gmail.com>
2018-10-24 12:23:51 -0700

    this.js MVP

--- assignments/this.js
+++ assignments/this.js
@@ -12,18 +12,59 @@
 // Principle 1
 
 // code example for Window Binding
+function windowBinding() {
+    console.log(`Example of window binding: ${this}`);
+}
+
+windowBinding();
 
 
 // Principle 2
 
 // code example for Implicit Binding
+const jedi = {
+    name: 'Obi-Wan',
+    weapon: 'Lightsaber',
+    useLightsaber: function() {
+        console.log(`${this.name} prepares to use his ${this.weapon}`)
+    }
+}
+
+jedi.useLightsaber();
 
 
 // Principle 3
 
 // code example for New Binding
+function Jedi2(person) {
+    this.person = person;
+    this.greeting = 'Hello I\m';
+    this.speak = function() {
+        console.log(`${this.greeting} ${this.person}.`)
+    }
+}
+
+const anakin = new Jedi2('Anakin');
+const maceWindu = new Jedi2('Mace Windu');
+
+anakin.speak();
+maceWindu.speak();
 
 
 // Principle 4
 
 // code example for Explicit Binding
+const jedi3 = {
+    name: 'Yoda'
+}
+
+const skills = ['Knowledge', 'Meditation', 'Strategy'];
+
+function showOff(skills1, skills2, skills3) {
+    console.log(`My name is ${this.name} and my skills are: ${skills1}, ${skills2}, ${skills3}.`);
+}
+
+showOff.call(jedi3, ...skills);
+showOff.apply(jedi3, skills);
+const shownOff = showOff.bind(jedi3, ...skills);
+shownOff();
