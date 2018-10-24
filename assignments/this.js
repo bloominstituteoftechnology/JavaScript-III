b5cae36955e3192a98c7c683f7a100ddea33c2c0
Connor Blanco <conblanco95@gmail.com>
2018-10-24 15:09:17 -0700

    completed the this.js file, referenced with Josh Knell's video and Cam's video

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,88 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding- Don't do this. ...seriously. If you do not implicate what `this` is, it will default "point" to the whole window when it is used. Window is the default binding for `this`.
+
+* 2. Implicit Binding- "Left of the dot when invoked", implied though not plainly expressed. This means we didn't tell `this` to do this, it just does. We didn't have to say, "set this", or "this is signed to", JS itself does it.
+
+* 3. New Binding- Constructor function (capitalize the variable), a new way of making objects that we haven't done before.
+
+* 4. Explicit Binding- The opposite of Implicit, meaning it is stated clearly and in detail, leaving no room for confusion or doubt. In this case we have to state it clearly. CALL: `.call()` method calls a function with a given `this` value and arguments provided individually. / APPLY: `.apply()` method calls a function with a given `this` value, and `arguments` provided as an array (or an array-like object) / BIND: `.bind()` method creates a new function that, when called, has it's `this` keyword set to the provided value, with a given sequence of arguments preceding any provided when the new function is called.
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 (Window/Global Object Binding)
+const windowFunction = function(){
+  console.log(this);
+}
+// invoke with:
+// windowFunction();
 
-// code example for Window Binding
+// Principle 2 (Implicit Binding)
 
-// Principle 2
+const flower = {
+  name: 'Lotus',
+  bloom: function() {
+    console.log(`The ${this.name} has bloomed`);
+  }
+  genus: {
+    name: 'Loteae',
+    sayGenus: function() {
+      console.log(`This is the genus: ${this.name}`);
+    }
+  }
+}
 
-// code example for Implicit Binding
+flower.bloom();
 
-// Principle 3
+flower.genus.sayGenus();
+// Principle 3 (New Binding)
 
-// code example for New Binding
+const Animal = function (color, name, type) {
+  this.color = color
+  this.name = name
+  this.type = type
+}
 
-// Principle 4
+const myCat = new Animal('white and black', 'wall-e', 'cat');
 
-// code example for Explicit Binding
+console.log(myCat);
+
+// Principle 4 (Explicit Binding)
+// CALL
+const person = {
+  name: 'Emma',
+}
+
+let favoriteBooks = ['Dewey', 'The Shining', 'The man who was Thursday', 'Odyssey']
+
+function introduce(book1, book2, book3, book4) {
+  console.log(`Hello! My name is ${this.name} and these are my favorite books ${book1}, ${book2}, ${book3}, ${book4}.`)
+}
+
+introduce.call(person, favoriteBooks[0], favoriteBooks[1], favoriteBooks[2], favoriteBooks[3]);
+
+// APPLY
+const person = {
+  name: 'Emma',
+}
+
+let favoriteBooks = ['Dewey', 'The Shining', 'The man who was Thursday', 'Odyssey']
+
+function introduce(book1, book2, book3, book4) {
+  console.log(`Hello! My name is ${this.name} and these are my favorite books ${book1}, ${book2}, ${book3}, ${book4}.`)
+}
+
+introduce.apply(person, favoriteBooks);
+
+// BIND
+const person = {
+  name: 'Emma',
+}
+let favoriteBooks = ['Dewey', 'The Shining', 'The man who was Thursday', 'Odyssey']
+function introduce(book1, book2, book3, book4) {
+  console.log(`Hello! My name is ${this.name} and these are my favorite books ${book1}, ${book2}, ${book3}, ${book4}.`)
+}
+const newIntroduce = introduce.bind(person, favoriteBooks[0], favoriteBooks[1], favoriteBooks[2], favoriteBooks[3]);
+newIntroduce();
