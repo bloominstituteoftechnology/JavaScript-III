1ebf568f36dd34f0940095da909141b4800e12ab
brettmadrid <brettmadrid9@gmail.com>
2018-10-24 00:26:12 -0700

    completed stretch goal
    Created two new objects: A Villian and a Hero Object which both inherit from
    the Humanoid object.  Both new objects have their own prototype methods which they use to do battle with each other.  Finally they do battle and one of them
    finally wins

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -65,9 +65,31 @@ Humanoid.prototype = Object.create(CharacterStats.prototype);
 Humanoid.prototype.greet = function() {
   return `${this.name} offers a greeting in ${this.language}.`;
 }
+
+// New Villian Object Inherits from Humanoid
+function Villian(villianAttributes) {
+    Humanoid.call(this, villianAttributes);
+}
+
+Villian.prototype = Object.create(Humanoid.prototype);
+
+Villian.prototype.mindSpell = function() {
+    return `${this.name} just cast a spell on your mind causing you to lose 5 health points.`
+}
+
+// New Hero Object Inherits from Humanoid
+function Hero(heroAttributes) {
+  Humanoid.call(this, heroAttributes);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+
+Hero.prototype.counterSpell = function() {
+  return `${this.name} just cast a counter spell on your mind causing you to lose 7 health points.`
+}
  
 /*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+  * Inheritance chain: GameObject -> CharacterStats -> Humanoid -> Villian and Hero
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
@@ -127,6 +149,40 @@ console.log(mage);
     language: 'Elvish',
   });
 
+  const badman = new Villian({
+      createdAt: new Date(),
+      dimensions: {
+          length: 1,
+          width: 2,
+          height: 4,
+      },
+      hp: 15,
+      name: 'Shoeface',
+      faction: 'House of Evil',
+      weapons: [
+          'Spells',
+          'Wand',
+      ],
+      language: 'Orcish',
+  });
+
+  const goodguy = new Hero({
+      createdAt: new Date(),
+      dimensions: {
+          length: 2,
+          width: 1,
+          height: 2,
+      },
+      hp: 15,
+      name: 'Aelforn',
+      faction: 'People of the West',
+      weapons: [
+          'Spells',
+          'Magic Sword',
+      ],
+      language: 'Elvish',
+  });
+
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.hp); // 15
@@ -138,7 +194,29 @@ console.log(mage);
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
+
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+  // New Villian Object inherited from Humanoid Object
+  console.log(badman)
+  console.log(badman.mindSpell());
+  goodguy.hp -= 5;
+  console.log(goodguy.hp);
+  // New Hero Object inherited from Humanoid Object
+  console.log(goodguy);
+  console.log(goodguy.counterSpell());
+  badman.hp -= 7;
+  console.log(badman.hp);
+  console.log(badman.mindSpell());
+  goodguy.hp -= 5;
+  console.log(goodguy.hp);
+  console.log(goodguy.counterSpell());
+  badman.hp -= 7;
+  console.log(badman.hp);
+  console.log(badman.mindSpell());
+  goodguy.hp -= 5;
+  console.log(goodguy.hp);
+  console.log(goodguy.destroy());
