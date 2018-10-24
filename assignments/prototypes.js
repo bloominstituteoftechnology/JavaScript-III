d6ab807e8399a5b7987e478a20661aa8f0932046
Alando Appling <alandoappling@gmail.com>
2018-10-23 20:27:16 -0700

    Finished MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,13 +1,13 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance heirarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
+
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -32,7 +32,7 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +41,63 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
+  function GameObject(attr){
+    this.createdAt = attr.createdAt;
+    this.dimensions = attr.dimensions;
+  }
+  GameObject.prototype.destroy = function(){
+    console.log(`${this.name} was removed from the game`);
+  }
+
+  CharacterStats.prototype = Object.create(GameObject.prototype);
+  Humanoid.prototype = Object.create(CharacterStats.prototype);
+  Villian.prototype = Object.create(Humanoid.prototype);
+  Hero.prototype = Object.create(Humanoid.prototype);
+
+  function CharacterStats(characterAttr){
+    GameObject.call(this, characterAttr);
+    this.hp = characterAttr.hp;
+    this.name = characterAttr.name;
+  }
+  CharacterStats.prototype.takeDamage = function(){
+    return `${this.name} took damage.${this.hp} health left`
+  }
+
+  function Humanoid(humanoidAttr){
+    CharacterStats.call(this, humanoidAttr);
+    this.faction = humanoidAttr.faction;
+    this.weapons = humanoidAttr.weapons;
+    this.language = humanoidAttr.language;
+  }
+
+  function Hero(heroAttr){
+    Humanoid.call(this, heroAttr);
+  }
+
+  Hero.prototype.blindFury = function(object){
+    if(object.hp <= 0){
+      return object.destroy();
+    }
+    object.hp--;
+    return object.takeDamage();
+    console.log("hp removal")
+  }
+
+  function Villian(villianAttr){
+    Humanoid.call(this, villianAttr)
+  }
+  Humanoid.prototype.greet = function(){
+    return `${this.name} offers a greeting in ${this.language}.`
+  }
+
+  const kenshi = new Hero({
+    hp:20,
+    name:'Kenshi',
+    faction: 'mk10',
+    weapons: ['katana'],
+    language: 'common tongue'
+  });
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -91,8 +147,14 @@
     ],
     language: 'Elvish',
   });
-
-  console.log(mage.createdAt); // Today's date
+  console.log(kenshi.blindFury(mage));
+  console.log(kenshi.blindFury(mage));
+  console.log(kenshi.blindFury(mage));
+  console.log(kenshi.blindFury(mage));
+  console.log(kenshi.blindFury(mage));
+  console.log(kenshi.blindFury(mage));
+  console.log(kenshi.blindFury(mage));
+  console.log(mage.hp); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.hp); // 15
   console.log(mage.name); // Bruce
@@ -102,9 +164,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+
+  // Stretch task:
+  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villian and one a hero and fight it out with methods!
