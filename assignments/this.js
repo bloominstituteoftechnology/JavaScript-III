93c7e2c7417165e5b73287e5315ba836f045bab4
Eiosx <eiosxenzo@gmail.com>
2018-10-23 23:12:53 -0400

    Answered questions for 'this'.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - When 'this' refers to the window/console object
+* 2. Implicit Binding - When 'this' refers to the object left of a method in dot notation
+* 3. Explicit Binding - When 'this' is explicitly stated in a method such as call,apply, or bind.
+* 4. New Binding - When 'this' refers to a specific instance of a constructor function that was made.
 *
 * write out a code example of each explanation above
 */
