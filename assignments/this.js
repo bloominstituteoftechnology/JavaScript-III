508999443eb2db427074287f8fc36b5363b54179
Bill Geist <wcgeist@gmail.com>
2018-10-23 21:36:43 -0400

    prototypes

--- assignments/this.js
+++ assignments/this.js
@@ -11,42 +11,42 @@
 
 // Principle 1
 
-function favTeam(team) {
-    console.log(this);
-    return team;
+// function favTeam(team) {
+//     console.log(this);
+//     return team;
     
-  }
-  console.log(favTeam("Ravens"));
+//   }
+//   console.log(favTeam("Ravens"));
 
 
-// Principle 2
+// // Principle 2
 
-const myTeam = {
-    sport: "Football",
-    sayTeamName: function(name) {
-      console.log(`The ${name} are my favorite ${this.sport} team`);
-      console.log(this);
-    }
-  };
-  myTeam.sayTeamName("Ravens");
+// const myTeam = {
+//     sport: "Football",
+//     sayTeamName: function(name) {
+//       console.log(`The ${name} are my favorite ${this.sport} team`);
+//       console.log(this);
+//     }
+//   };
+//   myTeam.sayTeamName("Ravens");
 
-// Principle 3
+// // Principle 3
 
-function ArmyNavy(serviceSchool, oponent) {
-    this.team1 = serviceSchool;
-    this.team2 = oponent;
-    this.goTeam = function() {
-      console.log(`Go ${this.team1}, Beat ${this.team2}`);
-      console.log(this);
-    };
-  }
+// function ArmyNavy(serviceSchool, oponent) {
+//     this.team1 = serviceSchool;
+//     this.team2 = oponent;
+//     this.goTeam = function() {
+//       console.log(`Go ${this.team1}, Beat ${this.team2}`);
+//       console.log(this);
+//     };
+//   }
   
-  const army = new ArmyNavy('Army', 'Navy');
-  const navy = new ArmyNavy('Navy', 'Army');
+//   const army = new ArmyNavy('Army', 'Navy');
+//   const navy = new ArmyNavy('Navy', 'Army');
   
-  army.goTeam();
-  navy.goTeam();
+//   army.goTeam();
+//   navy.goTeam();
 
-// Principle 4
+// // Principle 4
 
-army.goTeam.call(navy); navy.goTeam.apply(army);
+// army.goTeam.call(navy); navy.goTeam.apply(army);
