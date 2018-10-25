96aa49765b3b686a73ce2071fc98c33214343e32
Eiosx <eiosxenzo@gmail.com>
2018-10-25 15:24:33 -0400

    Completed MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,20 +7,20 @@
   
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
-function GameObject(createdAt, dimensions){
-  this.createdAt = createdAt;
-  this.dimensions = dimensions;
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.dimensions = attributes.dimensions;
 
 }
-GameObject.prototype.destroy = function(){
-  return 'Object was removed from the game.'
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`
 }
 
 /*
@@ -30,6 +30,17 @@ GameObject.prototype.destroy = function(){
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(attributes) {
+  GameObject.call(this, attributes);
+  this.hp = attributes.hp;
+  this.name = attributes.name;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage`
+}
 
 /*
   === Humanoid ===
@@ -40,7 +51,19 @@ GameObject.prototype.destroy = function(){
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid(attributes) {
+  CharacterStats.call(this, attributes);
+  this.faction = attributes.faction;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -49,68 +72,68 @@ GameObject.prototype.destroy = function(){
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Bruce',
-    faction: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Toungue',
-  });
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    hp: 15,
-    name: 'Sir Mustachio',
-    faction: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Toungue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Bruce',
+  faction: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Toungue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    hp: 10,
-    name: 'Lilith',
-    faction: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  hp: 15,
+  name: 'Sir Mustachio',
+  faction: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Toungue',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  hp: 10,
+  name: 'Lilith',
+  faction: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.hp); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.faction); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
 
   // Stretch task: 
   // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding - When 'this' refers to the window/console object
-* 2. Implicit Binding - When 'this' refers to the object left of a method in dot notation
+* 2. Implicit Binding - When 'this' refers to the object left of a method in dot/bracket notation.
 * 3. Explicit Binding - When 'this' is explicitly stated in a method such as call,apply, or bind.
 * 4. New Binding - When 'this' refers to a specific instance of a constructor function that was made.
 *
@@ -19,7 +19,7 @@ console.log(this);
 // code example for Implicit Binding
 const me = {
     name: "Evan",
-    sayName: function(){
+    sayName: function () {
         console.log(this.name);
     }
 }
@@ -28,22 +28,45 @@ me.sayName();
 // Principle 3
 
 // code example for New Binding
-const Weapon = function(name,type,level){
-    this.name = name;
-    this.type = type;
-    this.level = level;
-    this.description = function(){
-        console.log(`You have picked up ${this.name}, it is a ${this.type}, and is level ${this.level}`);
+const VideoGameObject = function (attributes) {
+    this.name = attributes.name;
+    this.type = attributes.type;
+    this.level = attributes.level;
+    this.description = function () {
+        console.log(`${this.name} is a ${this.type}, and is level ${this.level}`);
     }
 }
-sword = new Weapon('Death Eater', 'Sword', '90');
+sword = new VideoGameObject({
+    name: "Death Eater",
+    type: "Sword",
+    level: 90
+})
 sword.description();
 
 
 // Principle 4
 
 // code example for Explicit Binding
-const wepBroke = function(){
-    console.log(`${this.name} broke! You are unarmed...`)
+const SwordMaster = function (attributes) {
+    VideoGameObject.call(this, attributes);
+    this.weaponName = attributes.weaponName,
+        this.weaponEffect = attributes.weaponEffect
 }
-wepBroke.call(sword);
+
+SwordMaster.prototype = Object.create(VideoGameObject.prototype);
+
+SwordMaster.prototype.weaponDescription = function () {
+    console.log(`${this.weaponName}: ${this.weaponEffect}`)
+}
+
+const player1 = new SwordMaster({
+    name: "Eios",
+    type: "SwordMaster",
+    level: 93,
+    weaponName: "Muramasa",
+    weaponEffect: "Effect will initiate once in battle. Constantly craving blood, Muramasa's base damage will increase with each foe slain, up to a max multiplier of 3x. If no foe is slain within 15 seconds, Muramasa will feast off the life of it's owner at a rate of 10 hp per second."
+})
+
+
+player1.weaponDescription();
+
