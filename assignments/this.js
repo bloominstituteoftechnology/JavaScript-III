3a59e2e47c08a9e9dd5b2b43040dc06c2ca8d5e8
Ej <ejaona@gmail.com>
2018-11-20 11:05:43 -0600

    completed this.js objectives. Created examples for implicit, explicit, new, and window binding.

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,91 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Implicit binding is when the refrence to the 'this' key word that sits inside a method of an object
+ * is automatically assigned to the name of the object that it's being invoked on.
+ *
+ *
+ * 2. Explicit binding is when you must assign a refrence to the 'this' key word by using .apply, .call, or .bind
+ * to point at what 'this' should means.
+ *
+ * 3. New binding is when you use a constructor function to creat a new object that recieves all the
+ * properties (and refrence to 'this') of the constructor itself.
+ *
+ * 4. Window binding happens when none of the other three binding methods are met. The 'this' key word then looks to
+ * window object for refrence.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
 // code example for Window Binding
+const becky = {
+  name: "Becky",
+  age: 56,
+  gender: "Female",
+
+  speak: function() {
+    console.log(`My name is, ${this.name}`);
+  }
+};
+
+speak();
 
 // Principle 2
 
 // code example for Implicit Binding
+const spencer = {
+  name: "Spencer",
+  age: 28,
+  gender: "Male",
 
+  speak: function() {
+    console.log(`My name is, ${this.name}`);
+  }
+};
+
+spencer.speak();
 // Principle 3
 
 // code example for New Binding
+function Hero(name, power, city) {
+  this.name = name;
+  this.power = power;
+  this.city = city;
+
+  this.action = (name, city, power) => {
+    console.log(
+      `My name is ${this.name} and i'm saving ${
+        this.city
+      } from crime, with my ${this.power}`
+    );
+  };
+}
+
+const Batman = new Hero("Batman", "Money", "Gothom");
+
+Batman.action();
 
 // Principle 4
 
 // code example for Explicit Binding
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+}
+
+const larry = new Person("Larry", 25, "Male");
+
+const brad = {
+  name: "Brad",
+  age: 45
+};
+
+function speak() {
+  console.log(`My name is, ${this.name} and i am ${this.age} `);
+}
+
+speak.call(larry);
+speak.apply(brad);
