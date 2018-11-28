97c50cd82934d601367e5d90ccf59c9425a10643
Matt Basile <matthewbasile2@gmail.com>
2018-11-28 15:11:52 -0500

    Answer question 2 about implicit binding. LINES: this.js 5,18-25.

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding - The default this assignment is our window. This essentially is all of JavaScript in one object. We do not want this to occur in our code as it generally shows that we've missed assigned our this keyword. 
-* 2. Implicit Binding
+* 2. Implicit Binding - Implicit, or automatic binding, occurs when we store functions in objects. When we invoke a function, the object preceded the .functionName() is our this keyword.   
 * 3. Explicit Binding 
 * 4. New Keyword Binding
 *
@@ -10,14 +10,19 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
 console.log(this);
 
 // Principle 2
-
 // code example for Implicit Binding
-
+const myObj = {
+    test: "testing our this keyword",
+    demo: function(){
+        console.log(this.test); //Returns "testing our this keyword"
+        console.log(this); // Returns our full object - myObj
+    }
+}
+myObj.demo();
 // Principle 3
 
 // code example for New Binding
