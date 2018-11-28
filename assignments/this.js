a285df3f395c72e1e53f446d97be0348b06f8783
rmejia-tajc <rmejia.tajc@gmail.com>
2018-11-28 13:32:31 -0700

    complete this.js principle 1 & 2

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binding - references the window object - this is "the whole forest."
+* 2. Implicit Binding - this refers to the odject before the dot when a function is called.
+* 3. New binding - this refers to the object created using the construcor function.
+* 4. Explicit binding - this is defined by using the apply or call method.
 *
 * write out a code example of each explanation above
 */
@@ -13,9 +13,27 @@
 
 // code example for Window Binding
 
+function test1() {
+    console.log(this);
+  }
+  
+  test1();
+
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const students = {
+    boys: 7,
+    girls: 9,
+    both: function() {
+      console.log(this.boys + this.girls);
+    }
+  }
+
+  students.both();
+
 
 // Principle 3
 
@@ -24,3 +42,6 @@
 // Principle 4
 
 // code example for Explicit Binding
+
+
+
