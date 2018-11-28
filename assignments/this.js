1e8dd34fc399ae0ec0cc3deefe03e8db8e0d491f
Brandon Lent <blent1050@yahoo.com>
2018-11-28 15:41:29 -0500

    this.js principles in my own words completed

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,12 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window Binding:
-* 2. Impliciit Binding:
-* 3. New Binding:
-* 4. Explicit Binding:
+* 1. Window Binding: Would make this equal to the window. I really liked Josh's example of the forest -> `this` equals the entire forest. *Bad behavior
+* 2. Impliciit Binding: To know what `this` means in this context, you look to the left of the dot notation where you are invoking a function. 
+* 3. New Binding: JavaScript will create a brand new object for us and save it as this. So when you reference something like an Animal into a variable zebra
+    `this` would mean zebra.{Your reference} in this situation.
+* 4. Explicit Binding: You tell what the `this` statement exactly references. You can do this on a function by calling Animal.call(zebra) and `this` would be 
+    the same as stating zebra.{your-reference} in this context.
 *
 * write out a code example of each explanation above
 */
