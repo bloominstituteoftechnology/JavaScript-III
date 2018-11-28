a241f36840578676124778df3bc5a65830397075
cecil john tantay <cjbtantay@gmail.com>
2018-11-28 15:02:12 -0800

    prototypes.js done

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding is when the object is bound to the global scope, `this` context will be bound in the window or console Object
+* 2. Implicit Binding occurs when the `this` keyword is located in the same object as the 
+* 3. Explicit Binding
+* 4. New Binidng
 *
 * write out a code example of each explanation above
 */
