f5005e61fcd1699a88b7d9039d955a4aff053f01
Matt Basile <matthewbasile2@gmail.com>
2018-11-28 15:20:06 -0500

    Answer question 3. LINES: this.js 6, 27-35.

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. Window Binding - The default this assignment is our window. This essentially is all of JavaScript in one object. We do not want this to occur in our code as it generally shows that we've missed assigned our this keyword. 
 * 2. Implicit Binding - Implicit, or automatic binding, occurs when we store functions in objects. When we invoke a function, the object preceded the .functionName() is our this keyword.   
-* 3. Explicit Binding 
+* 3. Explicit Binding - Unlike implicit binding, explicit is not automatic, meaning we need to assign the this keyword. To this this we need to use a method, either .apply() or .call(). This will allow us to set the this value for our function depending on the parameters we pass to it. We can also use .bind() and hold this in a function but we'll learn more about this later.
 * 4. New Keyword Binding
 *
 * write out a code example of each explanation above
@@ -12,7 +12,6 @@
 // Principle 1
 // code example for Window Binding
 console.log(this);
-
 // Principle 2
 // code example for Implicit Binding
 const myObj = {
@@ -24,8 +23,18 @@ const myObj = {
 }
 myObj.demo();
 // Principle 3
-
 // code example for New Binding
+const me ={
+    name:"Matt",
+    location: "Brooklyn"
+};
+
+function howdy(){
+    "use strict"
+    console.log(`Howdy partner, my name is ${this.name} and I'm from ${this.location}!`);
+}
+
+howdy.call(me);
 
 // Principle 4
 
