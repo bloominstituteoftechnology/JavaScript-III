68e279f8a7220d1d46a6f0df145e2d05d245fdd3
matt0418 <mfeldma7@gmail.com>
2018-11-28 18:39:52 -0500

    Completed the project for the day, as well as all stretch goals

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
 
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.dimensions = attributes.dimensions;
+}
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+}
+
 /*
   === CharacterStats ===
   * healthPoints
@@ -22,6 +30,17 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(characterAttributes) {
+  GameObject.call(this, characterAttributes)
+  this.healthPoints = characterAttributes.healthPoints;
+  this.name = characterAttributes.name;
+}
+CharacterStats.prototype = Object.create(GameObject.prototype)
+CharacterStats.prototype.takeDamage = function() {
+  console.log(`${this.name} took damage.`);
+}
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,6 +52,59 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(humanAttributes) {
+  CharacterStats.call(this, humanAttributes)
+  this.team = humanAttributes.team;
+  this.weapons = humanAttributes.weapons;
+  this.language = humanAttributes.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype)
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
+
+// Villain Constructor 
+function Villian(villianAttributes) {
+  Humanoid.call(this, villianAttributes)
+  this.lair = villianAttributes.lair;
+}
+Villian.prototype = Object.create(Humanoid.prototype)
+Villian.prototype.laugh = function() {
+  console.log(`Haha Haha! (menacingly)`);
+}
+Villian.prototype.attack = function(chr) {
+  
+  if (chr.healthPoints <= 0) {
+    console.log(chr.destroy());
+  } else {
+    chr.healthPoints -= 5;
+  }
+  console.log(chr.healthPoints);
+};
+
+//Hero Constructor 
+
+
+function Hero(heroAttributes) {
+  Humanoid.call(this, heroAttributes);
+  this.base = heroAttributes.base;
+}
+  Hero.prototype = Object.create(Humanoid.prototype);
+  Hero.prototype.heroSpeak = function() {
+  return `This is a test!!!`;
+};
+  Hero.prototype.attack = function(chr) {
+  
+  if (chr.healthPoints <= 0) {
+    console.log(chr.destroy());
+  } else {
+    chr.healthPoints -= 5;
+  }
+  console.log(chr.healthPoints);
+};
+
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +113,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -92,6 +164,40 @@
     language: 'Elvish',
   });
 
+  const bigBadVillian = new Villian({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 3,
+      height: 5,
+    },
+    healthPoints: 25,
+    name: 'Uruk',
+    team: 'Dark Ones',
+    weapons: [
+      'Staff',
+      'Wand',
+    ],
+    language: 'Dark Tongue',
+    lair: 'Haunted Castle',
+  });
+
+  const bestHeroEver = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2
+    },
+    healthPoints: 50,
+    name: "The Hero",
+    team: "Hero Team",
+    weapons: ["Hero Sword", "Shield"],
+    language: "Hero Speak",
+    base: "Test Base"
+  });
+
+
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.healthPoints); // 15
@@ -99,10 +205,30 @@
   console.log(swordsman.team); // The Round Table
   console.log(mage.weapons); // Staff of Shamalama
   console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(archer.greet());
+  mage.takeDamage(); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+  console.log(bigBadVillian);
+  console.log(bestHeroEver.base);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+  bigBadVillian.attack(bestHeroEver);
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,58 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  Window Binding - Where 'this' binds to the window, when there is nothing else to bind to
+* 2.  Implicit Binding - It binds to the object containing the function
+* 3.  Explicit Binding - It binds to what we say it is supposed to bind to using call, bind or apply
+* 4.  New Binding - 'This' binds to the newly created objects
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+console.log(this)
 
 // Principle 2
 
-// code example for Implicit Binding
+const example = {
+    name: 'object example',
+    make_example: function() {
+        console.log(`${this.name}`)
+    }
+}
 
 // Principle 3
 
-// code example for New Binding
+const man = {
+    name: 'guy'
+}
+const interests = ['coding', 'games', 'extracurricular activities']
+
+function whats_ur_interests (interest1, interest2, interest3) {
+    console.log(`${interest1}, ${interest2} , ${interest3}`)
+};
+whats_ur_interests.call(man, interests)
+
+
 
 // Principle 4
 
-// code example for Explicit Binding
+const bigBadVillian = new Villian({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 3,
+      height: 5,
+    },
+    healthPoints: 25,
+    name: 'Uruk',
+    team: 'Dark Ones',
+    weapons: [
+      'Staff',
+      'Wand',
+    ],
+    language: 'Dark Tongue',
+    lair: 'Haunted Castle',
+  });
+
