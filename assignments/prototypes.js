625e2fc6513e31a593dc735a96e0a176fcf43968
Kingaa6 <Kingaa6@gmail.com>
2018-11-28 14:22:22 -0800

    Villian constructor added

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -49,17 +49,22 @@ function CharacterStats(characterAttibutes) {
   this.takeDamage = function () {
     return `${this.name} took damage.`
   }
+  this.death = function () {
+    if (this.healthPoints === 0) {
+      return `TEST`;
+    }
+  }
 }
+// testing CharacterStats Constructor - confirmed working
+// const tester2 = new CharacterStats({
+//   dimensions: 'who cares',
+//   healthPoints: 10,
+//   name: 'Tom'
+// });
 
-const tester2 = new CharacterStats({
-  dimensions: 'who cares',
-  healthPoints: 10,
-  name: 'Tom'
-});
+// console.log(tester2);
 
-console.log(tester2);
-
-console.log(tester2.destroy());
+// console.log(tester2.destroy());
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -74,23 +79,21 @@ console.log(tester2.destroy());
 function Humanoid(humanoidAttributes) {
   CharacterStats.call(this, humanoidAttributes);
   this.team = humanoidAttributes.team;
-  this.weapons = humanoidAttributes.weapon;
+  this.weapons = humanoidAttributes.weapons;
   this.language = humanoidAttributes.language;
   this.greet = function () {
     return `${this.name} offers a greeting in ${this.language}.`
   }
 }
-
-const tester3 = new Humanoid({
-  dimensions: '12',
-  healthPoints: 20,
-  name: 'Jo',
-  team: "teamteam",
-  weapon: "star",
-  language: "yes"
-});
-
-
+// testing Humanoid Constructor - confirmed working
+// const tester3 = new Humanoid({
+//   dimensions: '12',
+//   healthPoints: 20,
+//   name: 'Jo',
+//   team: "teamteam",
+//   weapon: "star",
+//   language: "yes"
+// });
 
 /*
  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -100,70 +103,116 @@ const tester3 = new Humanoid({
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
+
+
+console.log(mage);
+console.log(swordsman);
+console.log(archer);
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+
 
 // Stretch task: 
-// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+
+function Villian(villianAttributes) {
+  Humanoid.call(this, villianAttributes);
+  this.fireBomb = function (opponent) {
+    console.log(opponent.takeDamage());
+    --opponent.healthPoints;
+    if (opponent.healthPoints === 0) {
+      console.log(opponent.destroy());
+    }
+  }
+}
+
+const dingus = new Villian({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 7,
+  },
+  healthPoints: 25,
+  name: 'Dingus',
+  team: 'Death to Hero',
+  weapons: [
+    'Fire'
+  ],
+  language: 'EvilAF',
+});
+
+console.log(dingus.fireBomb(mage));
+console.log(mage.healthPoints);
+console.log(dingus.fireBomb(mage));
+console.log(mage.healthPoints);
+console.log(dingus.fireBomb(mage));
+console.log(mage.healthPoints);
+console.log(dingus.fireBomb(mage));
+console.log(mage.healthPoints);
+console.log(dingus.fireBomb(mage));
+
+
+
 // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
 // * Create two new objects, one a villain and one a hero and fight it out with methods!
