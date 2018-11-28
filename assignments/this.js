0f39c55eecce3ddc83a8ef464cbeec98e01a8f1d
Mike Landers <landers.mike@gmail.com>
2018-11-28 12:48:51 -0800

    Initial Commit * Explained 4 priciples of 'this' * Complete examples of each priciple

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,61 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global - binds 'this' to the Window object. Window being the browswer.
+* 2. Implicit - binds 'this' to the object in which the method is being invoked on
+* 3. New - binds 'this' to the new object the constructor function creates
+* 4. Explicit -  whenever .call , .apply, .bind method is used then 'this' is explictely defined to the arugment passed in
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 - Window Binding
 
-// code example for Window Binding
+    function sayName(name){
+        console.log(`Hello, this`)
+        return name;
+    }
+    sayName('Mike')
 
-// Principle 2
+// Principle 2 - Implicit Binding
 
-// code example for Implicit Binding
+    const user = {
+        greeting: 'Hello',
+        sayHello: function(name){
+            console.log(`${this.greeting}, ${name}`)
+        }
+    }
+    user.sayHello('Mike')
 
-// Principle 3
+// Principle 3 - New Binding
+    //New Person constructor
+    function NewPerson(attributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.weight = attributes.weight;
+        this.speak = function(){
+            return `Hello, my name is ${this.name} and I am ${this.age} years old.`
+        }
+    }//NewPerson
 
-// code example for New Binding
+    const mike = new NewPerson({
+        name: 'Mike',
+        age: 33,
+        weight: 2190
+    });
 
-// Principle 4
+    const joe = new NewPerson({
+        name: 'Joe',
+        age: 35,
+        weight: 200
+    });
 
-// code example for Explicit Binding
+    console.log(mike);
+    console.log(mike.speak());
+    console.log(joe)
+    console.log(joe.speak());
+
+// Principle 4 - Explicit Binding
+
+console.log(mike.speak.call(joe));
+console.log(joe.speak.apply(mike));
