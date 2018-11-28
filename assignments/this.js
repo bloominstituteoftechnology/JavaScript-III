95bf4f9308e0df09668f0b9f1ee961ff7c521c40
Imon Ovbude <imonovbude@gmail.com>
2018-11-28 14:15:01 -0800

    answered 3 questions and coded 3 examples

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
+* 1. window binding - points or bound to the whole window.
+* 2. implicit binding - points to the object when attached to a key.
+* 3. new binding - pass in the object you want to the this keyword to be bound to
 * 4. 
 *
 * write out a code example of each explanation above
@@ -12,14 +12,30 @@
 // Principle 1
 
 // code example for Window Binding
+function hi() {
+	console.log(this);
+}
+hi();
 
 // Principle 2
 
 // code example for Implicit Binding
+const person = {
+	name: 'Mike',
+	food: 'Pancakes',
+	cook: function() {
+		console.log(`${this.name} likes to eat ${this.food}`);
+	}
+};
 
 // Principle 3
 
 // code example for New Binding
+function intro() {
+	console.log(`Hi, my name is ${this.name}`);
+}
+
+intro.call(person);
 
 // Principle 4
 
