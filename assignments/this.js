f0f38bc5610faee399bec8a350e3ed9749a36979
Jaouad Sarouti <jadsarouti@outlook.com>
2018-11-28 14:49:01 -0500

    prototypes.js finished

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding 
+* 2. implicit binding
+* 3. explicit binding
+* 4. new object binding
 *
 * write out a code example of each explanation above
 */
