9a52c7194c9accae008d0cb90021e8bb29c45833
Estevan <estevan.rodriguez10@yahoo.com>
2018-11-28 12:30:18 -0800

    started project and answered first principle

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. Window Binding: Unless otherwise specified, the this keyword will always point to the window.
 * 2. 
 * 3. 
 * 4. 
@@ -12,6 +12,7 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this)
 
 // Principle 2
 
