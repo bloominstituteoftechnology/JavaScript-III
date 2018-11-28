7f3dfbfbe1e28a2e50f518a0f3708733744fd814
Samsoucy <newf930@outlook.com>
2018-11-28 17:06:54 -0500

    finished this file

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Windows Binding - calls a stand alone function invocation.
-* 2. Implicit binding
-* 3. Explicit Binding
-* 4. New Binding
+* 2. Implicit binding - The object in front of the dot is what the 'this' key word is bound to.
+* 3. Explicit Binding - You can force a function call to use a particular object. You control this binding.
+* 4. New Binding - builds new objects.
 *
 * write out a code example of each explanation above
 */
@@ -15,16 +15,54 @@
     function myName() {
         console.log(this);
     }
-    greeting();
+    myName();
 
 // Principle 2
 
 // code example for Implicit Binding
+    const dogs = {
+        name: "Bolt",
+        food: "Kibble",
+        eat: function() {
+            console.log(`${this.name} eats ${this.food}.`);
+        }
+    }
+    dogs.eat();
 
 // Principle 3
 
 // code example for New Binding
 
+    function SadPerson(person){
+        this.person = person;
+        this.say = "I'm sad today";
+        this.speak = function(){
+            console.log(`${this.say}, ${this.person}`)
+        }
+    }
+
+    const paul = new
+    SadPerson("Sean");
+    const sean = new
+    SadPerson("paul");
+
+    console.log(paul);
+
+    paul.speak();
+    sean.speak();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+    const animal = {
+        species: "dog"
+    }
+    const playsWith = [" balls", " bones", "and rope"];
+
+    function showsUs() {
+        console.log(`Hello! I am a ${this.species}, and I like to play with ${playsWith}.`);
+    }
+
+
+    showsUs.call(animal, playsWith);
