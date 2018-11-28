ca588c602408bae37b4960c25a65b8eddd9270b4
Nathan Thomas <nwthomas@me.com>
2018-11-28 12:38:10 -0800

    Complete GameObject in prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,13 +7,22 @@
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
+
+function GameObject(attributes) {
+  this.createAt = attributes.createAt;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  return "Object was removed from the game.";
+};
 
 /*
   === CharacterStats ===
@@ -32,12 +41,12 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
@@ -104,7 +113,7 @@
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 */
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+// Stretch task:
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
--- assignments/this.js
+++ assignments/this.js
@@ -1,13 +1,21 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Window Binding
+ *    The window binding is when you (often times accidentally) direct the "this" keyword to refer to the Window object. For instance,
+ *    if you were to write a function in the global scope that did not "use strict" and that had a console.log(this), the "this" would
+ *    refer to the window object. You typically don't want this to happen.
+ *
+ * 2. Implicit Binding
+ *    Implicit binding is when you
+ *
+ *
+ *
+ * 3. Explicit Binding
+ * 4. New Binding
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
