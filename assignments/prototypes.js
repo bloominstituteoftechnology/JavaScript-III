f2fc70bb0588ccf756e140c61cd372e78a883af2
rosilioroman <rosilioroman@gmail.com>
2018-11-28 16:01:01 -0700

    Completed the tasks on this.js and started working on the stretch tasks in prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -68,70 +68,124 @@ Humanoid.prototype.greet = function() {
   * Instances of CharacterStats should have all of the same properties as GameObject.
 */
 
+//==============Stretch Goals===================
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+function Hero (heroAtts) {
+  Humanoid.call(this, heroAtts);
+}
+Hero.prototype = Object.create(Humanoid.prototype);
+Hero.prototype.heroicSlash = function(heroDmgDone) {
+  console.log(`${this.name} slashed the villain for ${heroDmgDone}!`);
+}
+
+function Villain (villAtts) {
+  Humanoid.call(this, villAtts);
+}
+Villain.prototype = Object.create(Humanoid.prototype);
+Villain.prototype.evilSlam = function(villainDmgDone) {
+  console.log(`${this.name} maliciously slams the hero for ${villainDmgDone}`);
+}
+
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+  team: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Tongue',
+});
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  healthPoints: 15,
+  name: 'Sir Mustachio',
+  team: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Tongue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Lilith',
+  team: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+const mainHero = new Hero ({
+  createdAt: new Date(),
+  dimensions: {
+    length: 10,
+    width: 15,
+    height: 20,
+  },
+  healthPoints: 100,
+  name: 'Anduin',
+  team: 'Alliance',
+  weapons: [
+    'Stormwind Sword of the Kings',
+  ],
+  language: 'Common Tongue',
+});
+
+const mainVillain = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  healthPoints: 10,
+  name: 'Sylvanas',
+  team: 'Horde',
+  weapons: [
+    'Legendary Bow of the Forsaken',
+  ],
+  language: 'Elvish',
+});
+
+console.log(`A hero and a villain enter the fight! *gasp*`);
--- assignments/this.js
+++ assignments/this.js
@@ -1,28 +1,86 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: the "this" keyword is bound to the window/Console if it isn't told what to bind to. (this is the forest)
+*
+* 2. Implicit Binding: "this" keyword binds to the object of the method being invoked. (the object to the left of the dot)
+*
+* 3. New Binding: this principle applies to the usage of constructor functions. When a constructor function is invoked to create a new object, the "new" keyword allows us to bind "this" 
+    to the instance of the object being created.
+*
+* 4. Explicit Binding: whenever we use the .call and .apply methods, we are explicitly deciding what the "this" keyword is bound to.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
+function helloWorld () {
+    console.log(this); //"this" is bound to the Window (aka the forest)
+    return 'Hello world!';
+}
+console.log(helloWorld());
 
 // Principle 2
-
 // code example for Implicit Binding
+const randObj = {
+    food: "pizza",
+    topping: "cheese",
+    shape: "square",
+    shareFood: function() {
+        console.log(`I will share my ${this.shape} ${this.topping} ${this.food}.`)
+    }
+}
+randObj.shareFood(); //"this" is bound to randObj
 
 // Principle 3
-
 // code example for New Binding
 
+//Constructor function for a Computer object
+function Computer(specs) {
+    this.cpu = specs.cpu;
+    this.gpu = specs.gpu;
+    this.ram = specs.ram;
+    this.powerOn = function(){
+        console.log("The PC is now ON");
+    }
+}
+
+//create a new Computer object
+const myCustomBuild = new Computer({
+    cpu: 'Intel i9 9900k',
+    gpu: 'RTX 2080ti',
+    ram: 'G.skill TridentZ 64GB'
+});
+
+myCustomBuild.powerOn();
+console.log(myCustomBuild.cpu);
+console.log(myCustomBuild.gpu);
+console.log(myCustomBuild.ram);
+
 // Principle 4
+// code example for Explicit Binding (derived from the previous principle's code)
+function Laptop(laptopSpecs) {
+    Computer.call(this, laptopSpecs); 
+    this.screenSize = laptopSpecs.screenSize;
+    this.usbPorts = laptopSpecs.usbPorts;
+    this.open = function() {
+        console.log(`*laptop is now open*`);
+    }
+}
 
-// code example for Explicit Binding
+const myNewLaptop = new Laptop({
+    cpu: 'Intel i5 5400',
+    gpu: 'no dedicated graphics card',
+    ram: 'Samsung 16GB',
+    screenSize: '15-inch',
+    usbPorts: '2 x USB Type C, 2 x USB3'
+});
 
-
+myNewLaptop.open();
+myNewLaptop.powerOn();
+console.log(myNewLaptop.cpu);
+console.log(myNewLaptop.gpu);
+console.log(myNewLaptop.ram);
+console.log(myNewLaptop.screenSize);
+console.log(myNewLaptop.usbPorts);
