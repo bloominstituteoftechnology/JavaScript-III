0c54a3909d23d22346e93c16e496379e83f35ba1
Estevan <estevan.rodriguez10@yahoo.com>
2018-11-28 12:55:56 -0800

    completed second principle in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window Binding: Unless otherwise specified, the this keyword will always point to the window.
-* 2. 
+* 2. Implicit Binding: When you invoke a function, the this refers to the left hand side of the invocation.
 * 3. 
 * 4. 
 *
@@ -15,7 +15,15 @@
 console.log(this)
 
 // Principle 2
+const person = {
+age: "10",
+name: "Estevan",
+greet: function(){
+console.log(`Hello my name is ${this.name}`)
+}
 
+}
+person.greet(); //person is what this refers to in this case 
 // code example for Implicit Binding
 
 // Principle 3
