7c2cf38ff2044b5fbb509b78b61815d99dbb0b97
Olivia Osborn <olioz95@gmail.com>
2018-11-28 15:37:10 -0500

    explained four principles for 'this' keyword in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/global binding -- when using "this" in the global scope, the value will be the window object
+* 2. Implicit binding -- when "this" is bound within a local scope, the value is equal to the object before the dot (when calling the function)
+* 3. New binding -- when a constructor function is used to create an object, the value of "this" is applied to the specific instance of the object that is created
+* 4. Explicit binding -- when you control the value of "this" by using a call or apply method
 *
 * write out a code example of each explanation above
 */
