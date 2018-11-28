ed4797cf2e2626198c017b8521cc088e3382885f
joepound <14357797+joepound@users.noreply.github.com>
2018-11-28 15:37:05 -0700

    Provided definition of principles in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,21 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window / Global Object Binding
+*    A currently uncommon use of `this` binding that is bound to the outermost context (global context - as in the area that is not bound inside the definition of another object) - this would mean the entire console or window, hence the name
+*
+*   This would be like implicit binding, only in this case, the use of `this` is not placed inside an object.
+*
+* 2. Implicit Binding
+*    This use of `this` is tied to the context of the object that it is placed inside of. It is like telling the compiler that one is referring to 'this' object that you are inside of.
+*
+*   It is particularly useful when an object's member and a parameter used by the object have the same name (`this` essentially is used to be more specific here).
+*
+* 3. `new` Binding
+*    This binding basically creates a new instance of an object (termed as instantiation). It is like considering the object as a user-defined data type and then creating a new variable of type [object].
+*
+* 4. Explicit Binding
+*    This binding makes use of one of three methods (call, bind & apply) that are used to override the default or implied binding of the current object. These methods change the context of `this` for that object into the parameter that was specified.
 *
 * write out a code example of each explanation above
 */
