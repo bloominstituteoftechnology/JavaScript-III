012bb4ba51557a562ee5f478f2d1e6b2b188a112
Estevan <estevan.rodriguez10@yahoo.com>
2018-11-28 13:30:19 -0800

    completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
 *
 * 1. Window Binding: Unless otherwise specified, the this keyword will always point to the window.
 * 2. Implicit Binding: When you invoke a function, the this refers to the left hand side of the invocation.
-* 3. 
-* 4. 
+* 3. New Binding: Allows you to create new objects using the new keyword and this keyword
+* 4. Explicit Binding: referes to the user choosing which things bind together in the code. 
 *
 * write out a code example of each explanation above
 */
@@ -29,7 +29,27 @@ person.greet(); //person is what this refers to in this case
 // Principle 3
 
 // code example for New Binding
+function Animal(type){
+    this.type = type;
+    this.greet = "OMG its a";
+    this.speak = function(){
+        console.log(`${this.greet} ${this.type}`)
+    }
+
+}
+
+const pup = new Animal("Puppy");
+pup.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+const me = {
+    name : "estevan"
+}
+
+function sayHi(){
+console.log(`Hi my name is ${this.name}`)
+}
+
+sayHi.call(me);
