d564676bacbcd7641cbeaa6b18c54c7b2d2032d9
David Flack <davidscottflack@gmail.com>
2018-11-28 14:26:45 -0600

    Answer third 'this' principle

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. 'this' can reference the window or console's object. (For example: it can reveal the methods on Console, such as .log)
 * 2. When a method is called, 'this' refers to the object to the left of the .
-* 3. 
+* 3. When using a constructor method, 'this' refers to the specific objects being created
 * 4. 
 *
 * write out a code example of each explanation above
