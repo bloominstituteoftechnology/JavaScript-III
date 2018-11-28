a7fb9287ca6d4d17fceb1a32143d1ed0b9568046
ltrii <lelandtr@gmail.com>
2018-11-28 13:38:15 -0800

    Final with stretch

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,7 @@
 * 1. Window Binding - Defaults to the window/console. 
 * 2. Implicit Binding - Also referred to as Object Binding. Whatever object is before the . (thisObject.example) will be the object that this binds to.
 * 3. New Binding - When using constuctor functions to make new objects based on existing ones, this will refer to the object where the variables were defined.
-* 4. Explicit Binding - 
+* 4. Explicit Binding - when using methods like "call" or "apply" this has been explicitly definied. 
 *
 * write out a code example of each explanation above
 */
@@ -24,8 +24,22 @@ function example2(stuff) {
 
 // Principle 3
 
-// code example for New Binding
+function stubbedToe(exclaim) {
+    this.greeting = 'OW! ';
+    this.exclaim = exclaim;
+    this.speak = function() {
+      console.log(this.greeting + this.exclaim);
+      console.log(this);
+    };
+  }
+  
+  const bigtoe = new stubbedToe('MY BIG TOE!');
+  const foot = new stubbedToe('MY FOOOOOOT!');
+
+
 
 // Principle 4
 
-// code example for Explicit Binding
+bigtoe.speak.call(bigtoe);
+
+console.log(bigtoe.speak);
