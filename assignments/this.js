f367d10814789ea560fe8f13fe50a541b5056a53
Matt Basile <matthewbasile2@gmail.com>
2018-11-28 15:03:32 -0500

    Answer question 1 for this. LINES: this.js 4,15

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - The default this assignment is our window. This essentially is all of JavaScript in one object. We do not want this to occur in our code as it generally shows that we've missed assigned our this keyword. 
+* 2. Implicit Binding
+* 3. Explicit Binding 
+* 4. New Keyword Binding
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,7 @@
 // Principle 1
 
 // code example for Window Binding
+console.log(this);
 
 // Principle 2
 
