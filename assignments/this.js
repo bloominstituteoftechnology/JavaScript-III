1464f0483a9112ace5b030c2a0cb32ce56601d57
Nathan Thomas <nwthomas@me.com>
2018-11-28 13:29:36 -0800

    Finish first 2/4 short answer questions about 'this' phrase

--- assignments/this.js
+++ assignments/this.js
@@ -7,11 +7,15 @@
  *    refer to the window object. You typically don't want this to happen.
  *
  * 2. Implicit Binding
- *    Implicit binding is when you
- *
- *
+ *    Implicit binding is when you bind this inside of an object; the this keyword will then bind to the object in questions, such
+ *    as when you are creating a key-value pair in a constructor function with this.name = attributes.name. It is important to note
+ *    that, in methods (inside of a constructor function), you have to use a normal ES5 function istead of an ES6 arrow function. If
+ *    you use an arrow, the "this" keyword inside of the method will bind a level above the constructor function (or any function when
+ *    it's used).
  *
  * 3. Explicit Binding
+ *    Explicit binding is when
+ *
  * 4. New Binding
  *
  * write out a code example of each explanation above
