14605bd7eb39e933edd3bb82a0af9c45c90f7ec2
Sheila <s.hopper01110@gmail.com>
2018-11-28 19:04:35 -0500

    JS III added this js

--- assignments/this.js
+++ assignments/this.js
@@ -9,17 +9,57 @@
 * write out a code example of each explanation above
 */
 
-// ========Principle 1
-// code example for Window Binding
+// ========Principle 1 -  Window Binding
+
+// function welcome() {
+//     console.log(this);
+// }
+// welcome();
+
+// // =========Principle 2 - Implicit Binding
+
+// const mydog = {
+//     name: 'Ruto',
+//     food: 'Taste of The Wild kibbles',
+//     eats: function() {
+//         console.log(`${this.name} eats some ${this.food}`);
+//     }
+// }
+
+// mydog.eats();
 
 
-// =========Principle 2
-// code example for Implicit Binding
+// // =========Principle 3 - New Binding
+// function RandomThing(dog){
+//     this.dog = greeter;
+//     this.greeting = 'Woof!';
+//     this.speak = function() {
+//       console.log(`${this.greeting}, ${this.dog}`)
+//     }
+//   }
+  
+  
+ 
+  
+//   const ruto = new RandomThing('Prince');
+//   const prince = new RandomThing('Ruto');
+  
+//   console.log(ruto);
+  
+//   ruto.speak();
+//   prince.speak();
 
 
-// =========Principle 3
-// code example for New Binding
+// // ==========Principle 4 - Explicit Binding
 
-
-// ==========Principle 4
-// code example for Explicit Binding
+// const baking = {
+//     food: 'Cupcakes'
+//   }
+  
+//   const  ingridients = ['Flour', 'Sugar', 'Butter'];
+  
+//   function introduce( object1, object2, object3) {
+//     "use strict";
+//     console.log(`Hello! I'm making ${this.food}, and I like to bake with ${obejct1}, ${obejct2}, ${obejct3}`);
+//   }
+  
