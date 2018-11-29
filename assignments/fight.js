ba17ff99af9b96bde342ffa96274f70be66eaccf
Jaouad Sarouti <jadsarouti@outlook.com>
2018-11-29 18:46:00 -0500

    Just randomly adding stuff

--- assignments/fight.js
+++ assignments/fight.js
@@ -2,14 +2,17 @@
 // this code was just for fun;
 // it can be improved to a better game, ;)
 window.onload=function(){
+    sagatWin = document.querySelector("#congrat");
     let score = 0;
     span1 = document.querySelector("#span1");
     attackSagat = document.querySelector("#AttackSagat");
     Rio = document.querySelector(".Rio");
     attackSagat.addEventListener("click",function(){
         span1.innerHTML = score++;
-        console.log(score);
-        Rio.style.backgroundColor = "red";
+    
+            if(score === 10){
+                sagatWin.innerHTML= "You win the game,hail to the almighty Sagat";
+            }
         let one = Math.floor(Math.random()*100);
         let two = Math.floor(Math.random()*240);
         let three = Math.floor(Math.random()*1);
@@ -17,29 +20,26 @@ window.onload=function(){
         Rio.style.backgroundColor = rgb;
 
     })
-    let scoreSagat =0;
     span2 = document.querySelector("#span2");
     defendSagat = document.querySelector("#defendSagat");
     sagat = document.querySelector(".Sagat");
     defendSagat.addEventListener("click",function(){
-        span2.innerHTML = scoreSagat --;
-        sagat.style.backgroundColor= "yellow";
-        let one = Math.floor(Math.random()*10);
-        let two = Math.floor(Math.random()*200);
-        let three = Math.floor(Math.random()*60);
-        let rgb = 'rgb(' + one + ',' + two + ',' + three + ')';
-        sagat.style.backgroundColor = rgb;
+        span2.innerHTML = score--;
     })
     let scoreRio = 0;
     attackRio = document.querySelector("#attackRio");
     congrat = document.querySelector("#congrat");
     attackRio.addEventListener("click",function(){
         span2.innerHTML = scoreRio++;
+        let one = Math.floor(Math.random()*100);
+        let two = Math.floor(Math.random()*240);
+        let three = Math.floor(Math.random()*1);
+        let rgb = 'rgb(' + one + ',' + two + ',' + three + ')';
+        sagat.style.backgroundColor = rgb;
     })
-    let scoreRandom = 0;
     defendRio = document.querySelector("#defendRio");
     defendRio.addEventListener("click",function(){
-        span1.innerHTML = scoreRandom--;
+        span1.innerHTML = score--;
 
     })
 }
