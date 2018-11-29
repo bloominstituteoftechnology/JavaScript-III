8a95fbd50b6e3db464fe6345344cee4df5d053ab
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-11-28 18:49:06 -0600

    Updated some comments and fixed some lines of code

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -76,10 +76,18 @@ function Villian(vAttr) {
   this.dmgMitigation = vAttr.dmgMitigation;
 }
 
+//Allows the villian object to use the prototype methods created for the Humanoid object
 Villian.prototype = Object.create(Humanoid.prototype);
 
+//villians Attack method
 Villian.prototype.shadowAttack = function(target) {
-  return `${target} takes 5 damage from ${this.name}'s Shadow Attack! ${target} now has ${target.healthPoints} left!`
+  let dmg = 5;
+  let hp = target.healthPoints - dmg;
+
+    if(hp <= 0) {
+      return target.destroy();
+    } 
+    return `${target.name} takes ${dmg} damage from ${this.name}'s ${this.weapons[0]} Shadow Attack! ${target.name} now has ${hp} left!`;  
 }
 // ================================================= Hero constructor
 
@@ -90,18 +98,20 @@ function Hero(hAttr) {
   this.armorPierce = hAttr.armorPierce;
 }
 
+//Allows the Hero object to use the prototype methods created for the Humanoid object
 Hero.prototype = Object.create(Humanoid.prototype);
 
+//Hero's attack method
 Hero.prototype.weaponSlash = function(target) {
   let dmg = this.power + 5;
-  let res = target.healthPoints - dmg;
+  let hp = target.healthPoints - dmg;
 
-  if (target.healthPoints === 0) {
-    return target.destroy();
-  }
-  return `${target.name} takes 10 damage from ${this.name}'s ${this.weapons[0]} Slash Attack! ${target.name} now has ${res} left!`
+    if(hp <= 0) {
+      return target.destroy();
+    } 
+    return `${target.name} takes ${dmg} damage from ${this.name}'s ${this.weapons[0]} Slash Attack! ${target.name} now has ${hp} left!`;  
 }
- 
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -111,66 +121,66 @@ Hero.prototype.weaponSlash = function(target) {
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
 
-  // const mage = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 2,
-  //     width: 1,
-  //     height: 1,
-  //   },
-  //   healthPoints: 5,
-  //   name: 'Bruce',
-  //   team: 'Mage Guild',
-  //   weapons: [
-  //     'Staff of Shamalama',
-  //   ],
-  //   language: 'Common Tongue',
-  // });
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
 
-  // const swordsman = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 2,
-  //     width: 2,
-  //     height: 2,
-  //   },
-  //   healthPoints: 15,
-  //   name: 'Sir Mustachio',
-  //   team: 'The Round Table',
-  //   weapons: [
-  //     'Giant Sword',
-  //     'Shield',
-  //   ],
-  //   language: 'Common Tongue',
-  // });
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
 
-  // const archer = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 1,
-  //     width: 2,
-  //     height: 4,
-  //   },
-  //   healthPoints: 10,
-  //   name: 'Lilith',
-  //   team: 'Forest Kingdom',
-  //   weapons: [
-  //     'Bow',
-  //     'Dagger',
-  //   ],
-  //   language: 'Elvish',
-  // });
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
 
-  // console.log(mage.createdAt); // Today's date
-  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  // console.log(swordsman.healthPoints); // 15
-  // console.log(mage.name); // Bruce
-  // console.log(swordsman.team); // The Round Table
-  // console.log(mage.weapons); // Staff of Shamalama
-  // console.log(archer.language); // Elvish
-  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  // console.log(mage.takeDamage()); // Bruce took damage.
-  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
   const darkWarrior = new Villian({
     createdAt: new Date(),
@@ -205,14 +215,15 @@ Hero.prototype.weaponSlash = function(target) {
       'Sword of Epicness',
     ],
     language: 'Common Tongue',
-    power: 10,
+    power: 15,
     shieldPoints: 30,
     armorPierce: 5
   });
 
   console.log(warrior.weaponSlash(darkWarrior));
-  console.log(warrior.weaponSlash(darkWarrior));
-
+  console.log(darkWarrior.shadowAttack(warrior));
+  
+  
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
