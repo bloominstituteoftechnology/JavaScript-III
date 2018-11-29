d103bb9c8a564331ade3a9b1b9a28cbcdeec5dd9
JamesB <jblazer1@yahoo.com>
2018-11-28 18:47:42 -0800

    Add this.js solutions

--- assignments/this.js
+++ assignments/this.js
@@ -5,11 +5,12 @@
                       point at the window object.
 
 * 2. IMPLICIT Binding - happens automatically. Whenever a function is called by a preceding 
-                        dot, the object left of the dot gets "this". Always attached to an Object.
+                        dot, the object left of the dot gets "this". Always attached to an Object in JS.
 
 * 3. EXPLICIT Binding - the developer explicitly tells the "this" keyword what to point too.
 
-* 4. 
+* 4. NEW Binding - Whenever a constructor function is used, "this" refers to the specific instance of the object
+                   that is created and returned by the constructor function.
 *
 * write out a code example of each explanation above
 */
@@ -18,6 +19,7 @@
 function greeting() {
     console.log(this);
 }
+// greeting();
 // code example for Window Binding - above
 
 // Principle 2
@@ -36,9 +38,34 @@ const person = {
     name: "Duke"
 }
 
-const skills = ["Shooting", "Wrangling", "Acting"];
-// code example for New Binding
+const skills = ["Shooting", " Wrangling", " Acting"];
+
+function introduce(skills1, skills2, skills3) {
+    console.log(`Howdy partner! My name is ${this.name}, and I like ${skills1} ${skills2} ${skills3}.`);
+}
+introduce.call(person, skills); // calls all elements in array as one. - can use spread operator (...skills) to seperate or spread elements
+// Apply seperates the values into an array
+introduce.apply(person, skills);
+//Bind is "use this later" - works like call - can use spread operator (...skills) as with .call
+const useLater = introduce.bind(person, skills);
+useLater();
+// code example for Explicit Binding - above
 
 // Principle 4
+function RudePerson(greeter) {
+    this.greeter = greeter;
+    this.greeting1 = "What are you looking at";
+    this.greeting2 = "You are pretty ugly";
+    this.speak = function() {
+        console.log(`${this.greeting1}, ${this.greeter}`);
+    }
+    this.speak2 = function() {
+        console.log(`${this.greeting2}, ${this.greeter}`);
+    }
+}
 
-// code example for Explicit Binding
+const Buddy = new RudePerson("dude");
+const Dude = new RudePerson("buddy")
+Buddy.speak(); 
+Dude.speak2();
+// code example for New Binding - above
