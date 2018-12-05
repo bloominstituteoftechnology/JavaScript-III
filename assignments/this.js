e60e0c7e93940f80ef3e0cd9d59212a69f1323ec
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-05 15:30:35 -0600

    finished answers on this.js deleted past info

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,25 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window/Global Object Binding
-* 2. Implicit Binding
-* 3. New Binding
-* 4. Explicit Binding
+* 1. Window/Global Object Binding:
+
+- Answer: Window Binding is always there. Like in the example of the forest of trees, Window Binding is the entire forest.
+
+
+* 2. Implicit Binding:
+
+- Answer: Implicit (AUTOMATIC!) Binding. Automatic means that Implicit Binding essentially happens for free. It is automatic.
+If you look to your left of the invocation of a method there you will find your 'this' keyword.
+
+* 3. New Binding: 
+
+- Answer: Building NEW objects! When you build a constructor function make sure to
+calitalize the first letter to tell the next developer what you are doing!
+
+* 4. Explicit Binding:
+
+- Answer: We control this! In Explicit Binding we control which 'this' keyword is being applied.
+
 *
 * write out a code example of each explanation above
 */
