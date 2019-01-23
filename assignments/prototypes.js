314e6c132060991390aac7081ab2b4dcaea2769d
RMH <rory.hennessey@gmail.com>
2019-01-23 15:49:21 -0500

    mvp complete

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -32,7 +32,7 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -101,8 +101,126 @@
   console.log(archer.language); // Elvish
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game. 
 */
+
+function GameObject(attrs) {
+  this.createdAt = attrs.createdAt;
+  this.dimensions = attrs.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  console.log(`${this.name} was removed from the game.`);
+}
+
+
+// === CharacterStats ===
+//   * healthPoints
+//   * name
+//   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+//   * should inherit destroy() from GameObject's prototype
+// */
+
+function CharacterStats(charAttrs) {
+  GameObject.call(this, charAttrs);
+  this.healthPoints = charAttrs.healthPoints;
+  this.name = charAttrs.name;
+
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  console.log(`${this.name} took damage.`);
+}
+
+ // === Humanoid (Having an appearance or character resembling that of a human.) ===
+ //  * team
+ //  * weapons
+ //  * language
+ //  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+ //  * should inherit destroy() from GameObject through CharacterStats
+ //  * should inherit takeDamage() from CharacterStats
+
+function Humanoid(humanAttrs) {
+  CharacterStats.call(this, humanAttrs)
+  this.team = humanAttrs.team;
+  this.weapons = humanAttrs.weapons;
+  this.language = humanAttrs.language;
+  
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function () {
+  console.log(`${this.name} offers a greeting in ${this.language}.`);
+}
+
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+
+
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in global scope, the value of 'this' will be the window/console Object
+* 2. Implicit binding: whenever a function is called by a preceding dot, the object left of the dot gets 'this'
+* 3. Whenever a constructor function is used, 'this' refers to the specific instance of the object that is created and returned by tre constructor function
+* 4. Whenver JS's call or apply method is used, 'this' is explicitly defined. For building new objects.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,86 @@
 
 // code example for Window Binding
 
+unction someFunction() {
+      "use strict";
+    }
+    
+    const hobbit = {
+      name: 'Samwise',
+      food: 'taters',
+      cook: function() {
+        // with window binding, food cannot be found!
+       console.log(`${name} loves to eat ${food}`) 
+      }
+    }
+    
+    hobbit.cook();
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const hobbit = {
+      name: 'Samwise',
+      food: 'taters',
+      cook: function() {
+        // with window binding, food cannot be found!
+       console.log(`${this.name} loves to eat ${this.food}`) 
+      }
+    }
+    
+    hobbit.cook();
+
+
 
 // Principle 3
 
 // code example for New Binding
 
+const person = {
+    name: 'Sarah'
+  }
+  
+  const hobbit = {
+    name: 'Frodo'
+  }
+  
+  const skills = ['HTML', 'CSS', 'JS'];
+  
+  function introduce(skills) {
+    
+    console.log(`Hello!  My name is ${this.name}, and I have skills in: ${skills}`)
+  }
+
 // Principle 4
 
 // code example for Explicit Binding
+
+unction CordialPerson(name) {
+    this.greeter = name;
+    this.greeter = greeter;
+    this.greeting = "Hello";
+    this.speak = function() {
+      console.log(`${this.greeting}, ${this.greeter}`)
+    }
+  }
+  
+  const jerry = {
+    greeter: "Newman",
+    greeting = "Hello",
+    speak = function() {
+      console.log(`${this.greeting}, ${this.greeter}`)
+    }
+  }
+  
+  const newman = {
+    greeter: "Newman",
+    greeting = "Hello",
+    speak = function() {
+      console.log(`${this.greeting}, ${this.greeter}`)
+    }
+  }
+  
+  // new applies the this binding to a constructor function
+  const jerry = new CordialPerson('Newman');
+  const newman = new CordialPerson('Jerry');
