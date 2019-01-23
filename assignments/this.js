4d629e8ebb15e07091e21bb7c9275f565fd0a46d
Wes-White <wesleyrwhite86@gmail.com>
2019-01-23 07:40:15 -0800

    this questions 1-4 answered

--- assignments/this.js
+++ assignments/this.js
@@ -3,8 +3,8 @@
 *
 * 1. Implicit Binding- The object left of the dot gets the this. The left of the . is where the "this" is pointed. 
 * 2. Global/Window Binding - In global scope the vale of this will be the window or console object. 
-* 3. New Binding - 
-* 4. Explict Binding - 
+* 3. New Binding - Uses a constructor function with the new keyword to build a new instance. The this keyword inside the function is bound to the new object that is defined. 
+* 4. Explict Binding - Uses .call(), .bind(), and .apply() when using these methods this is specifcally defined. 
 *
 * write out a code example of each explanation above
 */
