1718134526a982371f459fa65dd5d467a0991c9a
ThatGuyUKnow593 <trenteuclide409@gmail.com>
2019-01-22 21:17:33 -0600

    initial commit

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. this in the default definition refers to the global object 'object window'
+* 2. implicit binding means it is using dot notation and refers to whatever is left of the dot
+* 3. explicit binding means explicity passing this to a call() or apply()
+* 4. new binding means that an object is created and this refers to attributes
 *
 * write out a code example of each explanation above
 */
@@ -20,6 +20,8 @@
 // Principle 3
 
 // code example for New Binding
+
+
 
 // Principle 4
 
