52fc8a29b7517aaa3487e6bc27ecc44b8b610979
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-01-23 16:27:37 -0500

    Priciple 1 done

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window. Whenever the function is conatined in the global scope. .this always becomes the window unless we change it. 
+* 2. Implicit  Binding. allows you to use what is inside of an object. used inside of an Object.
+* 3. Constructer Functions Used to consturct thing. uses new bindings
+* 4. Using explisict methods. Using .call or . apply
 *
 * write out a code example of each explanation above
 */
@@ -12,7 +12,12 @@
 // Principle 1
 
 // code example for Window Binding
+function Shake(part) {
+    console.log('Shake those ' + part);
 
+    //console.log(this);
+}
+Shake('hips');
 // Principle 2
 
 // code example for Implicit Binding
