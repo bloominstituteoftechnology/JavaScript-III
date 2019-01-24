573eb88a362649723ba1263fd9b521c24f064a4a
Mike <hallmychal@gmail.com>
2019-01-24 01:49:29 -0600

    MVP complete

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,41 +7,46 @@
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
-/*
-  === GameObject ===
-  * createdAt
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
-*/
-
-/*
-  === CharacterStats ===
-  * healthPoints
-  * name
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
-
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
  
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+  function GameObject () {
+  this.createdAt = Date();
+  this.dimensions = ['length = 2','width = 1','height = 1'];  
+  // this.destroy = function () {
+  //   return(`${this.name} was removed from the game.`)
+  // }
+   }
+   
+  GameObject.prototype.destroy = function () {
+    return(`${this.name} was removed from the game.`)
+  }
+
+  GameObject.prototype.takeDamage = function () {
+    return(`${this.name} took damage.`);
+  }
+
+  function CharacterStats (CharacterStats) {
+   this.healthPoints = CharacterStats.healthPoints;
+   this.name = CharacterStats.name;
+   GameObject.call(this, CharacterStats);
+  }
+
+  function Humanoid (Humanoid) {
+   this.team = Humanoid.team;
+   this.weapons = Humanoid.weapons;
+   this.language = Humanoid.language;
+   this.greet = function () {
+    return(`${this.name} offers a greeting in ${this.language}.`)
+  }
+   GameObject.call(this, Humanoid);
+   CharacterStats.call(this, Humanoid);
+
+  }
+ 
+  Humanoid.prototype = Object.create(GameObject.prototype);
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -58,6 +63,7 @@
     language: 'Common Tongue',
   });
 
+ 
   const swordsman = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -92,19 +98,23 @@
     language: 'Elvish',
   });
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+   console.log(mage.createdAt); // Today's date
+   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+   console.log(swordsman.healthPoints); // 15
+   console.log(mage.name); // Bruce
+   console.log(swordsman.team); // The Round Table
+   console.log(mage.weapons); // Staff of Shamalama
+   console.log(archer.language); // Elvish
+   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+   console.log(mage.takeDamage()); // Bruce took damage.
+   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+  // * Create two new objects, one a villain and one a hero and fight it out with 
+  //
+  
+  
