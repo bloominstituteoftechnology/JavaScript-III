02e086a555bc9f5cd200e6f3427b817badf166be
dustinsnoap <afrobacon@gmail.com>
2019-01-24 12:14:51 -0500

    cleaned up stretch code a bit, still not happy with it but it works fixed a few grammatical errors messages from heros attacks now show up on their own message board added a delay to villians attack

--- assignments/stretch.js
+++ assignments/stretch.js
@@ -1,162 +1,162 @@
-const drHangover = new Humanoid({
+// HERO CLASS = Villians are heros too
+function Hero(atts) {
+    Humanoid.call(this, atts); //inherit Humanoid properties
+    this.attacks = atts.attacks;
+    this.messageTag = atts.messageTag;
+    this.currentHitpointTag = atts.currentHitpointTag;
+    this.maxHitpointTag = atts.maxHitpointTag;
+    this.strengthTag = atts.strengthTag;
+    this.strength = atts.strength;
+    this.maxHitpoints = atts.maxHitpoints;
+    this.bac = atts.bac;
+}
+Hero.prototype = Object.create(Humanoid.prototype);
+Hero.prototype.attack = function(attacker, victim, atkNum) {
+    let atk = {};
+    if(!atkNum) {
+        atk = getRandomInt(attacker.attacks.length);
+        atk = attacker.attacks[atk];
+    }
+    else {
+        atk = attacker.attacks[atkNum];
+    }
+    console.log(atk.text);
+    if(atk.DMGenemy) victim.chgHitpoints(atk.DMGenemy);
+    if(atk.DMGself) attacker.chgHitpoints(atk.DMGself);
+    if(atk.STRenemy) victim.chgStrength(atk.STRenemy);
+    if(atk.STRself) attacker.chgStrength(atk.STRself);
+    if(atk.text) attacker.message(atk.text);
+}
+Hero.prototype.message = function(text) {
+    if(document.getElementById("msg-ok")) document.getElementById("msg-ok").outerHTML = "";
+    document.getElementById(this.messageTag).textContent = text;
+}
+Hero.prototype.chgHitpoints = function(val) {
+    this.healthPoints += val;
+    document.getElementById(this.currentHitpointTag).textContent = this.healthPoints;
+}
+Hero.prototype.chgMaxHitpoints = function(val) {
+    this.maxHitpoints += val;
+    document.getElementById(this.maxHitpointTag).textContent = this.maxHitpoints;
+}
+Hero.prototype.chgStrength = function(val) {
+    this.strength += val;
+    document.getElementById(this.strengthTag).textContent = this.strength;
+}
+Hero.prototype.chgBAC = function(val) {
+    this.bac += val;
+    if(this.bac >= 100) gameOver("Your body couldn't handle this much bro'ness. Try again tomorrow.");
+}
+Hero.prototype.halved = function() {
+    this.healthPoints /= 2;
+    this.strength /= 2;
+    document.getElementById(this.currentHitpointTag).textContent = this.healthPoints;
+    document.getElementById(this.maxHitpointTag).textContent = this.healthPoints;
+    document.getElementById(this.strengthTag).textContent = this.strength;
+    this.message("");
+}
+//VILLIAN
+const drHangover = new Hero({
     createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
     healthPoints: 1000,
     name: 'Dr. Hangover',
-    team: 'Not Good Cru',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'ughhhhh',
-  });
-drHangover.mana = 100;
-drHangover.strength = 10;
-
-drHangover.attack = function() {
-    let atk = getRandomInt(drHangover.attacks.length);
-    atk = drHangover.attacks[atk];
-    beercules.chgHealth(-atk.health);
-    beercules.chgStrength(-atk.strength);
-    beercules.message(atk.text);
-    ok = true;
-}
-drHangover.message = function(text) {
-    document.getElementById("msg-villian").textContent = text;
-}
-drHangover.hit = function(hit) {
-    this.healthPoints -= hit;
-    document.getElementById("villian-health-cur").textContent = this.healthPoints;
-}
-drHangover.attacks = [
-    {
-        health: drHangover.strength,
-        strength: 10,
-        text: `Dr. Hangover punches you in the gut; you feel nauseous. -${drHangover.strength} health -10 strenth.`,
-    },
-    {
-        health: 5,
-        strength: 0,
-        text: `You feel a slight ache move over you whole body as you try to move. -5 health.`
-    },
-    {
-        health: 5,
-        strength: 0,
-        text: `Dr. Hangover grabs ahold of you; his cold hands are cold and causes you to shake. -5 health.`
-    },
-    {
-        health: 0,
-        strength: 10,
-        text: `You're handed a rubix cube; this is not the time for that. -10 strength.`
-    },
-    {
-        health: 0,
-        strength: -10,
-        text: `Dr. Hangover uses taunt; you're heart begins to race. +10 strength.`
-    },
-    {
-        health: 20,
-        strength: 0,
-        text: `Dr. Hangover opens the flashlight app on his phone. The glare is too much. -20 health.`
-    },
-    {
-        health: 10,
-        strength: 0,
-        text: `Dr. Hangover drinks an ice cold glass of OJ in front of you. And no he's not sharing. Your cottenmouth intensifies. -10 health.`
-    },
-]
-
-const beercules = new Humanoid({
-  createdAt: new Date(),
-  dimensions: {
-    length: 2,
-    width: 1,
-    height: 1,
-  },
-  healthPoints: 100,
-  name: 'Beercules',
-  team: 'MacLarens',
-  weapons: [
-    'Edward 40 Hands',
-  ],
-  language: 'ughhhhh',
+    strength: 10,
+    attacks: [],
+    messageTag: "msg-villian",
+    currentHitpointTag: "villian-health-cur",
+    maxHitpointTag: "villian-health-tot",
+    strengthTag: "villian-strength-cur",
+    bac: 0,
 });
-beercules.maxHealth = 50;
-beercules.strength = 100;
-beercules.mana = 10;
-beercules.maxMana = 5;
-beercules.drunkeness = 0;
-
-//ATTACKS
-beercules.sleep = function() {
-    drHangover.message(`After taking a power nap you awake to find Dr. Hangover with ${beercules.strength/2} less hit points.`);
-    drHangover.hit(beercules.strength/2);
-    beercules.endTurn();
-}
-beercules.burrito = function() {
-    drHangover.message(`You devoir a burrito; the grease fills the empty void in your soul. You siphen 10 hit points from Dr. Hangover and gain 10 strength.`);
-    drHangover.hit(10);
-    beercules.chgHealth(10);
-    beercules.chgStrength(10);
-    beercules.endTurn();
-}
-beercules.drink = function() {
-    drHangover.message(`You crack open a cold one with the boys; you feel invigorated. +50 Strength`);
-    beercules.chgStrength(50);
-    beercules.getTipsy(10);
-    beercules.endTurn();
-}
-//MISC
-beercules.message = function(text) {
-    document.getElementById("msg-hero").textContent = text;
-}
+//VILLIAN ATTACKS
+drHangover.attacks = [
+    {   DMGenemy: -10,
+        STRenemy: -10,
+        text: `Dr. Hangover punches you in the gut; you feel nauseous. -10 health -10 strenth.`,
+    },{ DMGenemy: -30,
+        text: `Dr. Hangover tackles you to the ground. -30 health.`
+    },{ DMGenemy: -5,
+        text: `Dr. Hangover grabs ahold of you; his cold hands are cold and cause you to shake. -5 health.`
+    },{ STRenemy: -10,
+        text: `Dr. Hangover hands you a rubix cube; this is not the time for that. -10 strength.`
+    },{ DMGenemy: 10,
+        STRenemy: 10,
+        text: `Dr. Hangover uses taunt; your heart begins to race. +10 strength.`
+    },{ DMGenemy: -20,
+        text: `Dr. Hangover opens the flashlight app on his phone. The glare is too much. -20 health.`
+    },{ DMGenemy: -10,
+        text: `Dr. Hangover drinks an ice cold glass of OJ in front of you. And no he's not sharing. Your cottenmouth intensifies. -10 health.`
+},];
+//HERO
+const beercules = new Hero({
+    createdAt: new Date(),
+    name: 'Beercules',
+    healthPoints: 100,
+    maxHitpoints: 100,
+    bac: 0,
+    strength: 10,
+    attacks: [],
+    messageTag: "msg-hero",
+    currentHitpointTag: "hero-health-cur",
+    maxHitpointTag: "hero-health-tot",
+    strengthTag: "hero-strength-cur",
+});
 beercules.endTurn = function() {
-    drHangover.attack();
+    drHangover.attack(drHangover, beercules);
     ok = false;
 }
-beercules.getTipsy = function(points) {
-    beercules.drunkeness += points;
-    if(beercules.drunkeness >= 100) gameOver("Your body couldn't handle this much bro'ness. Try again tomorrow.");
-}
-beercules.chgStrength = function(points) {
-    beercules.strength += points;
-    document.getElementById("hero-strength-cur").textContent = beercules.strength;
-}
-beercules.chgHealth = function(points) {
-    beercules.healthPoints += points;
-    if(beercules.healthPoints > beercules.maxHealth) beercules.healthPoints = beercules.maxHealth;
-    document.getElementById("hero-health-cur").textContent = beercules.healthPoints;
-    if(beercules.healthPoints <= 0) gameOver("Bruh! You're dead.");
-}
-beercules.clrMsg = function() {
-    if(document.getElementById("msg-ok")) document.getElementById("msg-ok").outerHTML = "";
-    document.getElementById("msg-hero").textContent = "";
-}
-beercules.halved = function() {
-    beercules.healthPoints /= 2;
-    beercules.mana /= 2;
-    beercules.strength /= 2;
-    document.getElementById("hero-health-cur").textContent = beercules.healthPoints;
-    document.getElementById("hero-health-tot").textContent = beercules.healthPoints;
-    document.getElementById("hero-mana-cur").textContent = beercules.mana;
-    document.getElementById("hero-mana-tot").textContent = beercules.mana;
-    document.getElementById("hero-strength-cur").textContent = beercules.strength;
-    ok = true;
-    beercules.clrMsg();
-}
 
-let ok = false;
-let dead = false;
+//ATTACKS
+beercules.attacks = [
+    {   DMGenemy: 20,
+        text: `After taking a power nap you awake to find Dr. Hangover with 20 less hit points.`,
+    },{ DMGenemy: -10,
+        DMGself: 10,
+        STRenemy: 10,
+        text: `You devoir a burrito; the grease fills the empty void in your soul. You siphen 10 hit points from Dr. Hangover and gain 10 strength.`,
+    },{ STRself: 50,
+        text: `You crack open a cold one with the boys; you feel invigorated. +50 Strength.`,
+},];
+
+// LETS PLAY A GAME
 window.onload = function() {
     document.getElementById("msg-ok").addEventListener("click", clickedOK);
 }
+let canAttack = true;
 function clickedOK() {
-    document.getElementById("atk-sleep").addEventListener("click", beercules.sleep);
-    document.getElementById("atk-burrito").addEventListener("click", beercules.burrito);
-    document.getElementById("atk-drink").addEventListener("click", beercules.drink);
+    document.getElementById("atk-sleep").addEventListener("click", function() {
+        if(canAttack) {
+            beercules.message("");
+            beercules.attack(beercules, drHangover, 0);
+            canAttack = false;
+            setTimeout(function() {
+                drHangover.attack(drHangover, beercules);
+                canAttack = true;
+            }, 1000);
+        }
+    });
+    document.getElementById("atk-burrito").addEventListener("click", function() {
+        if(canAttack) {
+            beercules.message("");
+            beercules.attack(beercules, drHangover, 1);
+            canAttack = false;
+            setTimeout(function() {
+                drHangover.attack(drHangover, beercules);
+                canAttack = true;
+            }, 1000);
+        }
+    });
+    document.getElementById("atk-drink").addEventListener("click", function() {
+        if(canAttack) {
+            beercules.message("");
+            beercules.attack(beercules, drHangover, 2);
+            canAttack = false;
+            setTimeout(function() {
+                drHangover.attack(drHangover, beercules);
+                canAttack = true;
+            }, 1000);
+        }
+    });
     beercules.halved();
     beercules.message("You wake up wondering where your clothes are. Dr. Hangover is staring at you from across the room. What happened last night? All stats are now halved.");
 }
