f5f068a789c44e4d8cd48e973e572f2a8ced32c8
jkaddu <jkaddu@yahoo.com>
2019-01-30 13:27:55 -0800

    this work

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window binding is when 'this' is bound to the global/console scope.
-* 2. 
+* 2. When you call a function using dot notation the object before the the dot/function is what 'this' is referrig to.
 * 3. 
 * 4. 
 *
@@ -17,7 +17,15 @@ sayHi('John');
 // Principle 1
 
 // code example for Window Binding
+const theObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+        console.log(`${this.greeting} my name is ${name}`);
+        console.log(this);
+    }
 
+}
+theObj.sayHello('John');
 // Principle 2
 
 // code example for Implicit Binding
