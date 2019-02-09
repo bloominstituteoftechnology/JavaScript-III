9f93f8ce8e26acac165cb18ddd52c6935cb8502e
myasia barbee <myasiabarbee@icloud.com>
2019-02-09 11:11:51 -0500

    Pull request

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -108,3 +108,6 @@
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+
+  // pull request //
