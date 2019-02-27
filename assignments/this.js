8ef517e14371b4db92d6b69d32255ff6e7a9e6a2
danpatrickoneill <dan47573@gmail.com>
2019-02-27 15:02:50 -0500

    Finished this.js exercises

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding is what happens when this isn't really bound to anything
+* 2. Implicit binding is when this is bound to something before a dot, like when a method is being called.
+* 3. New binding is when this is bound using the new keyword and a constructor function.
+* 4. Explicit binding is when this is bound using a function like apply, call, or bind.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,38 @@
 // Principle 1
 
 // code example for Window Binding
-
+this;
 // Principle 2
 
 // code example for Implicit Binding
 
+
+const me = {
+  name: "Dan",
+  that: function () {
+    console.log(this.name);
+  }
+}
+
+me.that();
+
 // Principle 3
 
 // code example for New Binding
+function Person(attrs) {
+  this.name = attrs.name;
+}
+
+const myself = new Person({name: "Dan"})
+
+console.log(myself.name)
 
 // Principle 4
 
 // code example for Explicit Binding
+// Continued from Principle 2
+const you = {
+  name: "Jorp",
+}
+
+me.that.call(you)
