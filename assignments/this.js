e0c8b9c16474cfe7d5c444d92a700e36efde6888
Mindy Allen <mrsmallen07@gmail.com>
2019-03-06 17:09:41 -0500

    creating examples for the four principles

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit Binding:
+* 2. Explicit Binding:
+* 3. New Binding:
+* 4.  Window Binding:
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,36 @@
 
 // code example for Window Binding
 
-// Principle 2
+// Implicit Binding - 
 
-// code example for Implicit Binding
+const me ={
+    name: 'Mindy',
+    Age: 34,
+    sayName: function(){
+            console.log(this.name);
+    }
+};
+
+me.sayName();  
+
+// when you call a function/function is invoked look to left of dot
+//this keyword will reference whats to the left of the dot
 
 // Principle 3
 
 // code example for New Binding
 
-// Principle 4
+// Explicit Binding
 
-// code example for Explicit Binding
+const sayName = function(Book1, Book2, Book3){
+        console.log('My name is' + this.name + 'and I love' + Book1 + ',' + Book2 + ',and ' + Book3);
+};
+
+const Samuel = {
+    name: 'Samuel',
+    age: 34
+};
+
+const books =['Math', 'History', 'Science'];
+
+sayName.call(Samuel, books [0], books[1], books[2]);
