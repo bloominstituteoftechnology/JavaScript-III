b33df3567bcad61b0ad24dd747ec050a59dc03cd
Mindy Allen <mrsmallen07@gmail.com>
2019-03-06 18:11:26 -0500

    finished working on the this assignment piece

--- assignments/this.js
+++ assignments/this.js
@@ -1,19 +1,27 @@
+import Const from 'Const';
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Implicit Binding:
-* 2. Explicit Binding:
-* 3. New Binding:
-* 4.  Window Binding:
+* 1. Implicit Binding: You look at the left pf the dot at call time
+* 2. Explicit Binding: Telling the function what the context of this keyword is going to be- 
+using call, apply or bind
+* 3. New Binding: When you have function invoke with a new keyword is bound to new object
+being constructed
+* 4.  Window Binding: If none of the rules apply then  this keyword is going to default
+to the window object unless you're  in strict mode then it will just be undefined. 
+
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Window Binding example
 
-// code example for Window Binding
 
-// Implicit Binding - 
+
+
+// 
+
+// Implicit Binding example - 
 
 const me ={
     name: 'Mindy',
@@ -28,11 +36,18 @@ me.sayName();
 // when you call a function/function is invoked look to left of dot
 //this keyword will reference whats to the left of the dot
 
-// Principle 3
+// New Binding Example
 
-// code example for New Binding
+Const Animal = function (color, name, type) {
+    this.color = color;
+    this.name = name;
+    this.type = type;
+}
 
-// Explicit Binding
+const zebra = new Animal ('black and white', 'zorro', 'zebra');
+
+
+// Explicit Binding example
 
 const sayName = function(Book1, Book2, Book3){
         console.log('My name is' + this.name + 'and I love' + Book1 + ',' + Book2 + ',and ' + Book3);
@@ -45,4 +60,4 @@ const Samuel = {
 
 const books =['Math', 'History', 'Science'];
 
-sayName.call(Samuel, books [0], books[1], books[2]);
+sayName.apply(Samuel, books);
