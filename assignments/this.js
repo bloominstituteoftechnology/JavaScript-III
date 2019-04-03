4410097244b4b72c3c46e2f16a5a1b47c95fbad6
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-03 15:47:35 -0700

    Update this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. In a simple function call, this will default to the global object, and be undefined under strict mode.
+* 2. Implicit binding, it points to the object of which the function is called.
+* 3. This can also be explicit, in that you can point to a certain value using call, apply or bind.
+* 4. This can point to a newly constructed object using the "new" keyword.
 *
 * write out a code example of each explanation above
 */
