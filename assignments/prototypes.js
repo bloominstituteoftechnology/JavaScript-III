53a4c595eae93a2a1097bf2c4eda77cfdccdc0ae
Greg Jordan <gregljordan@gmail.com>
2019-04-03 19:14:42 -0400

    Working on the prototype code

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -6,7 +6,7 @@
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
+/*
   
 /*
   === GameObject ===
@@ -15,6 +15,21 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
+// const Person = function(color, name, gender) {
+//   this.color = color;
+//   this.name = name;
+//   this.gender = gender
+// };
+
+
+// const Greg = new Person("black", "Abdul", "Male");
+
+function GameObject (){
+
+}
+
+
 
 /*
   === CharacterStats ===
