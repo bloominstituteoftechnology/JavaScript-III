98e18249d41af773e435b97fac3a3545c42ecece
myasia barbee <myasiabarbee@icloud.com>
2019-04-02 22:21:56 -0400

    Console.log check | MVP

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -70,7 +70,7 @@ Humanoid.prototype.greet = function () {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
