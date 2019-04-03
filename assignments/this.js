6ccd860fb6e1803c5b01360d2e0a856492859a5c
mandihamza <mandihamza@gmail.com>
2019-04-02 22:03:30 -0600

    Created examples of the 4 principles of

--- assignments/this.js
+++ assignments/this.js
@@ -17,3 +17,85 @@
 *
 * write out a code example of each explanation above
 */
+
+// Principle 1
+// code example for Window Binding
+//   "use strict";  Doesn't allow items to be bound to the window object
+const favFood = function() {
+
+console.log(this.food);
+};
+
+const mandiFav = {
+food: `burrito`
+};
+
+//favFood(); returns undefined
+window.food = `Pork Carnitas`; // Binds Pork Carnitas to the window object, otherwise it returns undefined.
+favFood();
+
+// Principle 2
+// code example for Implicit Binding
+const Person = function(name, age){
+return {
+  name: 'Mandi',
+  age: 33,
+  sayName: function() {
+    console.log(`Hello, my name is ${this.name}.`);
+},
+daughter: {
+  name: 'KT',
+  sayName: function() {
+      console.log(`My daughter's name is ${this.name}.`);
+    }
+  }
+};
+};
+
+var mandi = Person('Mandi', 33);
+mandi.sayName();
+mandi.daughter.sayName();
+
+// Principle 3
+// code example for New Binding
+var Pet = function(breed, name, type) {
+this.breed = breed;
+this.name = name;
+this.type = type;
+};
+
+const Cat = new Pet('Tabby', 'Mau', 'Cat');
+
+// Principle 4
+// code example for Explicit Binding
+
+const dogBark = function(treat1, treat2, treat3) {
+console.log(`My name is ${this.name}, I like to bark at the mail man!`)
+};
+
+const odie = {
+name: 'Odie',
+breed: 'Mastif-Pit mix'
+};
+
+dogBark.call(odie);
+
+
+// =========Do I need this, IDFK =============
+// .call, .apply overide the binding of this
+const dogTreats = function(treat1, treat2, treat3) {
+  console.log(`My name is ${this.name}, and I like ${treat1}, ${treat2}, and ${treat3}.`)
+};
+
+const trooper = {
+name: 'Trooper',
+breed: 'Mastif-Pit mix'
+};
+
+const treats = ['beef bones', 'carrots', 'peanut butter'];
+
+dogTreats.call(trooper, treats[0], treats[1], treats[2]); //call
+dogTreats.apply(trooper, treats); //apply
+const moreDogTreats = dogTreats.bind(trooper, treats[0], treats[1], treats[2]); //bind
+
+moreDogTreats();
