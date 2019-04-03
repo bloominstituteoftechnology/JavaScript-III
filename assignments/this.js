ce4e4a0459927906694dac946e78e6e0f2fbc149
mandihamza <mandihamza@gmail.com>
2019-04-02 21:59:25 -0600

    Answered 4 principles of  questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,19 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+
+
+* 1. Window Binding - Unless stated otherwise the default binding of `this` is to the window/console object.
+
+
+* 2. Implicit Binding - Implicit binding means that the `this` keyword refers to the object to the left of the dot. (Dot Notation)
+
+
+* 3. New Binding - When `this` is used with constructor functions, this referes to the specific instance of the object that is created and returned by the constructor function. It actually creates brand new objects -- hence the name 'Constructor Function'. For example, if you pass 3 parameters to the constructor function such as breed, name, and type, and then invoke a function with the *new* keyword `this` will be bound to those three objects (this.breed, this.name, this.type).
+
+
+* 4. Explicit Binding -  Implicit binding refers to whatever is left of the dot (dot notation). For example:
+
 *
 * write out a code example of each explanation above
 */
-
-// Principle 1
-
-// code example for Window Binding
-
-// Principle 2
-
-// code example for Implicit Binding
-
-// Principle 3
-
-// code example for New Binding
-
-// Principle 4
-
-// code example for Explicit Binding
