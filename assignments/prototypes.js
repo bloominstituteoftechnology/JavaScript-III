3bc9e5c9f37608e7128af4befb7bb2323baf6f95
Greg Jordan <gregljordan@gmail.com>
2019-04-04 01:18:54 -0400

    Completed prototypes.js which marks the completion of this project.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,20 +16,17 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
-// const Person = function(color, name, gender) {
-//   this.color = color;
-//   this.name = name;
-//   this.gender = gender
-// };
 
+function GameObject (attr){
+    this.name = attr.name;
+    this.dimensions = attr.dimensions;
 
-// const Greg = new Person("black", "Abdul", "Male");
+};
 
-function GameObject (){
-
-}
-
-
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+  
+};
 
 /*
   === CharacterStats ===
@@ -37,6 +34,17 @@ function GameObject (){
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(cAttr) {
+  GameObject.call(this, cAttr);
+  this.healthPoints = cAttr.healthPoints;
+  this.name = cAttr.name;
+
+}
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+};
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -47,6 +55,19 @@ function GameObject (){
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+function Humanoid(hAttr) {
+  CharacterStats.call(this, hAttr);
+
+  this.team = hAttr.team;
+  this.weapons = hAttr.weapons;
+  this.language = hAttr.language;
+}
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -56,7 +77,7 @@ function GameObject (){
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
