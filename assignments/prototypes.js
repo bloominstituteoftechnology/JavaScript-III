5a6bf5e892770e33b89d5ba1eeaf47f090736159
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-07 19:09:25 -0700

    Update prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -145,4 +145,17 @@ Humanoid.prototype.greet = function() {
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
 
-  
+   function Villain(vAttributes) {
+   GameObject.call(this, vAttributes);
+   CharacterStats.call(this, vAttributes);
+   this.team = hAttributes.team;
+   this.weapons = hAttributes.weapons;
+   this.language = hAttributes.language;
+ }
+
+Humanoid.prototype = Object.create(GameObject.prototype);
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`
+}
