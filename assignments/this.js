b0f009a2950ed8c5d53dab1f143192bbddbdcb0d
anthonyvigz <anthonyvigz@gmail.com>
2019-04-23 12:30:26 -0400

    answered this questions and made window binding example

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object this binding refers to the global scope and everything the window contains.
+* 2. Implicit Binding refers to the object within which this is referring.
+* 3. New Bindings are typically used as constructors, as other objects can be passed through and understood as 'this.'
+* 4. Explicit Binding is when a new funcction is created that acts as the original, but 'this' is already predefined.
 *
 * write out a code example of each explanation above
 */
@@ -12,6 +12,11 @@
 // Principle 1
 
 // code example for Window Binding
+
+function checkIt(number) {
+    console.log(this);
+    return number;
+}
 
 // Principle 2
 
