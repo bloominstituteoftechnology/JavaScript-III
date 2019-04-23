177937cce45471b283aa46b2c1f72533d5957561
Jordan Lanier <jordan.6195@yahoo.com>
2019-04-23 17:24:20 -0400

    finished

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,30 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject (game){
+  this.createdAt = game.createdAt;
+  this.name = game.name;
+  this.dimensions = game.dimensions
+}
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game.`
+};
+
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats (charStat) {
+  GameObject.call(this,charStat);
+  this.healthPoints =  charStat.healthPoints;
+}
+CharacterStats.prototype = Object.create (GameObject.prototype);
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage.`;
+};
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +50,19 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+function Humanoid (hNoid) {
+  CharacterStats.call(this,hNoid);
+  this.team = hNoid.team,
+  this.weapons = hNoid.weapons,
+  this.language = hNoid.language
+}
+Humanoid.prototype = Object.create (CharacterStats.prototype);
+Humanoid.prototype.greet = function (){
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
  
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +71,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +132,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,26 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in the global scope, the value of “this” will be the window/console Object;
+* 2. Whenever a function is called by a preceding dot, the object before that dot is this
+* 3. Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+* 4. Whenever JavaScript’s call or apply method is used, this is explicitly defined.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
+//console.log(this);
 
 // Principle 2
-
 // code example for Implicit Binding
+//return `${this.name} took damage.`;
 
 // Principle 3
-
 // code example for New Binding
-
+//const mage = new Humanoid('I am new' )
+    
 // Principle 4
-
 // code example for Explicit Binding
+//CharacterStats.call(this,hNoid);
