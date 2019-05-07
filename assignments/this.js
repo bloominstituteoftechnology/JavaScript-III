b01e7002b4ab9f1c619527da8e79a78b70661881
unknown <jabaritf@gmail.com>
2019-05-07 19:53:50 -0400

    Wrote the 4 principles of 'This'

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window: The 'THIS' keyword follows the rules of different scopes so it must be defined in a funciton and cannot be used outside of it.
+* 2. Implicit binding: When using dot notation in a function the object before the dot become 'THIS' 
+* 3. New Binding: When a function returns an object the keys in the object have to be proceeded with 'new.'
+* 4. Explicit Binding: When using call or apply 'this' is got to be defined immediately.
 *
 * write out a code example of each explanation above
 */
