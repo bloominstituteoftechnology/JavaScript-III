592d518f9c80fa849e18177a7aec6914990f6483
kmcknight1 <kmcknight1991@gmail.com>
2019-05-08 18:10:33 -0500

    MVP and stretch complete

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,7 +7,7 @@
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -32,79 +32,185 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
+function GameObject(attrs) {
+  this.createdAt = attrs.createdAt;
+  this.name = attrs.name;
+  this.dimensions = attrs.dimensions;
+}
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+};
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+function CharacterStats(charAttrs) {
+  GameObject.call(this, charAttrs);
+  this.healthPoints = charAttrs.healthPoints;
+}
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+CharacterStats.prototype = Object.create(GameObject.prototype);
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+};
+
+function Humanoid(humanoidAttrs) {
+  GameObject.call(this, humanoidAttrs);
+  CharacterStats.call(this, humanoidAttrs);
+  this.team = humanoidAttrs.team;
+  this.weapons = humanoidAttrs.weapons;
+  this.language = humanoidAttrs.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
+function Hero(heroAttrs) {
+  GameObject.call(this, heroAttrs);
+  CharacterStats.call(this, heroAttrs);
+  Humanoid.call(this, heroAttrs);
+  this.specialPower = heroAttrs.specialPower;
+}
+
+Hero.prototype.heal = function(char, x) {
+  char.healthPoints += x;
+
+  if (x < 2) {
+    return `${this.name} used ${this.specialPower} to heal ${
+      char.name
+    } by ${x} point.`;
+  } else {
+    return `${this.name} used ${this.specialPower} to heal ${
+      char.name
+    } by ${x} points.`;
+  }
+};
+
+function Villian(villAttrs) {
+  GameObject.call(this, villAttrs);
+  CharacterStats.call(this, villAttrs);
+  Humanoid.call(this, villAttrs);
+  this.evilSaying = villAttrs.evilSaying;
+}
+
+Villian.prototype.attack = function(char, x) {
+  if (x >= char.healthPoints) {
+    char.healthPoints = "Dead";
+    return `${this.name} has killed ${char.name}.`;
+  } else {
+    char.healthPoints -= x;
+    return `${this.name} has taken ${x} health from ${char.name}.`;
+  }
+};
+
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1
+  },
+  healthPoints: 5,
+  name: "Bruce",
+  team: "Mage Guild",
+  weapons: ["Staff of Shamalama"],
+  language: "Common Tongue"
+});
+
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2
+  },
+  healthPoints: 15,
+  name: "Sir Mustachio",
+  team: "The Round Table",
+  weapons: ["Giant Sword", "Shield"],
+  language: "Common Tongue"
+});
+
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4
+  },
+  healthPoints: 10,
+  name: "Lilith",
+  team: "Forest Kingdom",
+  weapons: ["Bow", "Dagger"],
+  language: "Elvish"
+});
+
+const wizard = new Hero({
+  createdAt: new Date(),
+  name: "Remus",
+  dimensions: {
+    length: 3,
+    width: 3,
+    height: 5
+  },
+  healthPoints: 35,
+  team: "Dumbledore's Army",
+  weapons: ["Wand", "Cloak of Invisibility", "Pensieve"],
+  language: "Common Tongue",
+  specialPower: "Magic"
+});
+
+const politician = new Villian({
+  createdAt: new Date(),
+  name: "Herman",
+  dimensions: {
+    length: 2,
+    width: 5,
+    height: 2
+  },
+  healthPoints: 30,
+  team: "Republican",
+  weapons: ["Propoganda", "Fox News", "Rich Family"],
+  language: "Filibuster",
+  evilSaying: "We deserve stuff, they don't!"
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+console.log(mage.healthPoints);
+console.log(wizard.heal(mage, 4));
+console.log(mage.healthPoints);
+
+console.log(politician.attack(mage, 5));
+console.log(mage.healthPoints);
+console.log(politician.evilSaying);
+
+console.log(wizard.heal(mage, 2));
+console.log(mage.healthPoints);
+console.log(politician.attack(mage, 6));
+console.log(mage.healthPoints);
+
+// Stretch task:
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,178 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. "This" tries to bind itself to the window/global scope unless directed otherwise.
+ * 2. Using a "pointer" (aka: methods/object) to bind "this" to.
+ * 3. Using a constructor function to create/hold information/objects/functions within one pseudo-class.
+ * 4. Using function methods to have control over what "this" represents.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
-// code example for Window Binding
+function hello() {
+  return `Hello ${this}`;
+}
+
+console.log(hello());
+console.log(this);
 
 // Principle 2
 
-// code example for Implicit Binding
+const me = {
+  name: "Katie",
+  age: 27,
+  faveFood: "Seafood",
+  cook: function() {
+    return `${this.name} likes to cook ${this.faveFood}.`;
+  }
+};
+
+console.log(me.cook());
 
 // Principle 3
 
-// code example for New Binding
+function Person (name, age, occupation, location) {
+    this.name = name,
+    this.age = age,
+    this.occupation = occupation,
+    this.location = location,
+    this.children = [0],
+    this.SO = ['N/A']
+    this.spouse = ['N/A']
+}
+
+const katieMcKnight = new Person("Katie McKnight", 27, "Student", "St. Joseph, MO");
+const chanceHalo = new Person("Chance Halo", 22, "Student", "St. Joseph, MO");
+const theresaMcKnight = new Person("Theresa McKnight", 58, "Ministry Assistant", "St. Joseph, MO");
+const rickMcKnight = new Person("Rick McKnight", 62, "Retired", "St. Joseph, MO");
+const jenGewin = new Person("Jen Gewin", 31, "Health Advocate", "Cosby, MO");
+const joeyGewin = new Person("Joey Gewin", 32, "Cartographer", "Cosby, MO");
+const julesGewin = new Person("Jules Gewin", 2, "fun-loving toddler", "Cosby, MO");
+
+Person.prototype.addChild = function (child){
+    this.children.push(child);
+    if (this.children.includes(0)) {
+        this.children.shift();
+    }
+}
+
+Person.prototype.addSpouse = function (spouse){
+    this.spouse.push(spouse);
+    if (this.spouse.includes('N/A')) {
+        this.spouse.shift();
+    }
+}
+
+Person.prototype.addSO = function (so){
+    this.SO.push(so);
+    if (this.SO.includes('N/A')) {
+        this.SO.shift();
+    }
+}
+
+Person.prototype.sayHello = function () {
+    return `Hello, my name is ${this.name}.`;
+}
+
+Person.prototype.childrenCount = function() {
+    if (this.children.includes(0)) {
+        return `I have no children.`;
+    } else if (this.children.length < 2) {
+        return `I have ${this.children.length} child.`;
+    } else if (this.children.length > 1) {
+        return `I have ${this.children.length} children.`;
+    }
+}
+
+Person.prototype.aboutMeSpeak = function () {
+    return `My name is ${this.name}. I am ${this.age} years old. I am currently a ${this.occupation} in ${this.location}.`;
+}
+
+Person.prototype.love = function () {
+    if (this.SO == 'N/A' && this.spouse == "N/A") {
+        return `I love myself!`;
+    } else if (this.spouse == 'N/A') {
+        return `I love ${this.SO[0].name}!`;
+    } else if (this.SO == 'N/A') {
+        return `I love ${this.spouse[0].name}!`;
+    }
+}
+
+
+theresaMcKnight.addSpouse(rickMcKnight);
+rickMcKnight.addSpouse(theresaMcKnight);
+theresaMcKnight.addChild(jenGewin);
+theresaMcKnight.addChild(katieMcKnight);
+rickMcKnight.addChild(jenGewin);
+rickMcKnight.addChild(katieMcKnight);
+katieMcKnight.addSO(chanceHalo);
+chanceHalo.addSO(katieMcKnight);
+jenGewin.addSpouse(joeyGewin);
+joeyGewin.addSpouse(jenGewin);
+jenGewin.addChild(julesGewin);
+joeyGewin.addChild(julesGewin);
+
+
+
+console.log(theresaMcKnight);
+console.log(katieMcKnight);
+
+console.log(katieMcKnight.sayHello());
+console.log(theresaMcKnight.sayHello());
+console.log(chanceHalo.sayHello());
+
+
+console.log(katieMcKnight.childrenCount());
+console.log(jenGewin.childrenCount());
+console.log(theresaMcKnight.childrenCount());
+
+console.log(chanceHalo.aboutMeSpeak());
+console.log(theresaMcKnight.aboutMeSpeak());
+console.log(julesGewin.aboutMeSpeak());
+
+
+console.log(katieMcKnight.love());
+console.log(chanceHalo.love());
+console.log(theresaMcKnight.love());
+console.log(julesGewin.love());
+
+
 
 // Principle 4
 
 // code example for Explicit Binding
+const person1 = {
+    name: "Jessica",
+    age: 32
+}
+
+const person2 = {
+    name: "Austin",
+    age: 21
+}
+
+const person3 = {
+    name: "CJ",
+    age: 56
+}
+
+const person4 = {
+    name: "Katie",
+    age: 27
+}
+
+function introduction () {
+    return `Hi. I'm ${this.name} and I'm ${this.age} years old.`;
+}
+
+console.log(introduction.call(person1));
+console.log(introduction.call(person2));
+console.log(introduction.call(person3));
+
+//Bind is used to save the "this" keyword for later use. 
+
+const myIntro = introduction.bind(person4);
+
+console.log(myIntro());
