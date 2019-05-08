e430c19ce3a61ac15d970c0b476a8b03e5c1c0b3
Becheru888 <becheru93@gmail.com>
2019-05-08 20:23:47 +0100

    challenge completed undefined values rezolved

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -18,7 +18,7 @@
 */
 
 function GameObject(createdAt, name, dimensions) {
-    this.createdAt = createdAt,
+  this.createdAt = createdAt,
     this.name = name,
     this.dimensions = dimensions
 }
@@ -71,56 +71,10 @@ Humanoid.prototype.greet = function () {
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
+const mage = new Humanoid(new Date(), 'Bruce', { length: 2, width: 1, height: 1 }, 5, 'Mage Guild', ['Staff of Shamalama'], 'Common Tongue');
+const swordsman = new Humanoid(new Date(), 'Sir Mustachio', { length: 2, width: 2, height: 2 }, 15, 'The Round Table', ['Giant Sword', 'Shield'], 'Common Tongue');
+const archer = new Humanoid(new Date(), 'Lilith', { length: 2, width: 1, height: 1 }, 10, 'Forest Kingdom', ['Bow', 'Dagger'], 'Elvish');
 
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
-
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
-
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
 
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
@@ -138,4 +92,3 @@ Humanoid.prototype.greet = function () {
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
-
