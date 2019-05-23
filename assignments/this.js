bf6fe2f972646714ddbc266187d6d896b51e9500
Tatianna-Gilliam <gilliamtatiannam@gmail.com>
2019-05-23 15:46:52 -0700

    Explicit Example
    Have some error in the console.log.  Not sure why...

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,7 @@
 *
 * 1. Global binding= the value of 'this' will be the window/console object
 * 2. Implicit binding= When a function is called by a preceding dot, the object lef of the dot gets 'this'
-* 3. New binding= 'this' is tied to created object. Whenever a constructor function is used, 'this' refers to the specific instance of the object tha is created and returned by the constructor function
+* 3. New binding= 'this' is tied to created object. Whenever a constructor function is used, 'this' refers to the specific instance of the object that is created and returned by the constructor function
 * 4. Explicit binding= .bind, .call, .apply overides the this keyword
 *
 * write out a code example of each explanation above
@@ -33,17 +33,40 @@ function Person(obj){
       return `Hello, my name is ${this.name}, and I am ${this.age} years old!`;
     }
   }
+  
+  const tatianna = new Person({name: 'Tatianna', age:37});
+  const uzifatdaddy = new Person({name: 'Uzi' , age: '5'});
+  
+  tatianna.speak.call(uzifatdaddy);
+  uzifatdaddy.speak.apply(tatianna);
+  
+  
+  console.log(tatianna.speak());
+  console.log(uzifatdaddy.speak());
+  
 
 // Principle 4
 
-  
-  const tatianna = new Person({name: 'Ryan', age:37});
-  const uzifatdaddy = new Person({name: 'Haynes' , age: '5'});
-  
-  ryan.speak.call(uzifatdaddy);
-  uzifatdaddy.speak.apply(ryan);
-  
-  
-  tatianna.speak();
-  uzifatdaddy.speak();
+const stacey= {
+   name: "Stacey",
+   age: 34, 
+   sayName: function(){
+     console.log('Mynameis'+ this.name);
+   }
+}
+  stacey.sayName();
+
+  // not a method but a function on the global scope
+
+const sayName = funtion(){
+  console.log('Mynameis' + this.name);
+
+const OGtmoni = {
+  name: 'Tmoni',
+  age: 37, 
+}
+
+const languages = ['JS', 'Ruby', 'Python'];
+}
+sayName.call(OGtmoni, languages[0],languages[1],languages[2] );
 // code example for Explicit Binding
