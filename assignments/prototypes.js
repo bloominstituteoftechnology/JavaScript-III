63010d6fb9d64fccafee795e396f980b4852f005
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-05-28 20:56:51 -0700

    Completed all questions onn the principles of .this but not all the coding example. Did not finish the prototype project.

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,15 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+  this.dimensions = attributes.dimensions;
+}
+
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`
+}
 
 /*
   === CharacterStats ===
@@ -22,6 +31,14 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attributes) {
+  this.healthPoints = attributes.healthPoints;
+}
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +49,16 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
+function Humanoid(attributes) {
+  this.team = attributes.team;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+}
+
+Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  The Window/Global/Default Binding which refers to the Window (if nothing else is defaulted like in CodePen, CodePen is the Default this.name.) I like Josh's analogy like the forest. Since we don't know exactly which tree he's refering to, it's non specific.
+
+* 2. Implicit binding is the next principle which I remember as automatic which is done to both the objects and methods. With Implicit binding, it's referring to the object to the left of the (.) is what .this will be referring to.
+
+* 3. Explicit Binding which means stated clearly, leaving no room for error or doubt. In the example of a forest, I am referring to and pointing a specific tree. With the Explicit binding, you can use added functions to the function, which are .call, .apply and .bind.
+
+* 4. New Binding (Constructor function) is the last. You create (construct) a new function that creates a new object. It's nice to use when you have a ton of informational arrays are created.
 *
 * write out a code example of each explanation above
 */
@@ -13,9 +16,20 @@
 
 // code example for Window Binding
 
+console.log(this);
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const greetingHello = {
+    greeting: 'Hello!',
+    saysHello: function (name) {
+      console.log(`${this.greeting} my name is ${name}`);
+      console.log(this);
+    }
+  }
+  greetingHello.saysHello('Lisa');
 
 // Principle 3
 
