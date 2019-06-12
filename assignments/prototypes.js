c46da87c9139d8986f1d9516773955aad8c22050
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-7.local>
2019-06-12 12:22:20 -0700

    first commit

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,9 +1,13 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+  Object oriented design is commonly used in video games.  For this part of the 
+  assignment you will be implementing several constructor functions with their 
+  correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, 
+  CharacterStats, Humanoid.  
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid. 
+   Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
