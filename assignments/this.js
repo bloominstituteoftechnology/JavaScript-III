84633ee03e99aaf7449eb64955c35d2191e9da2d
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-12 15:09:47 -0400

    working on this.js principles

--- assignments/this.js
+++ assignments/this.js
@@ -2,7 +2,7 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Global binding: binds "this..." to the console object.
-* 2. Implicit binding: functions in methods; focuses on the object literals.
+* 2. Implicit binding: functions in methods; focuses on the object literals;implicit === automatic binding
 * 3. New Binding: refers to a specific point in which an object was created.
 * 4. Explicit binding: explicit is direct; .call and .apply methods are used in this.
 *
@@ -16,6 +16,16 @@
 // Principle 2
 
 // code example for Implicit Binding
+
+const doggo = {
+    name: 'Bear',
+    plays: 'fetch',
+    time: 'all day',
+    fetch: function(){
+        return `${this.name} will ${this.play} ${this.time} and everyday if you let her!`
+    }
+}
+console.log(doggo.fetch());
 
 // Principle 3
 
