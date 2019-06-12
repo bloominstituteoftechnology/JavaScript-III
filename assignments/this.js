f58b98c4d75bc507c767018ff3e4b8ef9b8f9968
Greg Johnston <johng3269@gmail.com>
2019-06-12 15:08:16 -0400

    this done

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,16 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Object Binging
+    When 'this' is in the Global Scope, its value is the window/console Object
+* 2. Implicit Binding
+    When 'this' is used before a function with a .
+
+* 3. New Binding
+    'this' refers to a specific instance of an object created and returned by a constructor function.
+
+* 4. Explicit Binding
+    Whenever .bind or .apply is used, 'this' becomes explicitly defined.
 *
 * write out a code example of each explanation above
 */
@@ -14,13 +20,57 @@
 // code example for Window Binding
 
 // Principle 2
-
 // code example for Implicit Binding
 
-// Principle 3
+const sayNameFunc = obj => {
+  obj.sayName = function() {
+    console.log(`Hello my name is ${this.name}`);
+    console.log(this);
+  };
+};
+const me = {name: 'Greg'};
 
+sayNameFunc(me);
+
+me.sayName();
+
+
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function person(greeter) {
+  this.greeting = 'Hello ';
+  this.greeter = greeter;
+  this.speak = function () {
+    console.log(this.greeting + this.greeter);
+    console.log(this)
+  };
+}
 
+const greg = new person('Greg');
+
+
+greg.speak();
+
+
+// Principle 4
 // code example for Explicit Binding
+
+function CordialPerson(greeter) {
+  this.greeting = 'Hello ';
+  this.greeter = greeter;
+  this.speak = function() {
+    console.log(this.greeting + this.greeter);
+    console.log(this);
+  };
+}
+
+const jerry = new CordialPerson('Newman');
+const newman = new CordialPerson('Jerry');
+const beverly = new CordialPerson('Beverly');
+const diego = new CordialPerson('Diego');
+
+jerry.speak();
+newman.speak();
+jerry.speak.call(beverly);
+newman.speak.apply(diego);
