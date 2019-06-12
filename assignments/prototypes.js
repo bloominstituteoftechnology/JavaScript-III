aaec1d4afed0813cff2319f7fa0cbe084efd47d6
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-12 18:04:29 -0400

    working on trying the stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -147,5 +147,18 @@ Humanoid.prototype.greet = function(){
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+function Hero(attributes){
+  name: 'Harold',
+  Humanoid.call(this, attributes);
+}
+function villain(attributes){
+  name: 'Steve',
+  Humanoid.call(this, attributes);
+}
+
+Hero.prototype = Object.create(Humanoid.prototype);
+villain.prototype = Object.create(Humanoid.prototype);
+
+
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
