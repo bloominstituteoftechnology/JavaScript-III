12230b732775e32a201087647ac5dc4c5e421347
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-12 17:41:30 -0400

    I need to see this done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -17,42 +17,32 @@
 */
 function GameObject(attributes){
   this.name = attributes.name;
+  this.createdArt = attributes.createdArt;
+  this.dimensions = attributes.dimensions;
 
 }
 GameObject.prototype.destroy = function(){
-
+  return `${this.name} was removed from the game`;
 }
 
-function CharacterStats(attributes){
-  GameObject.call(this, attributes)
 
-}
-
-CharacterStats.prototype = Object.create(GameObject.prototype)
-CharacterStats.prototype.takeDamage = function(){
-
-}
-
-function Humanoid(attributes){
-  CharacterStats.call(this, attributes)
-  this.team = attributes.team
-}
-
-Humanoid.prototype = Object.create (CharacterStats.prototype)
-Humanoid.prototype.greet = function(){
-
-}
-
-new Humanoid({
-  name: 'Nisa',
-  
-})
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(attributes){
+  GameObject.call(this, attributes)
+  this.healthPoints = attributes.healthPoints;
+
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function(){
+  return `${this.name} has been damaged!`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -63,7 +53,19 @@ new Humanoid({
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid(attributes){
+  CharacterStats.call(this, attributes)
+  this.team = attributes.team;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+}
+
+Humanoid.prototype = Object.create (CharacterStats.prototype)
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} greets in ${this.language}`;
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -73,66 +75,66 @@ new Humanoid({
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
 
-  // const mage = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 2,
-  //     width: 1,
-  //     height: 1,
-  //   },
-  //   healthPoints: 5,
-  //   name: 'Bruce',
-  //   team: 'Mage Guild',
-  //   weapons: [
-  //     'Staff of Shamalama',
-  //   ],
-  //   language: 'Common Tongue',
-  // });
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
 
-  // const swordsman = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 2,
-  //     width: 2,
-  //     height: 2,
-  //   },
-  //   healthPoints: 15,
-  //   name: 'Sir Mustachio',
-  //   team: 'The Round Table',
-  //   weapons: [
-  //     'Giant Sword',
-  //     'Shield',
-  //   ],
-  //   language: 'Common Tongue',
-  // });
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
 
-  // const archer = new Humanoid({
-  //   createdAt: new Date(),
-  //   dimensions: {
-  //     length: 1,
-  //     width: 2,
-  //     height: 4,
-  //   },
-  //   healthPoints: 10,
-  //   name: 'Lilith',
-  //   team: 'Forest Kingdom',
-  //   weapons: [
-  //     'Bow',
-  //     'Dagger',
-  //   ],
-  //   language: 'Elvish',
-  // });
+  const archer = new Humanoid({
+    createdAt: new Date(6/12/19),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
 
-  // console.log(mage.createdAt); // Today's date
-  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  // console.log(swordsman.healthPoints); // 15
-  // console.log(mage.name); // Bruce
-  // console.log(swordsman.team); // The Round Table
-  // console.log(mage.weapons); // Staff of Shamalama
-  // console.log(archer.language); // Elvish
-  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  // console.log(mage.takeDamage()); // Bruce took damage.
-  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith greets in Elvish.
+  console.log(mage.takeDamage()); // Bruce has been damaged!
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
   // Stretch task: 
