618293876f44e337a31cc79305a34f6ea8a14d20
Vanesa Hernandez Flores <fleurs@Vanesas-MacBook-Pro.local>
2019-06-12 18:19:11 -0500

    work in progress still

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. the "this" in line 16 will refer to the global
 * 2. 
 * 3. 
 * 4. 
@@ -12,10 +12,23 @@
 // Principle 1
 
 // code example for Window Binding
+function seeTourist(name) {
+    console.log(this);
+    return name;
+  }
+  seeTourist("Vanesa");
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    welcome: 'Welcome to DisneyLand',
+    enterDisney: function(name) {
+      console.log(`${this.welcome}, I hope you finde everything you need ${name}`);
+      console.log(this);
+    }
+  };
+  myObj.enterDisney('Vanesa');
 
 // Principle 3
 
