91452b4132754027d74054d0cd9c88b9371b95f6
A.D.Faris <adfaris@gmail.com>
2019-06-20 11:13:16 -0400

    example for implicit binding

--- assignments/this.js
+++ assignments/this.js
@@ -9,9 +9,7 @@
 * write out a code example of each explanation above
 */
 
-// Principle 1
-
-// code example for Window Binding
+// Principle 1 code example for Window Binding
 const calculateAge = function (age) {
   console.log(this)
   // this refers to the global window/Console Object since there is no context defined
@@ -20,9 +18,18 @@ const calculateAge = function (age) {
 
 calculateAge(35)
 
-// Principle 2
-
-// code example for Implicit Binding
+// Principle 2 code example for Implicit Binding
+const me = {
+  greetings: 'Hello, my name is',
+  name: 'Arega',
+  age: 42,
+  sayName: function () {
+    console.log(`${this.greetings}, ${this.name}`)
+    console.log(this)
+    // this here refers to the me object
+  }
+}
+me.sayName()
 
 // Principle 3
 
