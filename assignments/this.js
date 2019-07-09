a77122237a6f7718678754c4e139af606bb6f4d8
spettigrew <sara.pettigrew5@gmail.com>
2019-07-08 20:38:14 -0600

    Answers to 'this' questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,14 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Context - the value of 'this' will be the console object. The whole JS language!
+
+* 2. Implicit Binding - when using a dot syntax, the object on the left side of the dot is 'this'.
+
+* 3. New Binding/Constructor functions - a reusable blueprint of what the object is. All constructors return an object.
+
+* 4. Explicit Binding - we use functions to .bind, .apply or .call properties and attach it to a function. 
+
 *
 * write out a code example of each explanation above
 */
