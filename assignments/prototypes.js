0805f2bb6803a70e814b782c71bcb19eef955c47
Clif <clifhodges13@gmail.com>
2019-07-08 21:03:41 -0500

    restarting prototype assignment

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,13 +1,11 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-
   In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
-
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
   
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
@@ -32,12 +30,12 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+
 /*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
+ * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+ * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+ * Instances of CharacterStats should have all of the same properties as GameObject.
+ */
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
@@ -57,7 +55,6 @@
     ],
     language: 'Common Tongue',
   });
-
   const swordsman = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -74,7 +71,6 @@
     ],
     language: 'Common Tongue',
   });
-
   const archer = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -91,7 +87,6 @@
     ],
     language: 'Elvish',
   });
-
   console.log(mage.createdAt); // Today's date
   console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
   console.log(swordsman.healthPoints); // 15
@@ -104,7 +99,7 @@
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 */
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. If this is called in the global scope, it will refer to the window or console object
+* 2. If this is called inside of an object, it is implicit that it refers to that Object. also, refers to the object to the left of the dot
+* 3. When using a constructor class and building a new object with that class, the this keyword refers to each new instance of that Object
+* 4. You can use the keywords call, apply, or bind to explicitly attach the this keyword to something
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,65 @@
 
 // code example for Window Binding
 
+function print() {
+  console.log(this)
+}
+print();
+
 // Principle 2
 
 // code example for Implicit Binding
 
-// Principle 3
+const myObj = {
+  name: 'Marge',
+  date: new Date,
+  speak: function() {
+    console.log(`My name is ${this.name} and today is ${this.date}`)
+  }
+}
 
+console.log(myObj.speak());
+
+// Principle 3
 // code example for New Binding
+class Character {
+  constructor(attr) {
+    this.name = attr.name;
+    this.gender = attr.gender;
+    this.origin = attr.origin;
+    this.powers = attr.powers;
+    this.alliance = attr.alliance;
+    this.intro = function () {
+      console.log(`There's a new ${this.alliance} on the block. ${this.gender} name is ${this.name}, and comes from ${this.origin}. Powers include ${this.powers}.`);
+    };
+  }
+}
+
+const electro = new Character({
+  name: 'ELECTRO',
+  gender: 'His',
+  origin: 'South Dakota',
+  powers: 'electricity manipulation and telekenesis',
+  alliance: 'villain'
+})
+
+const shadow = new Character({
+  name: 'SHADOW',
+  gender: 'Her',
+  origin: 'Israel',
+  powers: 'light refraction, dimension hopping, and making a solid cup of tea',
+  alliance: 'hero'
+})
+
+electro.intro();
+shadow.intro();
 
 // Principle 4
 
 // code example for Explicit Binding
+
+electro.intro.call(shadow);
+
+
+
+console.log(`-----------------End of "this"-----------------`)
