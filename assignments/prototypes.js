bbbc90c6ce2af3a3db147b1086fec11565c09fc9
divsection <manningdigital@gmail.com>
2019-07-11 20:04:12 -0500

    uploaded done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+function GameObject(gameObjAttributes) {
+  this.createdAt = gameObjAttributes.createdAt;
+  this.dimensions = gameObjAttributes.dimensions;
+ };
+ 
+ GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`;
+};
 
 /*
   === CharacterStats ===
@@ -22,6 +30,18 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(charStatAttributes) {
+  GameObject.call(this, charStatAttributes);
+  this.healthPoints = charStatAttributes.healthPoints;
+  this.name = charStatAttributes.name;
+ };
+ 
+ CharacterStats.prototype = Object.create(GameObject.prototype);
+ 
+ CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+};
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,6 +53,19 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(huAttributes) {
+  CharacterStats.call(this, huAttributes);
+  this.team = huAttributes.team;
+  this.weapons = huAttributes.weapons;
+  this.language = huAttributes.language;
+ }
+ 
+ Humanoid.prototype = Object.create(CharacterStats.prototype);
+ 
+ Humanoid.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`;
+};
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +74,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +135,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -12,15 +12,41 @@
 // Principle 1
 
 // code example for Window Binding
-
+const favoriteColor = function() {
+    console.log(this.color);
+   }
+   
+favoriteColor();
 // Principle 2
 
 // code example for Implicit Binding
-
+const wesM = {
+    name: 'Wes',
+    color: 'Green',
+    food: 'Cheeseburger',
+    favoriteColor: function() {
+     console.log(`${this.name}'s favorite color is ${this.color}.`)
+    }
+}
+wesM.favoriteColor();
 // Principle 3
 
 // code example for New Binding
+function Person(name, color) {
+    this.name = name;
+    this.color = color;
+   }
+   
+   const James = new Person('James', 'orange');
+   
+   console.log(James);
+
 
 // Principle 4
 
 // code example for Explicit Binding
+let favoriteFood = function() {
+    console.log(`${this.name}'s favorite food is ${this.food}.`)
+   }
+   
+favoriteFood.call(wesM);
