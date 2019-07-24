5094bc08f637ea29203ac1fb4b0c1cf932787393
Adan <51211285+Adan011@users.noreply.github.com>
2019-07-24 17:35:45 -0500

    Done

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,35 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+  function GameObject(attrs) {
+    this.createdAt = attrs.createdAt;
+    this.name = attrs.name;
+    this.dimensions = attrs.dimensions;
+  }
+
+  GameObject.prototype.destroy = function(){
+    return `${this.name} was removed from the game.`;
+  }
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+function CharacterStats(attrs) {
+  GameObject.call(this, attrs);
+  this.healthPoints = attrs.healthPoints;
+}
+
+CharacterStats.prototype = Object.create(GameObject.prototype);
+
+CharacterStats.prototype.takeDamage = function() {
+  return `${this.name} took damage.`;
+};
+
+
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -33,6 +56,21 @@
   * should inherit takeDamage() from CharacterStats
 */
  
+function Humanoid(attrs) {
+  CharacterStats.call(this, attrs);
+  this.team = attrs.team;
+  this.weapons = attrs.weapons;
+  this.language = attrs.language;
+}
+
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function(){
+  return `${this.name} offers a greeting in ${this.language}.`;
+}
+
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +79,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +140,102 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+
+function Villain(evil) {
+  Humanoid.call(this, evil);
+  this.specialAbility = evil.specialAbility;
+}
+
+  Villain.prototype = Object.create(Humanoid.prototype);
+
+  Villain.prototype.attack = function(){
+    return `${this.name} sneak atacks for 20 dmg!`;
+  };
+
+  Villain.prototype.attack2 = function(){
+    return `${this.name} horizontal attacks for 15 dmg!`;
+  };
+
+  // function Humanoid(attrs) {
+  //   CharacterStats.call(this, attrs);
+  //   this.team = attrs.team;
+  //   this.weapons = attrs.weapons;
+  //   this.language = attrs.language;
+  // }
+  
+  // Humanoid.prototype = Object.create(CharacterStats.prototype);
+  
+  // Humanoid.prototype.greet = function(){
+  //   return `${this.name} offers a greeting in ${this.language}.`;
+  // }
+  
+  
+  
+
+
+
+
+
+
+function Hero(hero){
+  Humanoid.call(this, hero);
+  this.specialAttack = hero.specialAttack;
+}
+
+  Hero.prototype = Object.create(Humanoid.prototype);
+
+  Hero.prototype.sAttack = function(){
+  return `${this,name} grabs and throws for 10 dmg!`;
+  } 
+
+  Hero.prototype.sAttack2 = function(){
+    return `${this.name} counters with his ${this.weapon} and deals 30 dmg!`;
+  }
+
+const villain = new Villain({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 4,
+    height: 5,
+  },
+  healthPoints: 50,
+  name: 'DarkGoblin',
+  team: 'Dark Forest',
+  weapons: [
+    'Dagger',
+    'Scmitar',
+  ],
+  language: 'Goblin',
+  specialAbility: 'Sneak Attack',
+})
+
+const hero = new Hero({
+  createdAt: new Date(),
+  dimensions: {
+    length:2,
+    width: 5,
+    height: 6,
+  },
+  healthPoints: 65,
+  name: 'Great Knight',
+  team: 'The Order',
+  weapons: [
+    'Long Sword',
+  ],
+  language: 'Human',
+  specialAttack: 'Spin Attack',
+})
+
+
+console.log(villain.attack);
+console.log(hero.sAttack);
+console.log(villain.attack2);
+console.log(hero.sAttack2);
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in the global scope, the value of “this” will be the window/console Object.
+* 2. Whenever a function is called by a preceding dot, the object before that dot is this.
+* 3. Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+* 4. Whenever JavaScript’s call or apply method is used, this is explicitly defined.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,42 @@
 // Principle 1
 
 // code example for Window Binding
+function sayName(name) {
+    console.log(this);
+    return name;
+  }
+  sayName("Adan");
 
 // Principle 2
 
 // code example for Implicit Binding
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+      console.log(`${this.greeting} my name is ${name}`);
+    }
+  };
+  myObj.sayHello('Adan');
 
 // Principle 3
 
 // code example for New Binding
+function person(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+    };
+  }
+  
+  const tom = new person('Tom');
+  const harry = new person('Harry');
+  
+  tom.speak();
+  harry.speak();
 
 // Principle 4
 
 // code example for Explicit Binding
+tom.speak.call(harry); 
+harry.speak.apply(tom);
