6b618682c2ca81098700ee16d0637cc534e2f714
Taran <thetaranneeld@gmail.com>
2019-07-24 14:41:06 -0600

    finished this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - This is when the 'this' keyword is used without context.
+* 2. Implicit Binding - This is when the 'this' keyword is used with is referencing a variable from a function or object.
+* 3. New Binding - This is when the 'this' keyword is used in a constructor, and will represent any object that is created by the constructor.
+* 4. Explicit Binding - This is when you are referencing a function from an object, and you use a call(), bind(), or apply() function, to change what 'this' is referencing.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,72 @@
 
 // code example for Window Binding
 
+// function windowBinding() {
+//     return this;
+// }
+
+// console.log(windowBinding());
+
 // Principle 2
 
 // code example for Implicit Binding
+
+// const animal =  {
+//     species: "dog",
+//     about: function(name) {
+//         console.log(`${this.name} is a ${this.species}`);
+//     }
+// };
+
+// animal.about("Tucker");
 
 // Principle 3
 
 // code example for New Binding
 
+// function person(attributes) {
+//     this.type = attributes.species,
+//     this.firstName = attributes.name
+//     this.speak = function() {
+//         console.log(`${this.firstName} is a ${this.type}.`);
+//     };
+// };
+
+// const tyler = new person({
+//     species: "human",
+//     name: "Tyler"
+// });
+
+// tyler.speak();
+
 // Principle 4
 
 // code example for Explicit Binding
+
+function people(attributes) {
+    this.firstName = attributes.name
+    this.sayHello = function() {
+        console.log(`Hello, I am ${this.firstName}!`);
+    };
+};
+
+const rob = new people({
+    name: "Rob"
+});
+
+const alex = new people({
+    name: "Alex"
+});
+
+rob.sayHello.call(alex);
+alex.sayHello.apply(rob);
+
+const lisa = {
+    name: "Lisa"
+};
+
+function sayHi() {
+    console.log(`Hi, I'm ${this.name}!`);
+}
+let helloLisa = sayHi.bind(lisa);
+helloLisa();
