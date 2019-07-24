30c054197c7374e555636049a20d44d977180eda
MarquesC19 <Carter234>
2019-07-24 17:10:55 -0500

    project submission

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,42 @@
 */
 
 // Principle 1
-
+function sayName(name) {
+    console.log (this);
+    return name;
+}
+sayName("Marques")
 // code example for Window Binding
 
 // Principle 2
-
+const myObj = {
+    greeting: "Hello",
+    sayHello: function(name) {
+      console.log(`${this.greeting} my name is ${name}`);
+      console.log(this);
+    };
+}
+myObj.sayHello("Marques");
 // code example for Implicit Binding
 
 // Principle 3
-
+function CordialPerson(greeter) {
+    this.greeting = "Hello ";
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  
+   const Marques = new CordialPerson("Marques");
+  const Carter = new CordialPerson("Carter");
+  
+   Carter.speak();
+  Carter.speak();
 // code example for New Binding
 
 // Principle 4
-
+Marques.speak.call(Carter);
+Carter.speak.apply(Marques);
 // code example for Explicit Binding
