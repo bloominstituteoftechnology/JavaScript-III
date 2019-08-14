5e7b228a2d40db0206bd87e57de6603d6c3a04a9
emster7013 <52601501+emster7013@users.noreply.github.com>
2019-08-13 21:59:04 -0400

    added some things

--- assignments/this.js
+++ assignments/this.js
@@ -1,20 +1,30 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. The value of 'This' in the window/global binding will be the window/console object
+* 2. Implicit binding occurs when dot notation is used to invoke a function
+* 3. When there is a constructor function is used, this refers to specific instance of the object that has been created and returned by the constructor function.
+* 4. When the call or apply method is used this is defined
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+function sayName(name) {
+    console.log(this);
+    return name;
+}
+sayName('Jim Morrison');
 // code example for Window Binding
 
 // Principle 2
-
+const myObj = {
+    greeting: 'You smell',
+    saySmell: function(name){
+        console.log(`${this.greeting} my name is ${name}`);
+    }
+};
+myObj.saySmell('Jim');
 // code example for Implicit Binding
 
 // Principle 3
