29b158ddf081f84e77d0fe44b7d4f5ff03b0dc8b
lauralyeinc <52268790+lauralyeinc@users.noreply.github.com>
2019-08-13 22:25:08 -0400

    Working on this.js.

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,53 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global Binding: The value of "this" will be in the window/ console Object. 
+
+* 2. Implicit Binding: Whenever a function is called by a preceding dot, the object before that dot is "this". 
+
+* 3. New Binding: When using a constructor function (is a function that returns an object, it's an object creater. OOP) "this" refers to the specific instance of the object that is created and returned by the constructor function.
+
+* 4. Explicit Binding: Whenever JavaScript's call or apply method is used, "this" is explicted defined. We can override what a constructor objects get set to. 
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 -----------------------------------------------
+function sayName(name) {
+  console.log(this);
+  return name; 
+}
+console.log(sayName("Rose Nyland")); 
+// code example for Window (global) Binding
 
-// code example for Window Binding
+// Principle 2 -------------------------------------------------
+const myObj = {
+  greeting: "Hello",
+  sayHello: function(name) {
+    console.log(`${this.greeting} my name is ${name}`);
+  }
+};
+myObj.sayHello("Dorothy Zbornak");
 
-// Principle 2
+// code example for Implicit Binding -----------------------------
 
-// code example for Implicit Binding
+// Principle 3 ---------------------------------------------------
+function GoldenGirlPerson(greeter) {
+  this.greeting = "Hello ";
+  this. greeter = greeter;
+  this.speak = function() {
+    console.log(this.greeting + this.greeter);
+    console.log(this);
+  };
+}
+const Sophia = new GoldenGirlPerson("Blanche");
+const Blanche = new GoldenGirlPerson("Sophia");
+Sophia.speak();
+Blanche.speak();
 
-// Principle 3
 
-// code example for New Binding
+// code example for New Binding ----------------------------------
 
-// Principle 4
+// Principle 4 --------------------------------------------------
 
-// code example for Explicit Binding
+// code example for Explicit Binding ----------------------------
