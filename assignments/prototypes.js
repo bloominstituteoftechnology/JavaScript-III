19ab49965ec5615a4fb28bcef4516c2870b77b6d
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-20 18:49:39 +0200

    proposal for new prototypes exercise

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,110 +1,59 @@
 /*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In order to do these exercises you'll need your newly acquired knowledge on
+  constructor functions, methods, prototypes and the `this` keyword.
+  Here's an example of an exercise:
 
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  EXERCISE 0:
+
+  Build an Airplane constructor that takes a name.
+  Give airplanes the ability to take off and land.
+  If a plane takes off, its "isFlying" property is true.
+  If a plane lands, its "isFlying" property is false.
+
+  SOLUTION CODE:
+
+  function Airplane(name) {
+    this.name = name;
+    this.isFlying = false;
+  }
+  Airplane.prototype.takeOff = function () {
+    this.isFlying = true;
+  }
+  Airplane.prototype.land = function () {
+    this.isFlying = false;
+  }
+
+  HOW TO TEST OUR SOLUTION:
   
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
-  
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+  const myPlane = new Airplane('Jumbo');
+  console.log(myPlane.name)              // 'Jumbo'
+  console.log(myPlane.isFlying)          // false
+  myPlane.takeOff();
+  console.log(myPlane.isFlying)          // true
+  myPlane.land();
+  console.log(myPlane.isFlying)          // Jumbo
 */
 
 /*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
+
+EXERCISE 1
+
+- Build a Person Constructor that takes name and age.
+- Give persons the ability to greet, returning a string stating name and age.
+- Give persons the ability to eat edibles.
+- When eating an edible, it should be pushed into a "stomach" property which is an array.
+- Give persons the ability to poop.
+- When pooping, the stomach should empty.
+
+EXERCISE 2
+
+- Build a Car constructor that takes model name and make.
+- Give cars the ability to drive a distance.
+- By driving a car, the driven distance should be added to an "odometer" property.
+- Give cars the ability to crash.
+- A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
+- Give cars the ability to be repaired.
+- A repaired car can drive again.
+
 */
-
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- 
-/*
-  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
-  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
-  * Instances of CharacterStats should have all of the same properties as GameObject.
-*/
-
-// Test you work by un-commenting these 3 objects and the list of console logs below:
-
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
-
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
-
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
-
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villain and one a hero and fight it out with methods!
