d948c7484f8880741f1666d3f8a87e734ad2a534
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-08-20 19:09:23 +0200

    wip

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -6,10 +6,10 @@
 
   EXERCISE 0:
 
-  Build an Airplane constructor that takes a name.
-  Give airplanes the ability to take off and land.
-  If a plane takes off, its "isFlying" property is true.
-  If a plane lands, its "isFlying" property is false.
+  - Build an Airplane constructor that takes a name.
+  - Give airplanes the ability to take off and land.
+  - If a plane takes off, its "isFlying" property is true.
+  - If a plane lands, its "isFlying" property is false.
 
   SOLUTION CODE:
 
@@ -25,7 +25,7 @@
   }
 
   HOW TO TEST OUR SOLUTION:
-  
+
   const myPlane = new Airplane('Jumbo');
   console.log(myPlane.name)              // 'Jumbo'
   console.log(myPlane.isFlying)          // false
@@ -37,23 +37,140 @@
 
 /*
 
-EXERCISE 1
+  EXERCISE 1
 
-- Build a Person Constructor that takes name and age.
-- Give persons the ability to greet, returning a string stating name and age.
-- Give persons the ability to eat edibles.
-- When eating an edible, it should be pushed into a "stomach" property which is an array.
-- Give persons the ability to poop.
-- When pooping, the stomach should empty.
+  - Build a Person Constructor that takes name and age.
+  - Give persons the ability to greet, returning a string stating name and age.
+  - Give persons the ability to eat edibles.
+  - When eating an edible, it should be pushed into a "stomach" property which is an array.
+  - Give persons the ability to poop.
+  - When pooping, the stomach should empty.
 
-EXERCISE 2
+  EXERCISE 2
 
-- Build a Car constructor that takes model name and make.
-- Give cars the ability to drive a distance.
-- By driving a car, the distance driven should be added to an "odometer" property.
-- Give cars the ability to crash.
-- A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
-- Give cars the ability to be repaired.
-- A repaired car can be driven again.
+  - Build a Car constructor that takes model name and make.
+  - Give cars the ability to drive a distance.
+  - By driving a car, the distance driven should be added to an "odometer" property.
+  - Give cars the ability to crash.
+  - A crashed car can't be driven any more. Attempts return a string "I crashed at x miles!", x being the miles in the odometer.
+  - Give cars the ability to be repaired.
+  - A repaired car can be driven again.
 
+  EXERCISE 3
+
+  - Build a Baby constructor that subclasses the Person built earlier.
+  - Babies of course inherit the ability to greet, which can be strange.
+  - Babies should have the ability to play, which persons don't.
+  - By playing, a string is returned with some text of your choosing.
+
+  EXERCISE 4
+
+  Use your imagination and come with constructors that allow to build objects
+  With amazing and original capabities. Build 3 small ones, or a very
+  complicated one with lots of state. Surprise us!
+
+*/
+
+/*
+
+  STRETCH TASK
+
+  Object oriented design is commonly used in video games. You will be implementing several constructor functions with their correct inheritance hierarchy.
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+
+  Each constructor function has unique properties and methods that are defined in their block comments below:
+*/
+
+/*
+  === GameObject ===
+  * createdAt
+  * name
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+*/
+
+/*
+  === CharacterStats ===
+  * healthPoints
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/
+
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+
+/*
+  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+  * Instances of CharacterStats should have all of the same properties as GameObject.
+*/
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+/*
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 */
