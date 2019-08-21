6ccee350a5cf61ff39d659cc2035cf01e5066e5b
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-08-21 12:16:50 -0700

    Answered 'this' principal q's.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,13 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: Strict mode. Results often undefined to prevent code break.
+
+* 2. Implicit Binding: Auto, common. Function takes value left of 'this' in method objects.
+
+* 3. New Binding: Uses 'new' to create an object 'this' refers to.
+
+* 4. Explicit Binding: Uses 'call', 'apply'and 'bind' to refer directly to values.
 *
 * write out a code example of each explanation above
 */
