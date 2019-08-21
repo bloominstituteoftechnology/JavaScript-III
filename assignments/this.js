ea1df0af27db67da7973a39faa9a4aaeae6be7b0
Jake Johnson <jakejjohnson@gmail.com>
2019-08-21 15:29:52 -0500

    learning this

--- assignments/this.js
+++ assignments/this.js
@@ -13,6 +13,15 @@
 
 // code example for Window Binding
 
+function characterSpell1(){
+        console.log(this.spell);
+
+}
+'use strict';
+characterSpell1();
+const spell = "Q";
+
+
 // Principle 2
 
 // code example for Implicit Binding
@@ -21,6 +30,19 @@
 
 // code example for New Binding
 
+function LoLChar(char, role) {
+        this.char = char;
+        this.role = role;
+        this.declare = function () {
+            console.log(`Champion name: ${this.char} Champion role: ${this.role}`);
+        };
+}
+
+const ahri = new LoLChar('Ahri', 'Middle');
+const qiyana = new LoLChar('Qiyana', 'Middle');
+
+ahri.declare();
+qiyana.declare();
 // Principle 4
 
 // code example for Explicit Binding
