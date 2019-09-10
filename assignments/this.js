00817cdbba87bbd8cabd143bb4b0c1197a5b12c4
chiragthesia <harithesia@gmail.com>
2019-09-09 22:03:22 -0500

    completed this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,73 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window
+* 2. Implicit
+* 3. New Binding
+* 4. Explicit
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
-
+console.log(this);
+function chirag(indianBoy){
+    console.log(this);
+    return `I am chirag and i am ${indianBoy}`;
+}
+console.log(chirag);
 // Principle 2
 
-// code example for Implicit Binding
+const business = {
+    name: 'Actual Candy',
+    category: 'food',
+    product: 'sour candy',
+    owner: 'Max',
+    price: function(){
+        return `${this.name} sells ${this.product}`;
+    }
+}
+console.log(business);
 
 // Principle 3
 
-// code example for New Binding
+// const food = new food ('type of food');
+// function Food (type){
+//     this.type = typeFood;
+//     this.origin = function(){
+//         console.log(`I like to eat ${this.type}`);
+//     }
+// }
+
+// const chiragThesia = new Food('Pizza');
+// console.log(chiragThesia);
+
+const animal = new Animal('param');
+function Animal(food) {
+  this.food = food;
+  this.eat = function() {
+    console.log(`This animal likes to eat ${this.food}.`);
+  }
+}
+const zebra = new Animal('grass');
+console.log(zebra);
+zebra.eat();
+
+const lion = new Animal('Meat');
+lion.eat();
 
 // Principle 4
 
-// code example for Explicit Binding
+const lambdaClass = {
+    name: 'Chirag', 
+    classType: 'java script'
+}
+
+const material = ['Closures', 'functions', 'variables', 'this']
+
+function iamLearning(material1, material2, material3, material4){
+    console.log(this);
+    return `${this.name} am learning ${this.classType} and learning about ${material1}, ${material2}, ${material3}, and ${material4}`;
+}
+material
