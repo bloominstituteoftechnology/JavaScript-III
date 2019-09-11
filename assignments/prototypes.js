2d6b3365341c7805e383d7511236d3c7858eaebb
Trevor Thompson <Thompson_Trevor@bah.com>
2019-09-10 20:51:29 -0400

    First run at MVP level of this assignment

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,33 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+const GameObject = {
+
+  createdAt: 0,
+  name : 'Value',
+  dimensions: 100,
+  destroy: function() {
+    return(`${this.name} was removed from the game.`);
+  }
+};
+
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+const CharacterStats = new GameObject(Object.create);
+
+const CharacterStats = {
+
+   healthPoints: 0,
+   takeDamage: function() {
+      return(`${this.name} took damage`);
+   }
+
+};
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,6 +53,19 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+const Humanoid = new CharacterStats(Object.create);
+
+const Humanoid = {
+
+  team: 'playerx',
+  weapons : 'bat',
+  language : 'english',
+  greet : function() {
+    return(`${this.name} offers a greeting in ${this.language}`);
+  }
+
+}
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +75,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +136,6 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - This is binding done to connect to the main window of the javascript language. You will get all of the default variables and functions found with the language by tieing this to the console value.
+* 2. Implicit Binding - Is binding where the this keyword ties to the object before the dot.
+* 3. New Binding - this a type of binding, which is the creation of a new object which has the properties of the object that is to the left hand side of the equal sign
+* 4. Explicit Binding - this is when you have a function call a this. You are invoking this through a function call
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,29 @@
 
 // code example for Window Binding
 
+console.log(this);
+
 // Principle 2
 
 // code example for Implicit Binding
+
+const conceptObject = {
+
+    value: 0,
+    speak: function() {
+        console.log(this.value);
+        // the above is the implicit call
+    }
+
+};
 
 // Principle 3
 
 // code example for New Binding
 
+const testNew = new conceptObject();
+
 // Principle 4
 
 // code example for Explicit Binding
+testNew.speak();
