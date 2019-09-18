1d268305ad2b0511b70dfebed8690d69266bd49c
jarrodskahill <jarrodskahill@gmail.com>
2019-09-18 14:33:38 -0700

    finished this page

--- assignments/this.js
+++ assignments/this.js
@@ -10,20 +10,56 @@
 */
 
 // Principle 1
-function Jarrod(says) {
-    this.thing = says;
+function hotWheel(monster){
+    console.log(`I use to love` + `${this.monster}` + ` trucks`);
+    return monster;
+
 }
+hotWheel(this);  
 
 // code example for Window Binding
 
 // Principle 2
+let jay = {
+    name: "Jarrod",
+    says: "what's up guys!",
+    jaySays: function (){
+        console.log(this.says)
+    }
+}
+
+jay.jaySays();
+
+
 
 // code example for Implicit Binding
 
-// Principle 3
 
-// code example for New Binding
 
 // Principle 4
+function car(){
+    console.log(this.fast);
+}
+
+let wrx = {
+    brand: "subaru",
+    fast: "0-60 in 5.5 seconds"
+}
+
+car.call(wrx)
+
+
 
 // code example for Explicit Binding
+
+// Principle 3
+function fastCar(brz){
+    this.thing = brz;
+}
+
+let Car  = new fastCar('brz goes 0-60 in 6.2 sec');
+console.log(Car.thing);
+
+
+
+// code example for New Binding
