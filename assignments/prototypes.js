0c82631fe3a5cf9d02787b3cb1c83957150bc08a
MIAlien <aliass.jr1@gmail.com>
2019-10-08 16:57:47 -0400

    game object and char stats added

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,12 +16,26 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.dimensions;
+  this.dimensions = attributes.dimensions;
+}
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the databasee.`;
+};
 /*
   === CharacterStats ===
   * healthPoints
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+
+
+function CharacterStats(attributes){
+  GameObject.call(this, attributes)
+  this.healthPoints = attributes.healthPoints;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
