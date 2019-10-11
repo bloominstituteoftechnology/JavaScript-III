b8acf86fe9e9d07d603abf65cd3a10628389e9c4
jasmine-dean <Amaraleemusic@gmail.com>
2019-10-10 19:12:45 -0700

    fixed the anwers about the differnet types of binding. I studied each and gave examples to show understanding

--- assignments/this.js
+++ assignments/this.js
@@ -1,15 +1,20 @@
  // The for principles of "this";
 //  in your own words. explain the four principle for the "this" keyword below.
 
-//  1. Window Global object binding 
-// Window binding is like the forest of trees, not sure which one we are pointing at so we are just pointing at the forest. 
+// //  1. Window Global object binding 
+// When you are inside of the global scope, the value of this is the entire window/console Object. It's a broad area that will need some specification. We can specify what we are implitmenting or pointignt o by using something referred to as "Implicit Binding" 
 //  2. Implicit Binding 
-    // Implicit binding means that we are pointing out a specific tree in our forest by saying "that tree"
-//  3. New Binding 
-    // New binding creates a new fucntion that says to plant that  new tree over there by using the "new" example within the code 
+// Implicit binding is pointing  to something directly inside of the global scope. We can specify what that is exactly by using "this." for example we can say "this.greeing" "this.age" "this.animal"
+// //  3. New Binding 
+// New binding is when we use the new keyword to call a fucntion that creates an object for us. An example is 
+// fucntion CordialPerson(greeter) and then using...
+// Const jasmine = new CordialPerson('tyler')
+// jasmine.speak ();
+
 
 //  4. Explicit Binding 
-// This says I want to go plant a new tree over there. 
+// Explicit binding is when we call someting explicitly with a new constext using .call and .apply 
+// an example of this is jasmine.speak.call(tyler);tyler.speak.apply(jasmine);
 
 //  write out a code example of each explanation above
 
