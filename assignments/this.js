73eff29e51648ab4a78bcf5d636cc4b0400df38b
jarrodskahill <jarrodskahill@gmail.com>
2019-10-16 15:31:03 -0700

    finished this page

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+* 1. Window Binding is when no other rules are present the "this" keyword will automatically implement window object, if strict mode is active it will default undefined instead.
 * 2. 
 * 3. 
 * 4. 
@@ -10,17 +10,49 @@
 */
 
 // Principle 1
+function hotWheel(monster){
+    console.log(`I use to love` + `${this.monster}` + ` trucks`);
+    return monster;
+
+}
+hotWheel(this); 
 
 // code example for Window Binding
 
 // Principle 2
+let jay = {
+    name: "Jarrod",
+    says: "what's up guys!",
+    jaySays: function (){
+        console.log(this.says)
+    }
+}
+
+jay.jaySays();
 
 // code example for Implicit Binding
 
 // Principle 3
+function fastCar(brz){
+    this.thing = brz;
+}
+
+let Car  = new fastCar('brz goes 0-60 in 6.2 sec');
+console.log(Car.thing);
 
 // code example for New Binding
 
 // Principle 4
+function car(){
+    console.log(this.fast);
+}
+
+let wrx = {
+    brand: "subaru",
+    fast: "0-60 in 5.5 seconds"
+}
+
+car.call(wrx)
+
 
 // code example for Explicit Binding
