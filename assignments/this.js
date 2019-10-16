9bb9c5dc8a5167a6cf96a59dbd670ad63de43f6f
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-16 16:02:16 -0400

    [Add] This questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. When "this" is not specified or binded to a specific object it will "attach" to the global or window object.
+* 2. When implicit binding, "this" keyword is used the function is looking to the left of the . where "this" is used. the func must be
+invoked for this to work.
 * 3. 
 * 4. 
 *
@@ -10,11 +11,15 @@
 */
 
 // Principle 1
-
 // code example for Window Binding
+function notSpecified(x){
+    console.log(this);
+    return x;
+}
+
+notSpecified('Is this binded?');
 
 // Principle 2
-
 // code example for Implicit Binding
 
 // Principle 3
