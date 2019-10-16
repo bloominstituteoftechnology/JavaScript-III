69405aa7cdc9d2ca809c4b4254f4875976d4a0d9
Aaron-Doran <wesley.gossett@gmail.com>
2019-10-16 15:36:58 -0400

    Init commit. I filled out the answers to the first four questions, and then wrote out an ex for Window Binding.

--- assignments/this.js
+++ assignments/this.js
@@ -1,18 +1,18 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. The first principle is Global this. in strict mode it will return undefined other wise it returns a global object. Window Binding a.k.a Global Binding happens when there is nothing for this to put to i.e. no context for this keyword to refer to
+ * 2. The second principle is Implicit Binding. When using Implicit Binding whatever function precedes the dot that is what this is refering to. In Impliciting binding this always refers to what ever object is to the left of the dot. Implicit binding is used about 80% of the time when using keyword this.
+ * 3. The third principle is New Binding. When using New Binding this keyword refers to a specific instance of an object that is created then returned by a constructor function. A constructor function is a function that is made and returns an object. It is essentially an object creator used to create new objetcs that share some common traits i.e. every Person would have a name:, age:, and location:.
+ * 4. The fourth, and final principle is Explicit Binding. Whenever .call or .apply method is used the keyword this is Explicitly defined to what ever parameter is in the () of .call or .apply i.e. myObj.call(person.speack());.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
 
 // code example for Window Binding
-
+console.log(this);
 // Principle 2
 
 // code example for Implicit Binding
