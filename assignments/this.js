04b981b87350fd26b14e9741ed6de293e62ae33b
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-16 16:48:45 -0400

    [Add] Added Questons

--- assignments/this.js
+++ assignments/this.js
@@ -4,8 +4,8 @@
 * 1. When "this" is not specified or binded to a specific object it will "attach" to the global or window object.
 * 2. When implicit binding, "this" keyword is used the function is looking to the left of the . where "this" is used. the func must be
 invoked for this to work.
-* 3. 
-* 4. 
+* 3. Explicit Biding is when a function uses the .apply, .call, .bind methods to objects/functions and immediately invoke "this". 
+* 4. New Binding is when a new object is created with the new keyword, this binds "this" to each new instance of the object created. This references the particular instance in which is instantiated.
 *
 * write out a code example of each explanation above
 */
