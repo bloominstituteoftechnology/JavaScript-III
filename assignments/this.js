1632ee3b24770dcae2b9356593026a1ed1e379f1
avi <avikan1087@gmail.com>
2019-10-16 22:09:44 -0400

    finish JS3

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.
+* 2.
+* 3.
+* 4.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,49 @@
 // Principle 1
 
 // code example for Window Binding
+function x() {
+    console.log(this);
+
+}
+
+x();
 
 // Principle 2
 
 // code example for Implicit Binding
 
+const person = {
+  firstName: 'john',
+  lastName: 'duo',
+  fun: function(){
+    console.log(this.firstName);
+  }
+}
+
+person.fun();
+
 // Principle 3
 
 // code example for New Binding
 
+function person2(talk){
+  this.x = talk;
+}
+
+let newPerson = new person2('I am a new person');
+
+console.log(newPerson.x); // function is being invoked
+
+
 // Principle 4
 
 // code example for Explicit Binding
+function any (){
+  console.log(this.firstName);
+}
+let newAny = {
+  firstName : 'Avi',
+  lastName : 'Kandinov'
+}
+something = any.bind(newAny);
+something();
