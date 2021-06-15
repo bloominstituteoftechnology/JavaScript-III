ec2c9e7780028c07257da9960097c55ed5bd1811
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-14 21:22:12 -0400

    GameObject & CharacterStats constructors complete, CS GO prototype inherited

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -17,13 +17,13 @@
   * dimensions
   * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
 */
-
-function GameObject(parameter) {
-    this.createdAt = parameter.createdAt;
-    this.dimensions = parameter.dimensions;
+function GameObject(gameAttr) {
+  this.createdAt = new Date();
+  this.dimensions = gameAttr.dimensions;  
 }
+
 GameObject.prototype.destroy = function() {
-  return `Object was removed from the game.`
+  return `${this.name} was removed from the game`
 }
 
 /*
@@ -33,16 +33,18 @@ GameObject.prototype.destroy = function() {
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
-function CharacterStats (charStats) {
-  GameObject.call(this,charStats);
-  this.hp = charStats.hp;
-  this.name = charStats.name;
+function CharacterStats(charAttr) {
+  GameObject.call(this, charAttr);
+  this.hp = charAttr.hp;
+  this.name = charAttr.name;
 }
-
 CharacterStats.prototype = Object.create(GameObject.prototype);
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage.`
+
+CharacterStats.prototype.takeDamage = function(obj) {
+  return `${this.name} took damage`;
 }
+let x = new CharacterStats({name:'hero',hp:100, dimensions: {'length': 1, 'width': 2, 'height': 4}})
+
 /*
   === Humanoid ===
   * faction
@@ -52,16 +54,6 @@ CharacterStats.prototype.takeDamage = function() {
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
-function Humanoid(humStats) {
-    CharacterStats.call(this, humStats);
-    this.faction = humStats.faction;
-    this.weapons = humStats.weapons;
-    this.language = humStats.language;
-  }
-  Humanoid.prototype = Object.create(CharacterStats.prototype);
-  Humanoid.prototype.greet = function() {
-    return `${this.name} offers a greeting in ${this.language}`;
-  }
 
 
 /*
@@ -72,51 +64,51 @@ function Humanoid(humStats) {
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-const mage = new Humanoid({
-  'createdAt': new Date(),
-  'dimensions': {
-    'length': 2,
-    'width': 1,
-    'height': 1,
-  },
-    'hp': 5,
-    'name': 'Bruce',
-    'faction': 'Mage Guild',
-    'weapons': ['Staff of Shamalama'],
-    'language': 'Common Toungue',
-  });
+// const mage = new Humanoid({
+//   'createdAt': new Date(),
+//   'dimensions': {
+//     'length': 2,
+//     'width': 1,
+//     'height': 1,
+//   },
+//     'hp': 5,
+//     'name': 'Bruce',
+//     'faction': 'Mage Guild',
+//     'weapons': ['Staff of Shamalama'],
+//     'language': 'Common Toungue',
+//   });
 
-  const swordsman = new Humanoid({
-    'createdAt': new Date(),
-    'dimensions': {'length': 2, 'width': 2, 'height': 2},
-    'hp': 15,
-    'name': 'Sir Mustachio',
-    'weapons': ['Giant Sword', 'Shield'],
-    'language': 'Common Toungue',
-    'faction': 'Round Table'
-  });
+//   const swordsman = new Humanoid({
+//     'createdAt': new Date(),
+//     'dimensions': {'length': 2, 'width': 2, 'height': 2},
+//     'hp': 15,
+//     'name': 'Sir Mustachio',
+//     'weapons': ['Giant Sword', 'Shield'],
+//     'language': 'Common Toungue',
+//     'faction': 'Round Table'
+//   });
 
-  const archer = new Humanoid({
-    'createdAt': new Date(),
-    'dimensions': {'length': 1, 'width': 2, 'height': 4},
-    'hp': 10,
-    'name': 'Lilith',
-    faction: 'Forest Kingdom',
-    'weapons': ['Bow', 'Dagger'],
-    'language': 'Elvish'
-  });
+//   const archer = new Humanoid({
+//     'createdAt': new Date(),
+//     'dimensions': {'length': 1, 'width': 2, 'height': 4},
+//     'hp': 10,
+//     'name': 'Lilith',
+//     faction: 'Forest Kingdom',
+//     'weapons': ['Bow', 'Dagger'],
+//     'language': 'Elvish'
+//   });
 
   //console.log()
-   console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  // console.log(mage.createdAt); // Today's date
+  // console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  // console.log(swordsman.hp); // 15
+  // console.log(mage.name); // Bruce
+  // console.log(swordsman.faction); // The Round Table
+  // console.log(mage.weapons); // Staff of Shamalama
+  // console.log(archer.language); // Elvish
+  // console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  // console.log(mage.takeDamage()); // Bruce took damage.
+  // console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
   // Stretch task:
